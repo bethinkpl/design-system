b072623233aaf78888d4205c5fe77eb9e810b0bf
Damian Jóźwiak <damian.jozwiak@bethink.pl>
2020-03-20 13:21:15 +0100

    IT-2185 | m-labelWithInfo

---
+++ src/js/molecules/LabelWithInfo.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="m-labelWithInfo">
+		<slot />
+		<div>
+			<slot name="info" />
+		</div>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+	@import 'resources/assets/styles/styleguide/settings/colors';
+
+	.m-labelWithInfo {
+		justify-content: space-between;
+		display: flex;
+
+		.a-icon {
+			color: $color-cadet-blue;
+			cursor: pointer;
+		}
+	}
+</style>
