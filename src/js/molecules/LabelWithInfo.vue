7ac8b88227c5ab4138e120d51a4e71c666e816f0
Damian Jóźwiak <damian.jozwiak@bethink.pl>
2020-03-23 12:13:21 +0100

    IT-2185 | Lato in inputs and buttons

--- src/js/molecules/LabelWithInfo.vue
+++ src/js/molecules/LabelWithInfo.vue
@@ -9,10 +9,15 @@
 
 <style lang="scss" scoped>
 	@import 'resources/assets/styles/styleguide/settings/colors';
+	@import 'resources/assets/styles/styleguide/settings/spacings';
 
 	.m-labelWithInfo {
 		justify-content: space-between;
 		display: flex;
+
+		.a-label {
+			margin-bottom: $space-xxs;
+		}
 
 		.a-icon {
 			color: $color-cadet-blue;
