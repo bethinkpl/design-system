73ef1f659deccc1180f686131d73bd9c77b14b50
Piotr Ziółkowski <pioziol@gmail.com>
2020-07-22 09:39:47 +0200

    IT-2660 mobile csses

--- src/js/organisms/ExpandableOverlay.vue
+++ src/js/organisms/ExpandableOverlay.vue
@@ -1,30 +1,42 @@
 <template>
-	<div class="o-expandableOverlay">
-		<div class="o-expandableOverlay__bar" :class="{'o-expandableOverlay__fixed_bar': isExpanded}">
-			<div></div>
-			<span v-if="!isExpanded" class="o-expandableOverlay__bar__message">{{shortMessage}}</span>
-			<a class="a-button -text" @click.prevent="toggleIsExpanded">
-				<template v-if="isMobile">
-					<span v-if="isExpanded">Zwiń</span>
-					<span v-else>Czytaj więcej</span>
-				</template>
-				<i class="wnl-icon fa" :class="{ 'fa-angle-up': !isExpanded, 'fa-angle-down': isExpanded }" />
-			</a>
-		</div>
-		<div v-if="isExpanded" class="o-expandableOverlay__screen">
-			<div class="o-expandableOverlay__screen__body">
-				<slot />
-			</div>
-
-			<div class="o-expandableOverlay__screen__footer">
-				<div v-if="isMobile" class="o-expandableOverlay__screen__footer__message">
-					W razie problemów napisz do nas na Messengerze <br> lub wyślij maila na adres <a :href="mailtoContactEmail">{{contactEmail}}</a>
-				</div>
-				<img
-					class="o-expandableOverlay__screen__footer__messenger margin vertical"
-					:src="messengerIconSrc"
-					alt="Messenger icon"
+	<div class="o-expandableOverlay" :class="{'o-expandableOverlay__expanded': isExpanded}">
+		<div :class="{'o-expandableOverlay__container': isExpanded}">
+			<div class="o-expandableOverlay__bar">
+				<div style="width: 16%" />
+				<span v-if="!isExpanded" class="o-expandableOverlay__bar__message">{{shortMessage}}</span>
+				<a
+					class="a-button -text"
+					style="width: 16%"
+					@click.prevent="toggleIsExpanded"
 				>
+					<template v-if="!isMobile">
+						<span v-if="isExpanded">Zwiń</span>
+						<span v-else>Czytaj więcej</span>
+					</template>
+					<i class="wnl-icon fa" :class="{ 'fa-angle-up': isExpanded, 'fa-angle-down': !isExpanded }" />
+				</a>
+			</div>
+			<div
+				v-if="isExpanded"
+				class="o-expandableOverlay__screen"
+				:class="{'o-expandableOverlay__screen__desktop': !isMobile}"
+			>
+				<div
+					class="o-expandableOverlay__screen__body"
+				>
+					<slot />
+				</div>
+
+				<div class="o-expandableOverlay__screen__footer">
+					<div v-if="!isMobile" class="o-expandableOverlay__screen__footer__message">
+						W razie problemów napisz do nas na Messengerze <br> lub wyślij maila na adres <a :href="mailtoContactEmail">{{contactEmail}}</a>
+					</div>
+					<img
+						class="o-expandableOverlay__screen__footer__messenger margin vertical"
+						:src="messengerIconSrc"
+						alt="Messenger icon"
+					>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -36,24 +48,33 @@
 	.o-expandableOverlay {
 		$bar-height: 56px;
 		z-index: $z-index-critical-info-overlay;
+		background-color: #E6E8EE;
+
 
 		&__expanded {
+			position: fixed;
+			background-image: linear-gradient(#E6E8EE, white);
+			top: $navbar-height;
+			left: 0;
+			right: 0;
+			bottom: 0;
+		}
+
+		&__container {
+			overflow-x: scroll;
 			height: 100%;
 		}
+
 		&__bar {
 			display: flex;
 			flex: 0 57px;
 			justify-content: space-between;
 			height: $bar-height;
-			background-color: #E6E8EE;
-			box-shadow: 0 8px 16px 0 rgba(12, 23, 38, 0.15);
 			align-items: center;
 			text-align: center;
 
-
 			&__message {
-				position: relative;
-				width: 100%;
+				position: center;
 				color: #0C1726;
 				font-family: Lato, monospace;
 				font-size: 16px;
@@ -62,32 +83,25 @@
 			}
 		}
 
-		&__fixed_bar {
-			position: fixed;
-			top: $navbar-height;
-			bottom: 0;
-			left: 0;
-			right: 0;
-		}
-
 		&__screen {
 			display: flex;
 			align-items: center;
 			flex-direction: column;
 			justify-content: space-between;
-			background-image: linear-gradient(#E6E8EE, white);
-			position: fixed;
-			top: $navbar-height + $bar-height;
-			bottom: 0;
-			left: 0;
-			right: 0;
+
+			&__desktop {
+				position: fixed;
+				top: $navbar-height + $bar-height;
+				bottom: 0;
+				left: 0;
+				right: 0;
+			}
 
 			&__body {
 				display: flex;
 				align-items: center;
 				justify-content: center;
 				text-align: center;
-				position: relative;
 				height: 100%;
 			}
 
@@ -96,13 +110,9 @@
 				text-align: center;
 				justify-content: flex-end;
 				align-items: center;
+				margin-bottom: 23px;
 				height: 57px;
 				width: 100%;
-
-				&__message {
-					margin-left: 100px;
-					margin-bottom: 23px;
-				}
 
 				&__messenger {
 					margin-left: 23px;
@@ -139,7 +149,7 @@ export default {
 		messengerIconSrc() {
 			return this.getSharedImageUrl('messenger-icon.svg');
 		},
-		...mapGetters('ui', ['isMobile']),
+		...mapGetters(['isMobile']),
 	},
 	methods: {
 		toggleIsExpanded() {
