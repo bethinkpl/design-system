e3aaf3036eb669aa4c5e7414a6d8705932e140fa
Damian Jóźwiak <damian.jozwiak@bethink.pl>
2020-07-30 14:17:24 +0200

    IT-2638 | Modify a-title sizes

--- src/js/atoms/Title.vue
+++ src/js/atoms/Title.vue
@@ -15,28 +15,30 @@
 	@import 'resources/assets/styles/styleguide/settings/typography';
 
 	.a-title {
+		&.-level1 {
+			@include textLight;
+			@include header1;
+		}
 
 		&.-level2 {
-			@include textBold;
 			@include header2;
 		}
 
 		&.-level3 {
-			@include textBold;
 			@include header3;
 		}
 
 		&.-level4 {
-			@include titleLevel4
+			@include textBold;
+			@include header4;
 		}
 
 		&.-level5 {
-			@include header6;
+			@include header5;
 		}
 
 		&.-level6 {
-			@include textM;
-			@include textMedium;
+			@include header6;
 		}
 
 		&.-link {
