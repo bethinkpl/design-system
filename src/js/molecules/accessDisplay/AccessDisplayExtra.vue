8dc8a370312bc555d5286fd24c69611ce5318ed4
Maciej Brencz <maciej.brencz@bethink.pl>
2020-09-01 15:45:57 +0200

    Update resources/assets/js/components/global/styleguide/molecules/accessDisplay/AccessDisplayExtra.vue

--- src/js/molecules/accessDisplay/AccessDisplayExtra.vue
+++ src/js/molecules/accessDisplay/AccessDisplayExtra.vue
@@ -4,7 +4,7 @@
 		{{ access.extra.reason }}
 	</span>
 	<span v-else-if="access.order_id">
-		Zamówienie nr:
+		Zamówienie:
 		<router-link :to="{ name: 'my-orders' }">{{ access.order_id }}</router-link>
 		<span v-if="access.extra.comment">
 			{{ access.extra.comment }}
