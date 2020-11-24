1ecc914004c4707e20b33e1a9214d31277b73d4e
Przemyslaw Jan Beigert <przemyslaw.jan.beigert@gmail.com>
2020-11-24 16:36:30 +0100

    IT-3208: add unit test to TabItem component

--- src/js/atoms/TabItem.spec.ts
+++ src/js/atoms/TabItem.spec.ts
@@ -4,7 +4,7 @@ import { createLocalVue, shallowMount } from '@vue/test-utils';
 import TabItem from './TabItem.vue';
 
 describe('TabItem', () => {
-	const createComponent = ({ arg = false } = {}) => {
+	const createComponent = ({ isActive = false } = {}) => {
 		const localVue = createLocalVue();
 		localVue.use(Vuex);
 		const store = new Store({
@@ -15,7 +15,11 @@ describe('TabItem', () => {
 			localVue,
 			store,
 			mocks: {},
-			propsData: {},
+			propsData: {
+				faIconClass: 'fa-calendar',
+				isActive,
+				tooltipText: 'wpłynąłem na suchego przestwór oceanu',
+			},
 		});
 	};
 
@@ -23,5 +27,17 @@ describe('TabItem', () => {
 		const component = createComponent();
 
 		expect(component.exists()).toBe(true);
+	});
+
+	it('when prop isActive is set to false component should not render "isActive" class', () => {
+		const component = createComponent({ isActive: false });
+
+		expect(component.find('.-isActive').exists()).toBe(false);
+	});
+
+	it('when prop isActive is set to true component should render "isActive" class', () => {
+		const component = createComponent({ isActive: true });
+
+		expect(component.find('.-isActive').exists()).toBe(true);
 	});
 });
