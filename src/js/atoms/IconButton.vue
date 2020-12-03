a9953878a1e29501487291cbbff304bab0a441f2
Damian Jóźwiak <damian.jozwiak@bethink.pl>
2020-12-03 12:14:51 +0100

    IT-2639 | Remove ArrowButton

--- src/js/atoms/IconButton.vue
+++ src/js/atoms/IconButton.vue
@@ -1,12 +1,19 @@
 <template>
-	<wnl-button class="-icon" :state="hovered ? BUTTON_STATES.HOVERED : BUTTON_STATES.DEFAULT">
+	<wnl-button class="-icon" :radius="radius" :size="size" :type="type" :state="state">
 		<wnl-icon :fa-icon-class="faIconClass" size="x-small" />
 	</wnl-button>
 </template>
 
 <script>
 import WnlIcon from 'js/components/global/styleguide/atoms/Icon';
-import WnlButton, { STATES as BUTTON_STATES } from 'js/components/global/styleguide/atoms/Button';
+import WnlButton, {
+	STATES,
+	SIZES,
+	RADIUSES,
+	TYPES,
+} from 'js/components/global/styleguide/atoms/Button';
+
+export { STATES, SIZES, RADIUSES, TYPES };
 
 export default {
 	components: {
@@ -14,17 +21,38 @@ export default {
 		WnlButton,
 	},
 	props: {
+		size: {
+			type: String,
+			default: SIZES.MEDIUM,
+			validator(value) {
+				return Object.values(SIZES).includes(value);
+			},
+		},
+		radius: {
+			type: String,
+			default: RADIUSES.CAPSULE,
+			validator(value) {
+				return Object.values(RADIUSES).includes(value);
+			},
+		},
+		type: {
+			type: String,
+			default: TYPES.FILLED,
+			validator(value) {
+				return Object.values(TYPES).includes(value);
+			},
+		},
 		faIconClass: {
 			type: String,
 			required: true,
 		},
-		hovered: {
-			type: Boolean,
-			default: false,
+		state: {
+			type: String,
+			default: STATES.DEFAULT,
+			validator(value) {
+				return Object.values(STATES).includes(value);
+			},
 		},
-	},
-	created() {
-		this.BUTTON_STATES = BUTTON_STATES;
 	},
 };
 </script>
