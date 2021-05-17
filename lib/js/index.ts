eb60f889ffc15b96e2030a6d5ea88b48a90e03bd
Damian Jóźwiak <damian.jozwiak@bethink.pl>
2021-05-17 12:24:39 +0200

    It-3683 | Export consts

--- lib/js/index.ts
+++ lib/js/index.ts
@@ -1,8 +1,13 @@
 export { default as BadgeScore } from './atoms/BadgeScore';
+export * from './atoms/BadgeScore/BadgeScore.consts';
 export { default as App } from './atoms/App';
 export { default as Button } from './atoms/Button';
+export * from './atoms/Button/Button.consts';
 export { default as Icon } from './atoms/Icon';
+export * from './atoms/Icon/Icon.consts';
 export { default as IconButton } from './atoms/IconButton';
+export * from './atoms/IconButton/IconButton.consts';
 export { default as Input } from './atoms/Input';
 export { default as NumberInCircle } from './atoms/NumberInCircle';
+export * from './atoms/NumberInCircle/NumberInCircle.consts';
 export { default as TabItem } from './atoms/TabItem';
