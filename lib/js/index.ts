9cedb1f21aa530773779db1ad9f5e5c1f6a39145
Przemyslaw Jan Beigert <przemyslaw.jan.beigert@gmail.com>
2021-05-19 16:14:58 +0200

    IT-3662 | add select component to DS

--- lib/js/index.ts
+++ lib/js/index.ts
@@ -12,3 +12,4 @@ export { default as NumberInCircle } from './atoms/NumberInCircle';
 export * from './atoms/NumberInCircle/NumberInCircle.consts';
 export { default as TabItem } from './atoms/TabItem';
 export { default as AccessStatus } from './molecules/AccessStatus.vue';
+export { default as Select } from './atoms/Select.vue';
