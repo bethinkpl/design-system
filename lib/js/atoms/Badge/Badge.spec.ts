297830e9cd3d4fc9e9929170443edd121d3e2c83
Przemyslaw Jan Beigert <przemyslaw.jan.beigert@gmail.com>
2021-07-01 14:00:04 +0200

    IT-3247 | create Badge component

---
+++ lib/js/atoms/Badge/Badge.spec.ts
@@ -0,0 +1,39 @@
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+
+import Badge from './Badge.vue';
+
+describe('Badge', () => {
+	const createComponent = ({ label = '' } = {}) => {
+		const localVue = createLocalVue();
+
+		return shallowMount(Badge, {
+			localVue,
+			mocks: {},
+			propsData: {
+				label,
+			},
+			stubs: {},
+		});
+	};
+
+	it('should create', () => {
+		const component = createComponent();
+
+		expect(component.exists()).toBe(true);
+	});
+
+	it('should render badge class', () => {
+		const component = createComponent();
+
+		expect(component.find('.badge').exists()).toBe(true);
+	});
+
+	it.each(['test42', 'wpłynąłem na suchego przestwór oceanu'])(
+		'should render text from label props',
+		(label) => {
+			const component = createComponent({ label });
+
+			expect(component.find('.badge__label').text()).toContain(label);
+		},
+	);
+});
