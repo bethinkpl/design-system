56d8ffd720058d55e5c67301f568a696afaed11e
Przemyslaw Jan Beigert <przemyslaw.jan.beigert@gmail.com>
2021-10-28 14:06:05 +0200

    Modal | move component from platform

---
+++ lib/js/atoms/Modal/Modal.vue
@@ -0,0 +1,130 @@
+<template>
+	<div class="m-modal" @click.stop="$emit('close-modal')">
+		<div class="m-modal__contentWrapper" @click.stop>
+			<div v-if="showHeader" class="m-modal__header">
+				<slot name="header" />
+				<wnl-icon
+					touchable
+					:icon="ICONS.FA_TIMES"
+					class="-stormGray m-modal__header__close"
+					:size="ICON_SIZES.SMALL"
+					@click.native.stop="$emit('close-modal')"
+				/>
+			</div>
+			<div
+				class="m-modal__content"
+				:class="{ 'scrollable-main-container': animateScrollingContent }"
+			>
+				<slot />
+			</div>
+			<div class="m-modal__content__shadow" />
+			<div v-if="$slots.footer" class="m-modal__footer">
+				<slot name="footer" />
+			</div>
+		</div>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+@import 'resources/assets/styles/variables';
+@import 'design-system/lib/styles/settings/colors';
+@import 'design-system/lib/styles/settings/spacings';
+@import 'design-system/lib/styles/settings/typography';
+
+.m-modal {
+	align-items: center;
+	background-color: rgba(10, 10, 10, 0.85);
+	bottom: 0;
+	display: flex;
+	font-size: $font-size-text-l;
+	justify-content: center;
+	left: 0;
+	line-height: $line-height-text-l;
+	position: fixed;
+	right: 0;
+	top: 0;
+	z-index: $z-index-modal;
+
+	&__contentWrapper {
+		background-color: $color-total-white;
+		box-shadow: 0 4px 34px rgba(0, 0, 0, 0.85);
+		display: flex;
+		flex-direction: column;
+		margin: $space-xl $space-s $space-l;
+		max-height: calc(100vh - #{$space-xl + $space-l});
+		overflow: hidden;
+		position: relative;
+		width: 100%;
+
+		@media #{media-query-tablet()} {
+			margin: $space-xxl auto $space-xl;
+			max-height: calc(100vh - #{$space-xxl + $space-xl});
+			width: 640px;
+		}
+	}
+
+	&__header {
+		align-items: center;
+		box-shadow: 0 $space-xxxxs $space-xxxxs $color-total-white;
+		display: flex;
+		justify-content: space-between;
+		padding: $space-xs $space-xs $space-xxxs;
+		z-index: 1;
+
+		&__close:only-child {
+			margin-left: auto;
+		}
+	}
+
+	&__footer {
+		padding: $space-m 0;
+		text-align: center;
+	}
+
+	&__content {
+		overflow: auto;
+		padding: $space-s;
+
+		&__shadow {
+			box-shadow: 0 #{-$space-xxxxs} $space-xxxxs $color-total-white;
+			content: ' ';
+			height: $space-xxxxs;
+			z-index: 1;
+		}
+	}
+
+	// Atoms viable inside modal
+	.a-subtitle {
+		margin-bottom: $space-xs;
+		margin-top: $space-m;
+		text-transform: uppercase;
+
+		&:first-child {
+			margin-top: 0;
+		}
+	}
+}
+</style>
+
+<script lang="ts">
+import WnlIcon, { ICON_SIZES, ICONS } from '../Icon';
+
+export default {
+	name: 'Modal',
+	components: { WnlIcon },
+	props: {
+		showHeader: {
+			type: Boolean,
+			default: true,
+		},
+		animateScrollingContent: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	created() {
+		this.ICONS = ICONS;
+		this.ICON_SIZES = ICON_SIZES;
+	},
+};
+</script>
--- lib/js/index.ts
+++ lib/js/index.ts
@@ -11,6 +11,7 @@ export { default as Icon } from './atoms/Icon';
 export * from './atoms/Icon/Icon.consts';
 export { default as IconButton } from './atoms/IconButton';
 export * from './atoms/IconButton/IconButton.consts';
+export { default as Modal } from './atoms/Modal';
 export { default as NumberInCircle } from './atoms/NumberInCircle';
 export * from './atoms/NumberInCircle/NumberInCircle.consts';
 export { default as TabItem } from './atoms/TabItem';
