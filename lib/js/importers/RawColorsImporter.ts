0e54c561f05d69c1268565a1ccbb5a08bbe4da7c
damian.szkudlarek <damian.szkudlarek@bethink.pl>
2022-02-17 15:48:24 +0100

    - almost finished importers. - working version, but probably will be modified by reading json files instead of txt files.

--- lib/js/importers/RawColorsImporter.ts
+++ lib/js/importers/RawColorsImporter.ts
@@ -7,10 +7,21 @@ const fsC = require('fs')
 const colorsFilesConfig = JSON.parse(
 	fsC.readFileSync('lib/js/importers/configs/rawColorsConfig.json', 'utf8')
 	);
+interface IResultJsonObject {
+	id: string;
+	label: string;
+	value: string;
+	weight: number;
+}
 
-const ImportSingleColorFile = (sourceFile: string, destinationFile: string, isTheme: boolean) => {
+const ImportSingleColorFile = (
+	sourceFile: string,
+	destinationFile: string,
+	destinationFileJson: string,
+	isTheme: boolean) => {
 	try {
 		let result: Array<string> = [];
+		let resultJson = {};
 		const data = fsC.readFileSync(colorsFilesConfig.sourcePath + sourceFile, 'utf8');
 		const lines = data.split('\n');
 		for (let line = 0; line < lines.length; line++){
@@ -32,12 +43,33 @@ const ImportSingleColorFile = (sourceFile: string, destinationFile: string, isTh
 					});
 				result.push(colorAssignment);
 
+				/** SCSS row structure */
 				const colorAssignmentParts = colorAssignment.split(':');
 				result.push(colorAssignmentParts[0] + '-rgb' + ': ' + hexToRgb(colorAssignmentParts[1].substring(0, colorAssignmentParts[1].length - 1).trim()) + ';');
-			}
 
-			saveColorFile(colorsFilesConfig.destinationPath + destinationFile, result);
+
+				/** JSON object structure */
+				const colorName = colorAssignmentParts[0].replace(/--/i, '').replace(/raw-/i, '');
+				const colorNameSplitted = colorName.split('-');
+				const category = (colorNameSplitted[1] === undefined) ? 'default' : colorNameSplitted[0];
+				colorAssignmentParts[1] = colorAssignmentParts[1].replace(';','').replace(' ','')
+
+				const resultJsonObject: IResultJsonObject = {
+					id: sourceFile + '_' + colorName,
+					label: colorName,
+					value: colorAssignmentParts[1],
+					weight: colorNameSplitted[3] ? parseInt(colorNameSplitted[3]) : parseInt(colorNameSplitted[1])
+				};
+				if (resultJson[category] === undefined) {
+					resultJson[category] = [];
+				}
+				resultJson[category].push(resultJsonObject);
+				resultJson[category].sort((a,b) => (parseInt(a.weight) > parseInt(b.weight)) ? 1 : ((parseInt(b.weight) > parseInt(a.weight)) ? -1 : 0))
+			}
 		}
+
+		saveColorFile(colorsFilesConfig.destinationPath + destinationFile, result);
+		saveColorFileJSON(colorsFilesConfig.destinationPath + destinationFileJson, resultJson);
 	} catch (err) {
 		console.error(err)
 	}
@@ -52,6 +84,13 @@ const hexToRgb = (hex: string ) => {
 		parseInt(result[2], 16) + ', ' + parseInt(result[3], 16)  : null;
 }
 
+const saveColorFileJSON  = (filepath: string, content: any) => {
+	let file = fsC.createWriteStream(filepath);
+	file.on('error', function(err) { console.log(err) });
+	file.write(JSON.stringify(content));
+	file.end();
+}
+
 const saveColorFile = (filepath: string, content: any) => {
 	let file = fsC.createWriteStream(filepath);
 	file.on('error', function(err) { console.log(err) });
@@ -63,7 +102,7 @@ const saveColorFile = (filepath: string, content: any) => {
 
 const RawColorsImporter = () => {
 	colorsFilesConfig.files.forEach(file => {
-		ImportSingleColorFile(file.source, file.destination, file.isTheme);
+		ImportSingleColorFile(file.source, file.destination, file.destinationJson, file.isTheme);
 	});
 }
 
--- lib/js/importers/TokensImporter.ts
+++ lib/js/importers/TokensImporter.ts
@@ -1,29 +1,70 @@
-const fsT = require('fs')
+const fsT = require('fs');
+const axios = require("axios");
 const tokensFilesConfig = JSON.parse(
 	fsT.readFileSync('lib/js/importers/configs/tokensConfig.json', 'utf8')
 );
 
-const ImportSingleTokenFile = (sourceFile: string, destinationFile: string) => {
+interface ITokenJsonObject {
+	id: string;
+	label: string;
+	value: string;
+}
+
+const ImportSingleTokenFile = (
+	binId: string,
+	destinationFile: string,
+	destinationFileJson: string
+) => {
 	try {
 		let result: Array<string> = [];
-		const data = fsT.readFileSync(tokensFilesConfig.sourcePath + sourceFile, 'utf8');
-		const nodesRoot = JSON.parse(data);
-		nodesRoot.nodes.forEach(obj => {
-			let tokenColor = obj.fill.from.name;
-			const tokenName = obj.fill.to.name;
-			const calculatedOpacity = obj.fill.transforms?.opacity ? (1 - obj.fill.transforms.opacity * -1 * 0.01) : null;
-			const tokenOpacity = (typeof calculatedOpacity === "number") ? parseFloat(calculatedOpacity.toFixed(2).toString()) : null;
-			tokenColor = tokenColor.replace(/gray\/gray|green\/green|gold\/gold|orange\/orange|red\/red|violet\/violet|indigo\/indigo|blue\/blue|FFF\/FFF|theme\/theme/gi, function(matched){
-				return tokensFilesConfig.doubleStringsMap[matched];
-			}).replace(/\//i, '-');
-			const tokenValue = (typeof calculatedOpacity === "number") ? 'rgba(var(--' + tokenColor +'), ' + tokenOpacity + ')' : '--' + tokenColor;
+		let resultJson = {};
+		// fsT.readFileSync(tokensFilesConfig.sourcePath + binId, 'utf8');
 
-			result.push('$' + tokenName.replace(/\//i, '-') + ': ' + tokenValue + ';');
-		});
-		saveTokenFile(tokensFilesConfig.destinationPath + destinationFile, result);
+		axios.get(tokensFilesConfig.jsonBinApiUrl + binId + '/latest')
+			.then(function (response) {
+				response.data.nodes.forEach(obj => {
+					let tokenColor = obj.fill.from.name;
+					let tokenName = obj.fill.to.name;
+					const calculatedOpacity = obj.fill.transforms?.opacity ? (1 - obj.fill.transforms.opacity * -1 * 0.01) : null;
+					const tokenOpacity = (typeof calculatedOpacity === "number") ? parseFloat(calculatedOpacity.toFixed(2).toString()) : null;
+					tokenColor = tokenColor.replace(/gray\/gray|green\/green|gold\/gold|orange\/orange|red\/red|violet\/violet|indigo\/indigo|blue\/blue|FFF\/FFF|theme\/theme/gi, function(matched){
+						return tokensFilesConfig.doubleStringsMap[matched];
+					}).replace(/\//i, '-');
+					let tokenValue = (typeof calculatedOpacity === "number") ? 'rgba(var(--' + tokenColor +'), ' + tokenOpacity + ')' : '--' + tokenColor;
+
+					tokenValue = tokenValue.toLowerCase();
+					tokenName = tokenName.replace(/\//i, '-');
+					result.push('$' + tokenName + ': ' + tokenValue + ';');
+
+					/** JSON object structure */
+						// const colorName = tokenName;//colorAssignmentParts[0].replace(/--raw-/i, '');
+					const tokenNameSplitted = tokenName.split('-');
+					const category = tokenNameSplitted[1];
+					const resultJsonObject: ITokenJsonObject = {
+						id: binId + '_' + tokenName,
+						label: tokenName,
+						value: tokenValue,
+					};
+					if (resultJson[category] === undefined) {
+						resultJson[category] = [];
+					}
+					resultJson[category].push(resultJsonObject);
+				});
+				saveTokenFile(tokensFilesConfig.destinationPath + destinationFile, result);
+				saveTokenFileJSON(tokensFilesConfig.destinationPath + destinationFileJson, resultJson);
+			})
+
+
 	} catch (err) {
 		console.error(err)
 	}
+}
+
+const saveTokenFileJSON  = (filepath: string, content: any) => {
+	let file = fsT.createWriteStream(filepath);
+	file.on('error', function(err) { console.log(err) });
+	file.write(JSON.stringify(content));
+	file.end();
 }
 
 const saveTokenFile = (filepath, content) => {
@@ -33,10 +74,9 @@ const saveTokenFile = (filepath, content) => {
 	file.end();
 }
 
-
 const TokensImporter = () => {
 	tokensFilesConfig.files.forEach(file => {
-		ImportSingleTokenFile(file.source, file.destination);
+		ImportSingleTokenFile(file.binId, file.destination, file.destinationJson);
 	});
 }
 
