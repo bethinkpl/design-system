e7a356f746af4b1a41d562549cff68b4c5cecf04
Sebastian Dawidziak <sebastian@dawidziak.eu>
2022-03-09 13:11:08 +0100

    create typings when building production

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,28 @@
 module.exports = {
+	parallel: process.env.NODE_ENV !== 'production',
 	css: {
 		extract: false,
 	},
 	chainWebpack: (config) => {
+		/**
+		 * These are some necessary steps changing the default webpack config of the Vue CLI
+		 * that need to be changed in order for TypeScript based components to generate their
+		 * declaration (.d.ts) files.
+		 * Discussed here https://github.com/vuejs/vue-cli/issues/1081
+		 */
+		if (process.env.NODE_ENV === 'production') {
+			config.module.rule('ts').uses.delete('cache-loader');
+			config.module
+				.rule('ts')
+				.use('ts-loader')
+				.loader('ts-loader')
+				.tap((options) => ({
+					...options,
+					transpileOnly: false,
+					happyPackMode: false,
+				}));
+		}
+
 		const svgRule = config.module.rule('svg');
 
 		svgRule.uses.clear();
