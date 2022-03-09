8f20c612def896442429e11e4e80599888424d0f
Sebastian Dawidziak <sebastian@dawidziak.eu>
2022-03-09 16:53:40 +0100

    add comment why we disable parallel in production build

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+	/**
+	 * we disable parallel for production, because if we use ts-loader the Thread Loader does
+	 * not work properly and communicates it with an error:
+	 * 		Syntax Error: Thread Loader (Worker 0)
+	 * 		Cannot read properties of undefined (reading 'hooks')
+	 */
 	parallel: process.env.NODE_ENV !== 'production',
 	css: {
 		extract: false,
