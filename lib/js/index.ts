809ce53abb6161eff0875c01dd400a739592d391
damianmef <damianszkudlarek@gmail.com>
2022-03-29 09:54:36 +0200

    - IT-4308 - reorganize code according to SB view structure

--- lib/js/index.ts
+++ lib/js/index.ts
@@ -1,28 +1,28 @@
-export { default as Badge } from './components/Badge';
-export * from './components/Badge';
-export * from './components/Badge/Badge.consts';
-export { default as BadgeScore } from './components/BadgeScore';
-export * from './components/BadgeScore/BadgeScore.consts';
-export { default as Button } from './components/Button';
-export * from './components/Button/Button.consts';
+export { default as Badge } from './components/badges/Badge';
+export * from './components/badges/Badge';
+export * from './components/badges/Badge/Badge.consts';
+export { default as BadgeScore } from './components/badges/BadgeScore';
+export * from './components/badges/BadgeScore/BadgeScore.consts';
+export { default as Button } from './components/buttons/Button';
+export * from './components/buttons/Button/Button.consts';
 export { default as Card } from './components/Card';
 export { default as Icon } from './components/Icon';
 export * from './components/Icon/Icon.consts';
-export { default as IconButton } from './components/IconButton';
-export * from './components/IconButton/IconButton.consts';
+export { default as IconButton } from './components/buttons/IconButton';
+export * from './components/buttons/IconButton/IconButton.consts';
 export { default as Modal } from './components/Modal';
 export { default as NumberInCircle } from './components/NumberInCircle';
 export * from './components/NumberInCircle/NumberInCircle.consts';
 export { default as TabItem } from './components/TabItem';
-export { default as AccessStatus } from './components/Statuses/AccessStatus.vue';
-export { default as BlockadeStatus } from './components/Statuses/BlockadeStatus.vue';
+export { default as AccessStatus } from './components/statuses/AccessStatus/AccessStatus.vue';
+export { default as BlockadeStatus } from './components/statuses/BlockadeStatus/BlockadeStatus.vue';
 export * from './consts/colors';
 export { default as SurveyToggle } from './components/SurveyToggle/SurveyToggle.vue';
 export * from './components/SurveyToggle/SurveyToggle.consts';
-export { default as SurveyQuestionScale } from './components/SurveyQuestion/SurveyQuestionScale.vue';
-export { default as SurveyQuestionOpenEnded } from './components/SurveyQuestion/SurveyQuestionOpenEnded.vue';
-export * from './components/SurveyQuestion/SurveyQuestion.consts';
-export * from './components/SurveyQuestion/SurveyQuestion.domain';
+export { default as SurveyQuestionScale } from './components/survey-questions/SurveyQuestionScale/SurveyQuestionScale.vue';
+export { default as SurveyQuestionOpenEnded } from './components/survey-questions/SurveyQuestionOpenEnded/SurveyQuestionOpenEnded.vue';
+export * from './components/survey-questions/SurveyQuestion.consts';
+export * from './components/survey-questions/SurveyQuestion.domain';
 
 export {
 	initialize as initializeIcons,
