0a18e1cfd8f9d00a170f22b6b28b33fc82fd27e8
damianmef <damianszkudlarek@gmail.com>
2022-04-10 12:54:26 +0200

    - IT-4305 - move Statuses

--- lib/js/components/Statuses/BlockadeStatus/BlockadeStatus.vue
+++ lib/js/components/Statuses/BlockadeStatus/BlockadeStatus.vue
@@ -6,9 +6,9 @@
 </template>
 
 <style lang="scss" scoped>
-@import '../../styles/settings/colors';
-@import '../../styles/settings/spacings';
-@import '../../styles/settings/typography';
+@import '../../../../styles/settings/colors';
+@import '../../../../styles/settings/spacings';
+@import '../../../../styles/settings/typography';
 
 .blockadeStatus {
 	align-items: center;
