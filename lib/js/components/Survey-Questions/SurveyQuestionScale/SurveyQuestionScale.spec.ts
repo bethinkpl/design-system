594954d411db4dda347409809aafaf28ded49359
damianmef <damianszkudlarek@gmail.com>
2022-04-10 16:38:05 +0200

    - IT-4283 - fix paths

--- lib/js/components/Survey-Questions/SurveyQuestionScale/SurveyQuestionScale.spec.ts
+++ lib/js/components/Survey-Questions/SurveyQuestionScale/SurveyQuestionScale.spec.ts
@@ -2,7 +2,7 @@ import { createLocalVue, shallowMount } from '@vue/test-utils';
 
 import SurveyQuestionScale from './SurveyQuestionScale.vue';
 import SurveyToggle, { SURVEY_TOGGLE_MEANINGS } from '../../SurveyToggle';
-import Card from '../../../components/Cards/Card/Card';
+import Card from '../../Cards/Card/Card.vue';
 import { SurveyQuestionScaleOption } from '../SurveyQuestion.domain';
 
 describe('SurveyQuestionScale', () => {
