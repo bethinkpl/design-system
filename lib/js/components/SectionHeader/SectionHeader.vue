62997ca6fbafe7abc3d8006d2e4fde973816d40f
rogatty <7030884+rogatty@users.noreply.github.com>
2022-05-04 12:38:26 +0200

    IT-4469 | add iconLeft

--- lib/js/components/SectionHeader/SectionHeader.vue
+++ lib/js/components/SectionHeader/SectionHeader.vue
@@ -1,12 +1,21 @@
 <template>
 	<div class="sectionHeader" :class="{ [sizeClass]: true }">
-		<div class="sectionHeader__titleWrapper">{{ text }}</div>
+		<div class="sectionHeader__titleWrapper">
+			<ds-icon
+				v-if="iconLeft"
+				class="sectionHeader__iconLeft"
+				:icon="iconLeft"
+				:size="ICON_SIZES.SMALL"
+			/>
+			<span>{{ text }}</span>
+		</div>
 		<slot></slot>
 	</div>
 </template>
 
 <style scoped lang="scss">
 @import '../../../styles/settings/colors/tokens';
+@import '../../../styles/settings/spacings';
 @import '../../../styles/settings/typography';
 
 .sectionHeader {
@@ -16,6 +25,9 @@
 
 	&__titleWrapper {
 		@include textBold();
+
+		align-items: center;
+		display: flex;
 	}
 
 	&.-size-l &__titleWrapper {
@@ -25,15 +37,30 @@
 	&.-size-m &__titleWrapper {
 		@include headlineM();
 	}
+
+	&__iconLeft {
+		margin-right: $space-xxs;
+	}
 }
 </style>
 
 <script lang="ts">
 import { SECTION_HEADER_SIZES } from './SectionHeader.consts';
+import DsIcon, { IconItem, ICONS, ICON_SIZES } from '../Icon';
 
 export default {
 	name: 'SectionHeader',
+	components: {
+		DsIcon,
+	},
 	props: {
+		iconLeft: {
+			type: Object as () => IconItem,
+			default: null,
+			validate(iconLeft: IconItem) {
+				return Object.values(ICONS).includes(iconLeft);
+			},
+		},
 		size: {
 			type: String,
 			default: SECTION_HEADER_SIZES.M,
@@ -50,6 +77,9 @@ export default {
 		sizeClass(): string {
 			return `-size-${this.size}`;
 		},
+	},
+	created() {
+		this.ICON_SIZES = ICON_SIZES;
 	},
 };
 </script>
