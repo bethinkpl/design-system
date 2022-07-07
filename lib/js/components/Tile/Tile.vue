a687f96bc30aa72198909570523412721305a813
Piotr Ziółkowski <pioziol@gmail.com>
2022-07-07 07:45:03 +0200

    IT-4624 hover, text and icon colors changes

--- lib/js/components/Tile/Tile.vue
+++ lib/js/components/Tile/Tile.vue
@@ -1,5 +1,5 @@
 <template>
-  <ds-ripple :disable="!interactive">
+  <ds-ripple :disable="!interactive" :color="rippleColor">
     <div :class="[tileColor, { '-interactive': interactive }]" class="a-tile">
       <ds-icon
           v-if="iconLeft"
@@ -44,7 +44,6 @@
   &__additionalText {
     @include textXS;
 
-    color: $color-neutral-text;
     flex-grow: 1;
     margin-left: $space-xs;
     max-width: 30%;
@@ -63,71 +62,101 @@
     @include textS;
   }
 
-  &__iconLeft {
-    color: $color-neutral-icon;
-    margin-right: $space-xs;
-  }
-
-  &__iconRight {
-    color: $color-neutral-icon;
-    margin-left: $space-xs;
-
-    &.-interactive {
-      color: $color-primary-icon;
-    }
-  }
-
-  &.-interactive {
-    cursor: pointer;
-
-    &:hover {
-      background-color: $color-neutral-background-hovered;
-    }
-  }
-
-  &.-neutral {
-    background-color: $color-neutral-background;
-
-    #{$self}__eyebrowText {
-      color: $color-neutral-text-weak;
-    }
-  }
-
-  &.-success {
-    background-color: $color-success-background;
-
-    #{$self}__eyebrowText {
-      color: $color-success-text;
-    }
-  }
-
-  &.-fail {
-    background-color: $color-fail-background;
-
-    #{$self}__eyebrowText {
-      color: $color-fail-text;
-    }
-  }
-
-  &.-primary {
-    background-color: $color-primary-background;
-
-    #{$self}__eyebrowText {
-      color: $color-primary-text;
-    }
-  }
-
   &__text {
     @include textM;
 
     color: $color-neutral-text-heavy;
     margin-top: $space-xxxxxs;
   }
+
+  &__iconLeft,
+  &__iconRight {
+    margin-right: $space-xs;
+  }
+
+  &.-interactive {
+    cursor: pointer;
+  }
+
+  &.-neutral {
+    background-color: $color-neutral-background;
+
+    #{$self}__eyebrowText,
+    #{$self}__additionalText {
+      color: $color-neutral-text-weak;
+    }
+    #{$self}__iconLeft,
+    #{$self}__iconRight {
+      color: $color-neutral-icon;
+    }
+    &.-interactive {
+      &:hover {
+        background-color: $color-neutral-background-hovered;
+      }
+    }
+  }
+
+  &.-success {
+    background-color: $color-success-background;
+
+    #{$self}__eyebrowText,
+    #{$self}__additionalText {
+      color: $color-success-text;
+    }
+
+    #{$self}__iconLeft,
+    #{$self}__iconRight {
+      color: $color-success-icon;
+    }
+    &.-interactive {
+      &:hover {
+        background-color: $color-success-background-hovered;
+      }
+    }
+  }
+
+  &.-fail {
+    background-color: $color-fail-background;
+
+    #{$self}__eyebrowText,
+    #{$self}__additionalText {
+      color: $color-fail-text;
+    }
+
+    #{$self}__iconLeft,
+    #{$self}__iconRight {
+      color: $color-fail-icon;
+    }
+    &.-interactive {
+      &:hover {
+        background-color: $color-fail-background-hovered;
+      }
+    }
+  }
+
+  &.-primary {
+    background-color: $color-primary-background;
+
+    #{$self}__eyebrowText,
+    #{$self}__additionalText {
+      color: $color-primary-text;
+    }
+
+    #{$self}__iconLeft,
+    #{$self}__iconRight {
+      color: $color-primary-icon;
+    }
+    &.-interactive {
+      &:hover {
+        background-color: $color-primary-background-hovered;
+      }
+    }
+  }
 }
 </style>
 
 <script lang="ts">
-import DsRipple from '../Ripple';
+import DsRipple, { RIPPLE_COLORS } from '../Ripple';
 import DsIcon, { ICON_SIZES, ICONS } from '../Icon';
 import { VueConstructor } from 'vue';
 import { TILE_COLORS } from './Tile.consts';
@@ -184,6 +213,14 @@ export default {
         [TILE_COLORS.PRIMARY]: '-primary',
         [TILE_COLORS.SUCCESS]: '-success',
         [TILE_COLORS.FAIL]: '-fail',
+      }[this.color];
+    },
+    rippleColor() {
+      return {
+        [TILE_COLORS.NEUTRAL]: RIPPLE_COLORS.NEUTRAL,
+        [TILE_COLORS.PRIMARY]: RIPPLE_COLORS.PRIMARY,
+        [TILE_COLORS.SUCCESS]: RIPPLE_COLORS.SUCCESS,
+        [TILE_COLORS.FAIL]: RIPPLE_COLORS.FAIL,
       }[this.color];
     }
   },
