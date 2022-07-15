6cefb7d14fa915c78bbd5955675c7deb0832eb51
rogatty <7030884+rogatty@users.noreply.github.com>
2022-07-15 08:54:00 +0000

    Deploying to master from @ bethinkpl/design-system@5b7618403f86b59e35b2bfc7e6496cde02315513 🚀

---
+++ dist/components/Banner/Banner.consts.d.ts
@@ -0,0 +1,13 @@
+export declare const BANNER_COLORS: {
+    readonly DEFAULT: "default";
+    readonly NEUTRAL: "neutral";
+    readonly INFO: "info";
+    readonly SUCCESS: "success";
+    readonly WARNING: "warning";
+    readonly FAIL: "fail";
+    readonly DANGER: "danger";
+};
+export declare const BANNER_LAYOUTS: {
+    readonly HORIZONTAL: "horizontal";
+    readonly VERTICAL: "vertical";
+};
