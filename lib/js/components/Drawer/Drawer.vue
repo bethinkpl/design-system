29043b0c356c8b4fad2ac41ef4043f16b6bc6748
rogatty <7030884+rogatty@users.noreply.github.com>
2022-08-05 15:37:21 +0200

    IT-4681 | add position prop to Drawer

--- lib/js/components/Drawer/Drawer.vue
+++ lib/js/components/Drawer/Drawer.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="drawer">
+	<div class="drawer" :class="{ [positionClassName]: true }">
 		<slot name="header" />
 		<slot />
 		<slot name="footer" />
@@ -10,13 +10,42 @@
 @import '../../../styles/settings/colors/tokens';
 
 .drawer {
-	border-left: 1px solid $color-neutral-border-weak;
 	height: 100%;
+
+	&.-positionLeft {
+		border-right: 1px solid $color-neutral-border-weak;
+	}
+
+	&.-positionRight {
+		border-left: 1px solid $color-neutral-border-weak;
+	}
 }
 </style>
 
 <script lang="ts">
+import { PropType } from 'vue';
+
+import { DRAWER_POSITIONS, DrawerPosition } from './Drawer.consts';
+
 export default {
 	name: 'Drawer',
+	props: {
+		position: {
+			type: String as PropType<DrawerPosition>,
+			default: DRAWER_POSITIONS.RIGHT,
+			validate(position) {
+				Object.values(DRAWER_POSITIONS).includes(position);
+			},
+		},
+	},
+	computed: {
+		positionClassName(): string {
+			if (this.position === DRAWER_POSITIONS.LEFT) {
+				return '-positionLeft';
+			}
+
+			return '-positionRight';
+		},
+	},
 };
 </script>
