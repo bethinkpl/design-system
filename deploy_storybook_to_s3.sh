dd3ffe2a1c2c603ee3211ca8d8fb22fe9ec2c5e9
rogatty <7030884+rogatty@users.noreply.github.com>
2022-08-05 11:55:55 +0200

    Add deploy_storybook_to_s3.sh

---
+++ deploy_storybook_to_s3.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+if ! command -v s3cmd &> /dev/null
+then
+    echo "s3cmd could not be found"
+    exit
+fi
+
+BUCKET="wnl-design-system-preview"
+BRANCH=$(git symbolic-ref --short -q HEAD | grep -oE -- '[A-Z]+-[0-9]+')
+
+if [ ! "$BRANCH" ]
+then
+  echo "invalid branch name"
+  exit
+fi
+
+echo "I am building a storybook"
+yarn storybook:build
+
+echo "Deploying storybook to $BUCKET as $BRANCH"
+s3cmd put -r -P public/storybook/* s3://$BUCKET/$BRANCH/
+
+echo "Your storybook is available on: https://$BUCKET.s3.amazonaws.com/public/storybook/$BRANCH/index.html"
