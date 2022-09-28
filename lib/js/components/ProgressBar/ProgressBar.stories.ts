fad6a0c208b5cb172f93baa017b760f01bd3d6d1
damianmef <damianszkudlarek@gmail.com>
2022-09-28 14:23:26 +0200

    - other stories

--- lib/js/components/ProgressBar/ProgressBar.stories.ts
+++ lib/js/components/ProgressBar/ProgressBar.stories.ts
@@ -1,87 +1,174 @@
 import ProgressBar from './ProgressBar.vue';
 import {
-    PROGRESS_BAR_COLORS,
-    PROGRESS_BAR_SIZES,
-    PROGRESS_BAR_LAYOUTS,
-    PROGRESS_BAR_RADII,
-    PROGRESS_BAR_LAYERS,
-    PROGRESS_BAR_COLOR_SCHEMES
+	PROGRESS_BAR_COLORS,
+	PROGRESS_BAR_SIZES,
+	PROGRESS_BAR_LAYOUTS,
+	PROGRESS_BAR_RADII,
+	PROGRESS_BAR_LAYERS,
+	PROGRESS_BAR_COLOR_SCHEMES,
 } from './ProgressBar.consts';
 
-import { ArgTypes, Meta, StoryFn } from '@storybook/vue';
+import { Args, ArgTypes, Meta, StoryFn } from '@storybook/vue';
 
 export default {
-    title: 'Components/ProgressBar',
-    component: ProgressBar,
+	title: 'Components/ProgressBar',
+	component: ProgressBar,
 } as Meta<typeof ProgressBar>;
 
 const StoryTemplate: StoryFn<typeof ProgressBar> = (argTypes) => ({
-    components: { ProgressBar },
-    props: Object.keys(argTypes),
-    template: `<ProgressBar v-bind=$props></ProgressBar>`,
+	components: { ProgressBar },
+	props: Object.keys(argTypes),
+	template: `<ProgressBar v-bind=$props></ProgressBar>`,
 });
 
 export const Interactive = StoryTemplate.bind({});
 
 const argTypes = {
-    numberOfLayers: {
-        control: { type: 'select', options: Object.values(PROGRESS_BAR_LAYERS) },
-        defaultValue: PROGRESS_BAR_LAYERS.ONE,
-    },
-    colorScheme: {
-        control: { type: 'select', options: Object.values(PROGRESS_BAR_COLOR_SCHEMES) },
-        defaultValue: PROGRESS_BAR_COLOR_SCHEMES.DEFAULT,
-    },
-    color: {
-        control: { type: 'select', options: Object.values(PROGRESS_BAR_COLORS) },
-        defaultValue: PROGRESS_BAR_COLORS.PRIMARY,
-    },
-    size: {
-        control: { type: 'select', options: Object.values(PROGRESS_BAR_SIZES) },
-        defaultValue: PROGRESS_BAR_SIZES.MEDIUM,
-    },
-    ranges: {
-        control: { type: 'array' },
-        defaultValue: [
-            {
-                layer: 1,
-                color: '#000',
-                percentValueFrom: 0,
-                length: 30,
-            },
-        ],
-    },
-    radius: {
-        control: { type: 'select', options: Object.values(PROGRESS_BAR_RADII) },
-        defaultValue: PROGRESS_BAR_RADII.DEFAULT,
-    },
-    layout: {
-        control: { type: 'select', options: Object.values(PROGRESS_BAR_LAYOUTS) },
-        defaultValue: PROGRESS_BAR_LAYOUTS.DEFAULT,
-    },
-    labelText: {
-        control: { type: 'text' },
-        defaultValue: '',
-    },
-    labelData: {
-        control: { type: 'text' },
-        defaultValue: '',
-    },
-    labelDataSupporting: {
-        control: { type: 'text' },
-        defaultValue: '',
-    },
-    labelDataSuffix: {
-        control: { type: 'text' },
-        defaultValue: '',
-    },
+	numberOfLayers: {
+		control: { type: 'select', options: Object.values(PROGRESS_BAR_LAYERS) },
+		defaultValue: PROGRESS_BAR_LAYERS.ONE,
+	},
+	colorScheme: {
+		control: { type: 'select', options: Object.values(PROGRESS_BAR_COLOR_SCHEMES) },
+		defaultValue: PROGRESS_BAR_COLOR_SCHEMES.DEFAULT,
+	},
+	color: {
+		control: { type: 'select', options: Object.values(PROGRESS_BAR_COLORS) },
+		defaultValue: PROGRESS_BAR_COLORS.PRIMARY,
+	},
+	size: {
+		control: { type: 'select', options: Object.values(PROGRESS_BAR_SIZES) },
+		defaultValue: PROGRESS_BAR_SIZES.MEDIUM,
+	},
+	ranges: {
+		control: { type: 'array' },
+		defaultValue: [
+			{
+				layer: 1,
+				percentValueFrom: 0,
+				length: 30,
+			},
+		],
+	},
+	radius: {
+		control: { type: 'select', options: Object.values(PROGRESS_BAR_RADII) },
+		defaultValue: PROGRESS_BAR_RADII.DEFAULT,
+	},
+	layout: {
+		control: { type: 'select', options: Object.values(PROGRESS_BAR_LAYOUTS) },
+		defaultValue: PROGRESS_BAR_LAYOUTS.DEFAULT,
+	},
+	labelText: {
+		control: { type: 'text' },
+		defaultValue: '',
+	},
+	labelData: {
+		control: { type: 'text' },
+		defaultValue: '',
+	},
+	labelDataSupporting: {
+		control: { type: 'text' },
+		defaultValue: '',
+	},
+	labelDataSuffix: {
+		control: { type: 'text' },
+		defaultValue: '',
+	},
 } as ArgTypes;
 
 Interactive.argTypes = argTypes;
 
 Interactive.parameters = {
-    design: {
-        type: 'figma',
-        url: 'https://www.figma.com/file/726BIQ076eFnS0ElNuPVC2/Statystyki-i-Dashboard-Bazy-zada%C5%84-na-V1?node-id=548%3A50027',
-    },
+	design: {
+		type: 'figma',
+		url: 'https://www.figma.com/file/726BIQ076eFnS0ElNuPVC2/Statystyki-i-Dashboard-Bazy-zada%C5%84-na-V1?node-id=548%3A50027',
+	},
 };
+
+export const TwoLayers = StoryTemplate.bind({});
+
+TwoLayers.args = {
+	color: PROGRESS_BAR_COLORS.PRIMARY,
+	numberOfLayers: 2,
+	ranges: [
+		{
+			layer: 1,
+			percentValueFrom: 0,
+			length: 30,
+		},
+		{
+			layer: 2,
+			percentValueFrom: 30,
+			length: 30,
+		},
+	]
+} as Args;
+
+export const WithLabels = StoryTemplate.bind({});
+
+WithLabels.args = {
+	color: PROGRESS_BAR_COLORS.PRIMARY,
+	ranges: [
+		{
+			layer: 1,
+			percentValueFrom: 0,
+			length: 30,
+		},
+	],
+	labelText: 'Label text',
+	labelData: '30',
+	labelDataSupporting: '100',
+	labelDataSuffix: '(%)',
+} as Args;
+
+export const WithGaps = StoryTemplate.bind({});
+
+WithGaps.args = {
+	color: PROGRESS_BAR_COLORS.PRIMARY,
+	numberOfLayers: 2,
+	ranges: [
+		{
+			layer: 1,
+			percentValueFrom: 0,
+			length: 10,
+		},
+		{
+			layer: 2,
+			percentValueFrom: 20,
+			length: 10,
+		},
+		{
+			layer: 1,
+			percentValueFrom: 40,
+			length: 10,
+		},
+		{
+			layer: 2,
+			percentValueFrom: 50,
+			length: 10,
+		},
+		{
+			layer: 1,
+			percentValueFrom: 70,
+			length: 100,
+		},
+	]
+} as Args;
+
+export const Compact = StoryTemplate.bind({});
+
+Compact.args = {
+	color: PROGRESS_BAR_COLORS.PRIMARY,
+	layout: PROGRESS_BAR_LAYOUTS.COMPACT,
+	ranges: [
+		{
+			layer: 1,
+			percentValueFrom: 0,
+			length: 30,
+		}
+	],
+	labelText: 'Label text',
+	labelData: '30',
+	labelDataSupporting: '100',
+	labelDataSuffix: '(%)',
+} as Args;
--- lib/js/components/ProgressBar/ProgressBar.vue
+++ lib/js/components/ProgressBar/ProgressBar.vue
@@ -1,7 +1,7 @@
 <template>
-  <div
-       :class="{
-      'progressBar': true,
+	<div
+		:class="{
+			progressBar: true,
 			'-primary': color === PROGRESS_BAR_COLORS.PRIMARY,
 			'-warning': color === PROGRESS_BAR_COLORS.WARNING,
 			'-fail': color === PROGRESS_BAR_COLORS.FAIL,
@@ -11,32 +11,43 @@
 			'-schemeMedium': colorScheme === PROGRESS_BAR_COLOR_SCHEMES.MEDIUM,
 			'-schemeMediumNeutral': colorScheme === PROGRESS_BAR_COLOR_SCHEMES.MEDIUM_NEUTRAL,
 			'-compact': layout === PROGRESS_BAR_LAYOUTS.COMPACT,
-		  }">
-    <div class="progressBar__label">
-      <div class="progressBar__labelText">{{ labelText }}</div>
-      <div v-if="labelDataExist" class="progressBar__labelDataWrapper">
-        <div v-if="labelData" class="progressBar__labelData">{{ labelData }}</div>
-        <div v-if="labelDataSupporting" class="progressBar__labelDataSupporting">
-          <span class="progressBar__labelDataSeparator">/</span>
-          {{ labelDataSupporting }}
-        </div>
-        <div v-if="labelDataSuffix" class="progressBar__labelDataSuffix">{{ labelDataSuffix }}</div>
-      </div>
-    </div>
-    <div
-      :class="{
-        'progressBar__bar': true,
-        '-small': size === PROGRESS_BAR_SIZES.SMALL,
-			  '-xsmall': size === PROGRESS_BAR_SIZES.XSMALL,
-			  '-noRadius': radius === PROGRESS_BAR_RADII.NONE,
-      }">
-      <div class="progressBar__results">
-        <div v-for="(range) in ranges" :class="{progressBar__result: true, '-secondary': range.layer === 2 && numberOfLayers === 2 }" :style="{ left: range.percentValueFrom+'%', width: range.length+'%'}">
-        </div>
-      </div>
-    </div>
-
-  </div>
+		}"
+	>
+		<div class="progressBar__label">
+			<div class="progressBar__labelText">{{ labelText }}</div>
+			<div v-if="labelDataExist" class="progressBar__labelDataWrapper">
+				<div v-if="labelData" class="progressBar__labelData">{{ labelData }}</div>
+				<div v-if="labelDataSupporting" class="progressBar__labelDataSupporting">
+					<span class="progressBar__labelDataSeparator">/</span>
+					{{ labelDataSupporting }}
+				</div>
+				<div v-if="labelDataSuffix" class="progressBar__labelDataSuffix">{{
+					labelDataSuffix
+				}}</div>
+			</div>
+		</div>
+		<div
+			:class="{
+				progressBar__bar: true,
+				'-small': size === PROGRESS_BAR_SIZES.SMALL,
+				'-xsmall': size === PROGRESS_BAR_SIZES.XSMALL,
+				'-noRadius': radius === PROGRESS_BAR_RADII.NONE,
+			}"
+		>
+			<div class="progressBar__results">
+				<div
+					v-for="(range, index) in ranges"
+					:key="index"
+					:class="{
+						progressBar__result: true,
+						'-secondary': range.layer === 2 && numberOfLayers === 2,
+					}"
+					:style="{ left: range.percentValueFrom + '%', width: range.length + '%' }"
+				>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <style scoped lang="scss">
@@ -52,262 +63,267 @@ $progress-bar-xs-height: 4px;
 $progress-bar-border-radius: 8px;
 
 $progress-bar-layers: (
-  'primary': (
-    'default': $color-primary-text,
-    'ghost': $color-primary-data-ghost,
-    'medium': $color-primary-data-medium,
-    'weak': $color-primary-data-weak,
-    'neutral-weak': $color-neutral-data-weak,
-  ),
-  'info': (
-    'default': $color-info-text,
-    'ghost': $color-info-data-ghost,
-    'medium': $color-info-data-medium,
-    'weak': $color-info-data-weak,
-    'neutral-weak': $color-neutral-data-weak,
-  ),
-  'neutral': (
-    'default': $color-neutral-data,
-    'ghost': $color-neutral-data-ghost,
-    'medium': $color-neutral-data-medium,
-    'weak': $color-neutral-data-weak,
-    'neutral-weak': $color-neutral-data-weak,
-  ),
-  'success': (
-    'default': $color-success-text,
-    'ghost': $color-success-data-ghost,
-    'medium': $color-success-data-medium,
-    'weak': $color-success-data-weak,
-    'neutral-weak': $color-neutral-data-weak,
-  ),
-  'warning': (
-    'default': $color-warning-text,
-    'ghost': $color-warning-data-ghost,
-    'medium': $color-warning-data-medium,
-    'weak': $color-warning-data-weak,
-    'neutral-weak': $color-neutral-data-weak,
-  ),
-  'fail': (
-    'default': $color-fail-text,
-    'ghost': $color-fail-data-ghost,
-    'medium': $color-fail-data-medium,
-    'weak': $color-fail-data-weak,
-    'neutral-weak': $color-neutral-data-weak,
-  ),
+	'primary': (
+		'default': $color-primary-text,
+		'ghost': $color-primary-data-ghost,
+		'medium': $color-primary-data-medium,
+		'weak': $color-primary-data-weak,
+		'neutral-weak': $color-neutral-data-weak,
+	),
+	'info': (
+		'default': $color-info-text,
+		'ghost': $color-info-data-ghost,
+		'medium': $color-info-data-medium,
+		'weak': $color-info-data-weak,
+		'neutral-weak': $color-neutral-data-weak,
+	),
+	'neutral': (
+		'default': $color-neutral-data,
+		'ghost': $color-neutral-data-ghost,
+		'medium': $color-neutral-data-medium,
+		'weak': $color-neutral-data-weak,
+		'neutral-weak': $color-neutral-data-weak,
+	),
+	'success': (
+		'default': $color-success-text,
+		'ghost': $color-success-data-ghost,
+		'medium': $color-success-data-medium,
+		'weak': $color-success-data-weak,
+		'neutral-weak': $color-neutral-data-weak,
+	),
+	'warning': (
+		'default': $color-warning-text,
+		'ghost': $color-warning-data-ghost,
+		'medium': $color-warning-data-medium,
+		'weak': $color-warning-data-weak,
+		'neutral-weak': $color-neutral-data-weak,
+	),
+	'fail': (
+		'default': $color-fail-text,
+		'ghost': $color-fail-data-ghost,
+		'medium': $color-fail-data-medium,
+		'weak': $color-fail-data-weak,
+		'neutral-weak': $color-neutral-data-weak,
+	),
 );
 
 .progressBar {
-  $self: &;
+	$self: &;
 
-  &__bar {
-    background-color: $color-default-background;
-    box-shadow: inset 0 1px 4px $color-default-shadow-heavy;
-    height: $progress-bar-height;
-    border-radius: $progress-bar-border-radius;
-    overflow: hidden;
-    &.-small {
-      height: $progress-bar-s-height;
-    }
+	@each $color-name, $color-map in $progress-bar-layers {
+		&.-#{$color-name} {
+			#{$self}__result {
+				background: map-get($color-map, 'default');
 
-    &.-xsmall {
-      height: $progress-bar-xs-height;
-      box-shadow: inset 0 1px 3px $color-default-shadow-heavy;
-      border-radius: 0;
-    }
-    &.-noRadius {
-      border-radius: 0;
-    }
-  }
+				&.-secondary {
+					background: map-get($color-map, 'ghost');
+				}
+			}
 
-  &__label {
-    display: flex;
-    justify-content: space-between;
-    margin-bottom: $space-xxxs;
+			&.-schemeMedium {
+				#{$self}__result {
+					background: map-get($color-map, 'medium');
 
-    @media #{breakpoint-s()} {
-      margin-bottom: $space-xxs;
-    }
-  }
+					&.-secondary {
+						background: map-get($color-map, 'weak');
+					}
+				}
+			}
 
-  &__labelText {
-    @include headlineXS();
-    @include textBold;
+			&.-schemeMediumNeutral {
+				#{$self}__result {
+					background: map-get($color-map, 'medium');
 
-    color: $color-neutral-text-heavy;
-    @media #{breakpoint-s()} {
-      @include headlineS();
-    }
-  }
+					&.-secondary {
+						background: map-get($color-map, 'neutral-weak');
+					}
+				}
+			}
+		}
+	}
 
-  &__labelDataWrapper {
-    @include textS();
-    @include textBold;
+	&__bar {
+		background-color: $color-default-background;
+		box-shadow: inset 0 1px 4px $color-default-shadow-heavy;
+		height: $progress-bar-height;
+		border-radius: $progress-bar-border-radius;
+		overflow: hidden;
 
-    align-items: baseline;
-    color: $color-neutral-text-heavy;
-    display: flex;
-    margin-left: $space-xxs;
-  }
+		&.-small {
+			height: $progress-bar-s-height;
+		}
 
-  &__labelDataSupporting {
-    color: $color-neutral-text;
-    display: flex;
-    margin-left: $space-xxxxxs;
-  }
+		&.-xsmall {
+			height: $progress-bar-xs-height;
+			box-shadow: inset 0 1px 3px $color-default-shadow-heavy;
+			border-radius: 0;
+		}
 
-  &__labelDataSeparator {
-    margin-right: $space-xxxxxs;
-  }
+		&.-noRadius {
+			border-radius: 0;
+		}
+	}
 
-  &__labelDataSuffix {
-    @include textXS;
+	&__label {
+		display: flex;
+		justify-content: space-between;
+		margin-bottom: $space-xxxs;
 
-    color: $color-neutral-text-weak;
-    margin-left: $space-xxxxs;
-  }
+		@media #{breakpoint-s()} {
+			margin-bottom: $space-xxs;
+		}
+	}
 
-  &-noRadius {
-    border-radius: 0;
-  }
+	&__labelText {
+		@include headlineXS();
+		@include textBold;
 
-  &.-compact {
-    #{$self}__labelText {
-      @include headlineXS();
-    }
-    #{$self}__label {
-      margin-bottom: $space-xxxs;
-    }
-  }
+		color: $color-neutral-text-heavy;
 
-  &__results {
-    height: 100%;
-    position: relative;
-    width: 100%;
-  }
+		@media #{breakpoint-s()} {
+			@include headlineS();
+		}
+	}
 
-  &__result {
-    height: 100%;
-    position: absolute;
-    top:0;
-  }
+	&__labelDataWrapper {
+		@include textS();
+		@include textBold;
 
-  @each $color-name, $color-map in $progress-bar-layers {
-    &.-#{$color-name} {
-      #{$self}__result {
-        background: map-get($color-map, 'default');
-        &.-secondary {
-          background: map-get($color-map, 'ghost');
-        }
-      }
+		align-items: baseline;
+		color: $color-neutral-text-heavy;
+		display: flex;
+		margin-left: $space-xxs;
+	}
 
-      &.-schemeMedium {
-        #{$self}__result {
-          background: map-get($color-map, 'medium');
+	&__labelDataSupporting {
+		color: $color-neutral-text;
+		display: flex;
+		margin-left: $space-xxxxxs;
+	}
 
-          &.-secondary {
-            background: map-get($color-map, 'weak');
-          }
-        }
-      }
-      &.-schemeMediumNeutral {
-        #{$self}__result {
-          background: map-get($color-map, 'medium');
+	&__labelDataSeparator {
+		margin-right: $space-xxxxxs;
+	}
 
-          &.-secondary {
-            background: map-get($color-map, 'neutral-weak');
-          }
-        }
-      }
-    }
-  }
+	&__labelDataSuffix {
+		@include textXS;
+
+		color: $color-neutral-text-weak;
+		margin-left: $space-xxxxs;
+	}
+
+	&-noRadius {
+		border-radius: 0;
+	}
+
+	&.-compact {
+		#{$self}__labelText {
+			@include headlineXS();
+		}
+		#{$self}__label {
+			margin-bottom: $space-xxxs;
+		}
+	}
+
+	&__results {
+		height: 100%;
+		position: relative;
+		width: 100%;
+	}
+
+	&__result {
+		height: 100%;
+		position: absolute;
+		top: 0;
+	}
 }
 </style>
 
 <script lang="ts">
 import {
-  PROGRESS_BAR_COLORS,
-  PROGRESS_BAR_SIZES,
-  PROGRESS_BAR_RADII,
-  PROGRESS_BAR_LAYOUTS,
-  PROGRESS_BAR_COLOR_SCHEMES
+	PROGRESS_BAR_COLORS,
+	PROGRESS_BAR_SIZES,
+	PROGRESS_BAR_RADII,
+	PROGRESS_BAR_LAYOUTS,
+	PROGRESS_BAR_COLOR_SCHEMES,
 } from './ProgressBar.consts';
 
 export default {
-  name: 'ProgressBar',
-  props: {
-    numberOfLayers: {
-      type: Number,
-      default: 1,
-    },
-    colorScheme: {
-      type: String,
-      default: PROGRESS_BAR_COLOR_SCHEMES.DEFAULT,
-      validate(size) {
-        return Object.values(PROGRESS_BAR_COLOR_SCHEMES).includes(size);
-      },
-    },
-    color: {
-      type: String,
-      required: true,
-      validate(color) {
-        return Object.values(PROGRESS_BAR_COLORS).includes(color);
-      },
-    },
-    size: {
-      type: String,
-      default: PROGRESS_BAR_SIZES.MEDIUM,
-      validate(size) {
-        return Object.values(PROGRESS_BAR_SIZES).includes(size);
-      },
-    },
-    ranges: {
-      type: Array,
-      required: true,
-    },
-    radius: {
-      type: String,
-      default: PROGRESS_BAR_RADII.DEFAULT,
-      validate(size) {
-        return Object.values(PROGRESS_BAR_RADII).includes(size);
-      },
-    },
-    layout: {
-      type: String,
-      default: PROGRESS_BAR_LAYOUTS.DEFAULT,
-      validate(size) {
-        return Object.values(PROGRESS_BAR_LAYOUTS).includes(size);
-      },
-    },
-    labelText: {
-      type: String,
-      default: null,
-    },
-    labelData: {
-      type: String,
-      default: null,
-    },
-    labelDataSupporting: {
-      type: String,
-      default: null,
-    },
-    labelDataSuffix: {
-      type: String,
-      default: null,
-    },
-  },
-  data() {
-    return {
-      PROGRESS_BAR_COLORS: Object.freeze(PROGRESS_BAR_COLORS),
-      PROGRESS_BAR_SIZES: Object.freeze(PROGRESS_BAR_SIZES),
-      PROGRESS_BAR_RADII: Object.freeze(PROGRESS_BAR_RADII),
-      PROGRESS_BAR_LAYOUTS: Object.freeze(PROGRESS_BAR_LAYOUTS),
-      PROGRESS_BAR_COLOR_SCHEMES: Object.freeze(PROGRESS_BAR_COLOR_SCHEMES),
-    };
-  },
-  computed: {
-    labelDataExist() {
-      return this.labelData || this.labelDataSupporting || this.labelDataSuffix;
-    }
-  },
+	name: 'ProgressBar',
+	props: {
+		numberOfLayers: {
+			type: Number,
+			default: 1,
+		},
+		colorScheme: {
+			type: String,
+			default: PROGRESS_BAR_COLOR_SCHEMES.DEFAULT,
+			validate(size) {
+				return Object.values(PROGRESS_BAR_COLOR_SCHEMES).includes(size);
+			},
+		},
+		color: {
+			type: String,
+			required: true,
+			validate(color) {
+				return Object.values(PROGRESS_BAR_COLORS).includes(color);
+			},
+		},
+		size: {
+			type: String,
+			default: PROGRESS_BAR_SIZES.MEDIUM,
+			validate(size) {
+				return Object.values(PROGRESS_BAR_SIZES).includes(size);
+			},
+		},
+		ranges: {
+			type: Array,
+			required: true,
+		},
+		radius: {
+			type: String,
+			default: PROGRESS_BAR_RADII.DEFAULT,
+			validate(size) {
+				return Object.values(PROGRESS_BAR_RADII).includes(size);
+			},
+		},
+		layout: {
+			type: String,
+			default: PROGRESS_BAR_LAYOUTS.DEFAULT,
+			validate(size) {
+				return Object.values(PROGRESS_BAR_LAYOUTS).includes(size);
+			},
+		},
+		labelText: {
+			type: String,
+			default: null,
+		},
+		labelData: {
+			type: String,
+			default: null,
+		},
+		labelDataSupporting: {
+			type: String,
+			default: null,
+		},
+		labelDataSuffix: {
+			type: String,
+			default: null,
+		},
+	},
+	data() {
+		return {
+			PROGRESS_BAR_COLORS: Object.freeze(PROGRESS_BAR_COLORS),
+			PROGRESS_BAR_SIZES: Object.freeze(PROGRESS_BAR_SIZES),
+			PROGRESS_BAR_RADII: Object.freeze(PROGRESS_BAR_RADII),
+			PROGRESS_BAR_LAYOUTS: Object.freeze(PROGRESS_BAR_LAYOUTS),
+			PROGRESS_BAR_COLOR_SCHEMES: Object.freeze(PROGRESS_BAR_COLOR_SCHEMES),
+		};
+	},
+	computed: {
+		labelDataExist() {
+			return this.labelData || this.labelDataSupporting || this.labelDataSuffix;
+		},
+	},
 };
 </script>
