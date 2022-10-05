7b8e71b50131c83f85a7f7e63bad55fb25aa2a6a
kvas-damian <kvas-damian@users.noreply.github.com>
2022-10-05 11:28:27 +0000

    Deploying to master from @ bethinkpl/design-system@481f39bb45aef4d5a5f6c3850324a22d64519450 🚀

---
+++ dist/components/ProgressBar/ProgressBar.consts.d.ts
@@ -0,0 +1,35 @@
+export declare const PROGRESS_BAR_COLORS: {
+    readonly NEUTRAL: "neutral";
+    readonly INFO: "info";
+    readonly PRIMARY: "primary";
+    readonly SUCCESS: "success";
+    readonly WARNING: "warning";
+    readonly FAIL: "fail";
+};
+export declare const PROGRESS_BAR_SIZES: {
+    readonly MEDIUM: "medium";
+    readonly SMALL: "small";
+    readonly XSMALL: "extra small";
+};
+export declare const PROGRESS_BAR_RADII: {
+    DEFAULT: string;
+    NONE: string;
+};
+export declare const PROGRESS_BAR_LAYOUTS: {
+    DEFAULT: string;
+    COMPACT: string;
+};
+export declare const PROGRESS_BAR_LAYERS: {
+    ONE: number;
+    TWO: number;
+};
+export declare const PROGRESS_BAR_COLOR_SCHEMES: {
+    DEFAULT: string;
+    MEDIUM: string;
+    MEDIUM_NEUTRAL: string;
+};
+export interface Range {
+    layer: Number;
+    start: Number;
+    length: Number;
+}
