389efec1fa330ae9394aa1a04fb2ddc13fdb3d2f
damianmef <damianszkudlarek@gmail.com>
2022-11-04 11:35:22 +0100

    - IT-4899 - working import for: variables, css-variables, tokens!

--- tools/importers/SynchronizeTypographyTokens.ts
+++ tools/importers/SynchronizeTypographyTokens.ts
@@ -2,7 +2,14 @@ const fsT = require('fs');
 const axios = require('axios');
 const tokensFilesConfig = require('./configs/SynchronizeTypographyTokensConfig.json');
 const typographyPrefix = 'typography-';
+const tokensKey = 'TypographyTokens';
 const variablesExcludedKeys = ['fontFamilies', 'TypographyTokens'];
+
+const fontFamilyProperty = 'font-families';
+const transformCssProperty = {
+	'text-case': 'text-transform',
+	'-regular': '-normal',
+};
 
 const jsonKeyToCssProperty = {
 	lineHeight: 'line-height',
@@ -86,6 +93,113 @@ const ImportTypographyRaw = (
 	arrayToFile(tokensFilesConfig.destinationPath + binValues.destinationVariables, resultScss);
 };
 
+const ImportTypographyTokensRaw = (binValues: configFileObject, jsonTypography: any) => {
+	let resultScss: Array<string> = [];
+
+	for (let key in jsonTypography[tokensKey]) {
+		resultScss.push(iterateObject(jsonTypography[tokensKey][key], key, []));
+	}
+
+	resultScss = resultScss.flat();
+	const tempTokens = buildTokenMixins(resultScss);
+
+	const finalTokens: Array<string> = [];
+	finalTokens.push("@import 'variables';");
+	let finalTokens2 = finalTokens.concat(tempTokens);
+	arrayToMixinFile(
+		tokensFilesConfig.destinationPath + binValues.destinationVariables,
+		finalTokens2,
+	);
+};
+
+function buildTokenMixins(tokens: Object): Array<string> {
+	let result: Array<string> = [];
+	for (let key in tokens) {
+		result.push('\n@mixin ' + tokens[key].tokenCamelCase + '() {');
+		tokens[key].attributes.forEach((attribute) => {
+			let [cssAttributePartOne, cssAttributePartTwo] = attribute.split('-');
+			result.push(
+				cssAttributePartOne +
+					'-' +
+					cssAttributePartTwo +
+					': $typography-' +
+					attribute +
+					';',
+			);
+		});
+		result.push('}');
+	}
+
+	return result;
+}
+
+function iterateObject(obj, keyResult, result) {
+	if ('value' in obj) {
+		return;
+	}
+
+	for (let key in obj) {
+		let temporaryKey = keyResult + ' ' + key;
+		let resultToPush = iterateObject(obj[key], temporaryKey, result);
+
+		if (resultToPush === undefined) {
+			let attributes: string[] = [];
+			for (let attrKey in obj[key].value) {
+				let attrValue = obj[key].value[attrKey]
+					.replace(/([a-z0-9])([A-Z])/g, '$1-$2')
+					.replace(/\.+/g, '-')
+					.slice(1, -1)
+					.toLowerCase();
+
+				attrValue = attrValue
+					.replace('text-case', transformCssProperty['text-case'])
+					.replace('-regular', transformCssProperty['-regular']);
+
+				if (attrValue && !attrValue.includes(fontFamilyProperty)) {
+					attributes.push(attrValue);
+				}
+			}
+			result.push({
+				token: temporaryKey
+					.replace(/([a-z0-9])([A-Z])/g, '$1-$2')
+					.replace(/\s+/g, '-')
+					.toLowerCase(),
+				tokenCamelCase: camelize(temporaryKey),
+				attributes,
+			});
+		}
+	}
+
+	return result;
+}
+
+function camelize(str) {
+	return str
+		.replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
+			return index === 0 ? word.toLowerCase() : word.toUpperCase();
+		})
+		.replace(/\s+/g, '');
+}
+
+const arrayToMixinFile = (filepath: string, content: Array<string>) => {
+	let file = fsT.createWriteStream(filepath);
+	file.on('error', function (err) {
+		console.log(err);
+	});
+
+	const patternTheme = /@mixin|@import|}/i;
+
+	content.forEach(function (v) {
+		let hasLineIndentation = true;
+		if (v.match(patternTheme)) {
+			hasLineIndentation = false;
+		}
+		console.log(v);
+		file.write(hasLineIndentation ? '\t' + v + '\n' : v + '\n');
+	});
+	file.end();
+};
+
 const arrayToFile = (filepath: string, content: Array<string>) => {
 	let file = fsT.createWriteStream(filepath);
 	file.on('error', function (err) {
@@ -126,6 +240,10 @@ const SynchronizeSingleBin = async (bin) => {
 		bin.files.variablesRaw,
 		response.data.record.values.LMSDesignSystemTypography,
 		bin.isTheme,
+	);
+	ImportTypographyTokensRaw(
+		bin.files.tokens,
+		response.data.record.values.LMSDesignSystemTypography,
 	);
 };
 
