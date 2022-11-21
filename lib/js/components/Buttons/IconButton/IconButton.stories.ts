37326ad392cf717e4fd3c2168db5135a3c5e065c
damianmef <damianszkudlarek@gmail.com>
2022-11-21 15:07:02 +0100

    - IT-4895 - fixes after CR

---
+++ lib/js/components/Buttons/IconButton/IconButton.stories.ts
@@ -0,0 +1,173 @@
+import IconButton from './IconButton.vue';
+import {
+	ICON_BUTTON_SIZES,
+	ICON_BUTTON_COLOR_SCHEMES,
+	ICON_BUTTON_TYPES,
+	ICON_BUTTON_COLORS,
+	ICON_BUTTON_STATES,
+} from './IconButton.consts';
+import { BUTTON_ELEVATIONS, BUTTON_RADIUSES } from '../Button';
+import { ICONS } from '../../Icon';
+
+import { Args, ArgTypes, Meta, StoryFn } from '@storybook/vue';
+
+export default {
+	title: 'Components/Buttons/IconButton',
+	component: IconButton,
+} as Meta<typeof IconButton>;
+
+const StoryTemplate: StoryFn<typeof IconButton> = (argTypes) => ({
+	components: { IconButton },
+	props: Object.keys(argTypes),
+	computed: {
+		isInverted() {
+			return this.color === 'inverted';
+		},
+	},
+	created() {
+		this.ICONS = ICONS;
+		this.ICON_BUTTON_COLORS = ICON_BUTTON_COLORS;
+	},
+	template: `<div :class="{ contrastBackground: isInverted }">
+	<icon-button
+		:icon="ICONS[icon]"
+		:size="size"
+		:type="type"
+		:radius="radius"
+		:color-scheme="colorScheme"
+		:touchable="touchable"
+		:elevation="elevation"
+		:color="color"
+		:state="state"
+	><span v-if="label !== ''">{{label}}</span></icon-button>
+	</div>`,
+});
+
+export const Interactive = StoryTemplate.bind({});
+
+const args = {
+	size: ICON_BUTTON_SIZES.LARGE,
+	type: ICON_BUTTON_TYPES.ICON_ONLY,
+	radius: BUTTON_RADIUSES.CAPSULE,
+	label: 'Zamknij',
+	colorScheme: ICON_BUTTON_COLOR_SCHEMES.ALL_IN_COLOR,
+	touchable: true,
+	elevation: BUTTON_ELEVATIONS.NONE,
+	color: 'primary',
+	state: ICON_BUTTON_STATES.DEFAULT,
+} as Args;
+
+const argTypes = {
+	size: {
+		control: { type: 'select', options: Object.values(ICON_BUTTON_SIZES) },
+		defaultValue: ICON_BUTTON_SIZES.LARGE,
+	},
+	type: {
+		control: { type: 'select', options: Object.values(ICON_BUTTON_TYPES) },
+		defaultValue: ICON_BUTTON_TYPES.ICON_ONLY,
+	},
+	radius: {
+		control: { type: 'select', options: Object.values(BUTTON_RADIUSES) },
+		defaultValue: BUTTON_RADIUSES.CAPSULE,
+	},
+	icon: {
+		control: { type: 'select', options: Object.keys(ICONS) },
+		defaultValue: 'FA_XMARK',
+	},
+	color: {
+		control: { type: 'select', options: Object.values(ICON_BUTTON_COLORS) },
+		defaultValue: 'primary',
+	},
+	colorScheme: {
+		control: { type: 'select', options: Object.values(ICON_BUTTON_COLOR_SCHEMES) },
+		defaultValue: ICON_BUTTON_COLOR_SCHEMES.ALL_IN_COLOR,
+	},
+	elevation: {
+		control: { type: 'select', options: Object.values(BUTTON_ELEVATIONS) },
+		defaultValue: BUTTON_ELEVATIONS.NONE,
+	},
+	state: {
+		control: { type: 'select', options: Object.values(ICON_BUTTON_STATES) },
+		defaultValue: ICON_BUTTON_STATES.DEFAULT,
+	},
+} as ArgTypes;
+
+Interactive.argTypes = argTypes;
+Interactive.args = args;
+
+Interactive.parameters = {
+	design: {
+		type: 'figma',
+		url: 'https://www.figma.com/file/izQdYyiBR1GQgFkaOIfIJI/LMS---DS---Components?node-id=180%3A2709',
+	},
+};
+
+const StoryFilledWithLabelTemplate: StoryFn<typeof IconButton> = (argTypes) => ({
+	components: { IconButton },
+	props: Object.keys(argTypes),
+	template:
+		'<icon-button :icon="ICONS.FA_XMARK" :type="ICON_BUTTON_TYPES.FILLED" >Zamknij</icon-button>',
+	created() {
+		this.ICONS = ICONS;
+		this.ICON_BUTTON_TYPES = ICON_BUTTON_TYPES;
+	},
+});
+
+export const FilledWithLabel = StoryFilledWithLabelTemplate.bind({});
+
+const StoryOutlinedRoundedTemplate: StoryFn<typeof IconButton> = (argTypes) => ({
+	components: { IconButton },
+	props: Object.keys(argTypes),
+	template:
+		'<icon-button :icon="ICONS.FA_CHEVRON_RIGHT" :type="ICON_BUTTON_TYPES.OUTLINED" :radius="BUTTON_RADIUSES.ROUNDED" />',
+	created() {
+		this.ICONS = ICONS;
+		this.ICON_BUTTON_TYPES = ICON_BUTTON_TYPES;
+		this.BUTTON_RADIUSES = BUTTON_RADIUSES;
+	},
+});
+
+export const OutlinedRounded = StoryOutlinedRoundedTemplate.bind({});
+
+const StoryFilledPrimaryTemplate: StoryFn<typeof IconButton> = (argTypes) => ({
+	components: { IconButton },
+	props: Object.keys(argTypes),
+	template:
+		'<icon-button :icon="ICONS.FA_XMARK" :type="ICON_BUTTON_TYPES.FILLED" :color="ICON_BUTTON_COLORS.PRIMARY" :size="ICON_BUTTON_SIZES.X_SMALL" />',
+	created() {
+		this.ICONS = ICONS;
+		this.ICON_BUTTON_TYPES = ICON_BUTTON_TYPES;
+		this.ICON_BUTTON_COLORS = ICON_BUTTON_COLORS;
+		this.ICON_BUTTON_SIZES = ICON_BUTTON_SIZES;
+	},
+});
+
+export const FilledPrimary = StoryFilledPrimaryTemplate.bind({});
+
+const StoryIconOnlyInfoTemplate: StoryFn<typeof IconButton> = (argTypes) => ({
+	components: { IconButton },
+	props: Object.keys(argTypes),
+	template:
+		'<icon-button :icon="ICONS.FA_FLAG" :color="ICON_BUTTON_COLORS.INFO" :size="ICON_BUTTON_SIZES.MEDIUM" />',
+	created() {
+		this.ICONS = ICONS;
+		this.ICON_BUTTON_COLORS = ICON_BUTTON_COLORS;
+		this.ICON_BUTTON_SIZES = ICON_BUTTON_SIZES;
+	},
+});
+
+export const IconOnlyInfo = StoryIconOnlyInfoTemplate.bind({});
+
+const StoryIconOnlyWarningTemplate: StoryFn<typeof IconButton> = (argTypes) => ({
+	components: { IconButton },
+	props: Object.keys(argTypes),
+	template:
+		'<icon-button :icon="ICONS.HEAD_WITH_QUESTION_MARK" :color="ICON_BUTTON_COLORS.WARNING" :size="ICON_BUTTON_SIZES.MEDIUM" />',
+	created() {
+		this.ICONS = ICONS;
+		this.ICON_BUTTON_COLORS = ICON_BUTTON_COLORS;
+		this.ICON_BUTTON_SIZES = ICON_BUTTON_SIZES;
+	},
+});
+
+export const IconOnlyWarning = StoryIconOnlyWarningTemplate.bind({});
---
+++ lib/js/components/SurveyQuestions/SurveyQuestionOpenEnded/SurveyQuestionOpenEnded.stories.ts
@@ -0,0 +1,53 @@
+import SurveyQuestionOpenEnded from './SurveyQuestionOpenEnded.vue';
+import { SURVEY_QUESTION_STATES } from '../SurveyQuestion.consts';
+
+import { Args, ArgTypes, Meta, StoryFn } from '@storybook/vue';
+
+export default {
+	title: 'Components/SurveyQuestions/SurveyQuestionOpenEnded',
+	component: SurveyQuestionOpenEnded,
+} as Meta<typeof SurveyQuestionOpenEnded>;
+
+const StoryTemplate: StoryFn<typeof SurveyQuestionOpenEnded> = (argTypes) => ({
+	components: { SurveyQuestionOpenEnded },
+	props: Object.keys(argTypes),
+	data() {
+		return { valueData: this.value };
+	},
+	watch: {
+		value() {
+			// value is a prop here, Vue don't like modifying props, so we wrap it into valueData
+			this.valueData = this.value;
+		},
+	},
+	template:
+		'<survey-question-open-ended  :title="title" v-model="valueData" :state="state" :placeholder="placeholder">' +
+		'<div v-if="explanation" slot="explanation" v-html="explanation" />' +
+		'</survey-question-open-ended>',
+});
+
+export const Interactive = StoryTemplate.bind({});
+
+const args = {
+	title: "Main question write here if it's long it will collapse.",
+	value: 'value',
+	explanation:
+		'<h3 class="-headlineL -textRegular" style="text-align: center; margin-bottom: 16px;">Jak ocenić, czy slajdy i diagramy były zrozumiałe?</h3><div>Wyczerpujące materiały dają poczucie pełnego zrozumieina, przy jednoczesnym usystematyzowaniu informacji. Wpływa na to nie tylko ich jakość, ale też ilość.</div>',
+	placeholder: 'Wpisz swoją odpowiedź',
+	state: SURVEY_QUESTION_STATES.DEFAULT,
+} as Args;
+
+const argTypes = {
+	explanation: { control: { type: 'text' } },
+	state: { control: { type: 'select', options: Object.values(SURVEY_QUESTION_STATES) } },
+} as ArgTypes;
+
+Interactive.argTypes = argTypes;
+Interactive.args = args;
+
+Interactive.parameters = {
+	design: {
+		type: 'figma',
+		url: 'https://www.figma.com/file/izQdYyiBR1GQgFkaOIfIJI/LMS---DS---Components?node-id=1555%3A41739',
+	},
+};
