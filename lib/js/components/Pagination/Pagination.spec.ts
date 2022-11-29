70efabb2b28cf4e7c66a226b8d9eaef23dc597d6
Sebastian Dawidziak <sebastian@tweetgeek.pl>
2022-11-29 14:56:17 +0100

    fix test

--- lib/js/components/Pagination/Pagination.spec.ts
+++ lib/js/components/Pagination/Pagination.spec.ts
@@ -153,6 +153,6 @@ describe('Pagination', () => {
 		},
 	])('should validate for %o', ({ props, expected }) => {
 		const component = createComponent(props);
-		expect(component.emitted()['change-page']).toEqual(expected);
+		expect(component.emitted()['change-page']).toBe(expected);
 	});
 });
