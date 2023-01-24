9991e1f2830088c2bd362027953520250c7dfd33
damianmef <damianszkudlarek@gmail.com>
2023-01-24 10:04:25 +0100

    IT-4874 - fix lint

--- lib/js/components/Icons/FeatureIcon/FeatureIcon.vue
+++ lib/js/components/Icons/FeatureIcon/FeatureIcon.vue
@@ -1,7 +1,7 @@
 <template>
-  <div
-      class="featureIcon"
-      :class="{
+	<div
+		class="featureIcon"
+		:class="{
 			'-doubleBackground': doubleBackground,
 			'-danger': color === FEATURE_ICON_COLOR.DANGER,
 			'-info': color === FEATURE_ICON_COLOR.INFO,
@@ -15,9 +15,9 @@
 			'-large': size === FEATURE_ICON_SIZES.LARGE,
 			'-x-large': size === FEATURE_ICON_SIZES.X_LARGE,
 		}"
-  >
-    <wnl-icon class="featureIcon__icon" :icon="icon" :size="iconSize" />
-  </div>
+	>
+		<wnl-icon class="featureIcon__icon" :icon="icon" :size="iconSize" />
+	</div>
 </template>
 
 <style lang="scss">
@@ -26,162 +26,157 @@
 @import '../../../../styles/settings/colors/tokens';
 
 $feature-icon-colors: (
-    'neutral': (
-        'background': $color-neutral-background-medium,
-        'border': $color-neutral-background,
-        'icon': $color-neutral-icon,
-    ),
-    'neutral-weak': (
-        'background': $color-neutral-background-medium,
-        'border': $color-neutral-background,
-        'icon': $color-neutral-icon-weak,
-    ),
-    'primary': (
-        'background': $color-primary-background-medium,
-        'border': $color-primary-background,
-        'icon': $color-primary-icon,
-    ),
-    'success': (
-        'background': $color-success-background-medium,
-        'border': $color-success-background,
-        'icon': $color-success-icon,
-    ),
-    'danger': (
-        'background': $color-danger-background-medium,
-        'border': $color-danger-background,
-        'icon': $color-danger-icon,
-    ),
-    'warning': (
-        'background': $color-warning-background-medium,
-        'border': $color-warning-background,
-        'icon': $color-warning-icon,
-    ),
-    'info': (
-        'background': $color-info-background-medium,
-        'border': $color-info-background,
-        'icon': $color-info-icon,
-    ),
+	'neutral': (
+		'background': $color-neutral-background-medium,
+		'border': $color-neutral-background,
+		'icon': $color-neutral-icon,
+	),
+	'neutral-weak': (
+		'background': $color-neutral-background-medium,
+		'border': $color-neutral-background,
+		'icon': $color-neutral-icon-weak,
+	),
+	'primary': (
+		'background': $color-primary-background-medium,
+		'border': $color-primary-background,
+		'icon': $color-primary-icon,
+	),
+	'success': (
+		'background': $color-success-background-medium,
+		'border': $color-success-background,
+		'icon': $color-success-icon,
+	),
+	'danger': (
+		'background': $color-danger-background-medium,
+		'border': $color-danger-background,
+		'icon': $color-danger-icon,
+	),
+	'warning': (
+		'background': $color-warning-background-medium,
+		'border': $color-warning-background,
+		'icon': $color-warning-icon,
+	),
+	'info': (
+		'background': $color-info-background-medium,
+		'border': $color-info-background,
+		'icon': $color-info-icon,
+	),
 );
 
 $feature-icon-padding-large: 10px;
 
 @mixin setBorderColor($border) {
-  border-color: $border;
+	border-color: $border;
 }
 
 @mixin setFeatureIconColor($self, $background, $icon) {
-  background-color: $background;
-  border-color: $background;
+	background-color: $background;
+	border-color: $background;
 
-  #{$self}__icon {
-    color: $icon;
-  }
+	#{$self}__icon {
+		color: $icon;
+	}
 }
 
 .featureIcon {
-  $self: &;
+	$self: &;
 
-  border-radius: 100%;
-  border-style: solid;
-  border-width: $space-xxxs;
-  display: inline-block;
-  padding: $space-xxxs;
+	@each $color-name, $color-map in $feature-icon-colors {
+		&.-#{$color-name} {
+			@include setFeatureIconColor(
+				$self,
+				map-get($color-map, 'background'),
+				map-get($color-map, 'icon')
+			);
+		}
+		&.-#{$color-name}.-doubleBackground {
+			@include setBorderColor(map-get($color-map, 'border'));
+		}
+	}
 
-  &.-small {
-    border-width: $space-xxxxs;
-    padding: $space-xxxxs;
-  }
+	border-radius: 100%;
+	border-style: solid;
+	border-width: $space-xxxs;
+	display: inline-block;
+	padding: $space-xxxs;
 
-  &.-large {
-    border-width: $space-xxxs;
-    padding: $feature-icon-padding-large;
-  }
+	&.-small {
+		border-width: $space-xxxxs;
+		padding: $space-xxxxs;
+	}
 
-  &.-x-large {
-    border-width: $space-xxs;
-    padding: $space-xs;
-  }
+	&.-large {
+		border-width: $space-xxxs;
+		padding: $feature-icon-padding-large;
+	}
 
-  @each $color-name, $color-map in $feature-icon-colors {
-    &.-#{$color-name} {
-      @include setFeatureIconColor(
-        $self,
-        map-get($color-map, 'background'),
-        map-get($color-map, 'icon'),
-      );
-    }
-    &.-#{$color-name}.-doubleBackground {
-      @include setBorderColor(
-        map-get($color-map, 'border'),
-      );
-    }
-  }
+	&.-x-large {
+		border-width: $space-xxs;
+		padding: $space-xs;
+	}
 }
-
 </style>
 
 <script lang="ts">
 import { VueConstructor } from 'vue';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import WnlIcon, { ICONS, ICON_SIZES } from '../Icon';
 import { FEATURE_ICON_SIZES, FEATURE_ICON_COLOR } from './FeatureIcon.consts';
 
 export default {
-  name: 'FeatureIcon',
-  components: {
-    FontAwesomeIcon,
-    WnlIcon,
-  },
-  props: {
-    icon: {
-      type: Object,
-      default: null,
-      validator(icon: VueConstructor) {
-        return Object.values(ICONS).includes(icon);
-      },
-    },
-    size: {
-      type: String,
-      default: FEATURE_ICON_SIZES.MEDIUM,
-      validator: (value: string) => Object.values(FEATURE_ICON_SIZES).includes(value),
-    },
-    color: {
-      type: String,
-      required: true,
-      validator(color) {
-        return Object.values(FEATURE_ICON_COLOR).includes(color);
-      },
-    },
-    doubleBackground: {
-      type: Boolean,
-      default: true,
-    },
-  },
-  data() {
-    return {
-      FEATURE_ICON_COLOR: Object.freeze(FEATURE_ICON_COLOR),
-      FEATURE_ICON_SIZES: Object.freeze(FEATURE_ICON_SIZES),
-    };
-  },
-  computed: {
-    sizeClassName() {
-      return `-${this.size}`;
-    },
-    iconSize(): string {
-      if (this.size === FEATURE_ICON_SIZES.SMALL) {
-        return ICON_SIZES.X_SMALL;
-      }
+	name: 'FeatureIcon',
+	components: {
+		WnlIcon,
+	},
+	props: {
+		icon: {
+			type: Object,
+			default: null,
+			validator(icon: VueConstructor) {
+				return Object.values(ICONS).includes(icon);
+			},
+		},
+		size: {
+			type: String,
+			default: FEATURE_ICON_SIZES.MEDIUM,
+			validator: (value: string) => Object.values(FEATURE_ICON_SIZES).includes(value),
+		},
+		color: {
+			type: String,
+			required: true,
+			validator(color) {
+				return Object.values(FEATURE_ICON_COLOR).includes(color);
+			},
+		},
+		doubleBackground: {
+			type: Boolean,
+			default: true,
+		},
+	},
+	data() {
+		return {
+			FEATURE_ICON_COLOR: Object.freeze(FEATURE_ICON_COLOR),
+			FEATURE_ICON_SIZES: Object.freeze(FEATURE_ICON_SIZES),
+		};
+	},
+	computed: {
+		sizeClassName() {
+			return `-${this.size}`;
+		},
+		iconSize(): string {
+			if (this.size === FEATURE_ICON_SIZES.SMALL) {
+				return ICON_SIZES.X_SMALL;
+			}
 
-      if (this.size === FEATURE_ICON_SIZES.LARGE) {
-        return ICON_SIZES.LARGE;
-      }
+			if (this.size === FEATURE_ICON_SIZES.LARGE) {
+				return ICON_SIZES.LARGE;
+			}
 
-      if (this.size === FEATURE_ICON_SIZES.X_LARGE) {
-        return ICON_SIZES.X_LARGE;
-      }
+			if (this.size === FEATURE_ICON_SIZES.X_LARGE) {
+				return ICON_SIZES.X_LARGE;
+			}
 
-      return ICON_SIZES.MEDIUM;
-    },
-  },
+			return ICON_SIZES.MEDIUM;
+		},
+	},
 };
 </script>
