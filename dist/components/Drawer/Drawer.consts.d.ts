49cf53bba4d1e63af0ceb3e4c6306ba6912bc2e3
Przemek Ferkaluk <pferkaluk@gmail.com>
2023-02-21 14:58:48 +0100

    add options modifiers and append to body

--- dist/components/Drawer/Drawer.consts.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const DRAWER_POSITIONS: {
-    readonly LEFT: "left";
-    readonly RIGHT: "right";
-};
-export declare type DrawerPosition = typeof DRAWER_POSITIONS[keyof typeof DRAWER_POSITIONS];
--- dist/lib/js/components/TagList/TagList.consts.d.ts
+++
@@ -1,9 +0,0 @@
-export declare const TAG_LIST_COLORS: {
-    NEUTRAL_WEAK: string;
-    NEUTRAL_STRONG: string;
-};
-export declare const TAG_LIST_SIZES: {
-    X_SMALL: string;
-    SMALL: string;
-    MEDIUM: string;
-};
