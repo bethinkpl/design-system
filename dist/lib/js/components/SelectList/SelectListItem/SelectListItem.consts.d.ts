b8745c2880e1e2a633e0b4d2d6a721f51c589d95
kvas-damian <kvas-damian@users.noreply.github.com>
2023-03-02 07:07:08 +0000

    Deploying to master from @ bethinkpl/design-system@8f2ec740a52058bfc613e4b34502839fb5eed551 🚀

---
+++ dist/lib/js/components/SelectList/SelectListItem/SelectListItem.consts.d.ts
@@ -0,0 +1,13 @@
+export declare const SELECT_LIST_ITEM_SELECTION_MODE: {
+    SELECT_ONLY: string;
+    TOGGLE: string;
+};
+export declare const SELECT_LIST_ITEM_SIZES: {
+    X_SMALL: string;
+    SMALL: string;
+    MEDIUM: string;
+};
+export declare const SELECT_LIST_ITEM_STATES: {
+    DEFAULT: string;
+    LOADING: string;
+};
