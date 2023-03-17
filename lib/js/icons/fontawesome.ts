1aaacb17b7ba673ce8e3d4b378683ddcfce9f5d5
Damian Jóźwiak <damian.jozwiak@bethink.pl>
2023-03-17 12:47:44 +0100

    IT-5092 | Add new FA icons

--- lib/js/icons/fontawesome.ts
+++ lib/js/icons/fontawesome.ts
@@ -21,7 +21,12 @@ import { faBook } from '@fortawesome/pro-regular-svg-icons/faBook';
 import { faBookBlank } from '@fortawesome/pro-regular-svg-icons/faBookBlank';
 import { faBookCopy } from '@fortawesome/pro-regular-svg-icons/faBookCopy';
 import { faBoxArchive } from '@fortawesome/pro-regular-svg-icons/faBoxArchive';
+import { faCalendar } from '@fortawesome/pro-regular-svg-icons/faCalendar';
 import { faCalendarDays } from '@fortawesome/pro-regular-svg-icons/faCalendarDays';
+import { faCalendarDay } from '@fortawesome/pro-regular-svg-icons/faCalendarDay';
+import { faCalendarRange } from '@fortawesome/pro-regular-svg-icons/faCalendarRange';
+import { faCalendarCircleExclamation } from '@fortawesome/pro-regular-svg-icons/faCalendarCircleExclamation';
+import { faCalendarCheck } from '@fortawesome/pro-regular-svg-icons/faCalendarCheck';
 import { faCartShopping } from '@fortawesome/pro-regular-svg-icons/faCartShopping';
 import { faChartColumn } from '@fortawesome/pro-regular-svg-icons/faChartColumn';
 import { faChartLineUp } from '@fortawesome/pro-regular-svg-icons/faChartLineUp';
@@ -113,6 +118,7 @@ import { faTag } from '@fortawesome/pro-regular-svg-icons/faTag';
 import { faTags } from '@fortawesome/pro-regular-svg-icons/faTags';
 import { faThumbsUp } from '@fortawesome/pro-regular-svg-icons/faThumbsUp';
 import { faTrashCan } from '@fortawesome/pro-regular-svg-icons/faTrashCan';
+import { faTreePalm } from '@fortawesome/pro-regular-svg-icons/faTreePalm';
 import { faTriangleExclamation } from '@fortawesome/pro-regular-svg-icons/faTriangleExclamation';
 import { faTrophyStar } from '@fortawesome/pro-regular-svg-icons/faTrophyStar';
 import { faUnlockKeyhole } from '@fortawesome/pro-regular-svg-icons/faUnlockKeyhole';
@@ -196,7 +202,12 @@ export const FONTAWESOME_ICONS = {
 	FA_BOOK_SPARKLES_SOLID: fasBookSparkles,
 	FA_BOOKMARK_SOLID: fasBookmark,
 	FA_BOX_ARCHIVE: faBoxArchive,
+	FA_CALENDAR: faCalendar,
+	FA_CALENDAR_CIRCLE_EXCLAMATION: faCalendarCircleExclamation,
+	FA_CALENDAR_CHECK: faCalendarCheck,
+	FA_CALENDAR_DAY: faCalendarDay,
 	FA_CALENDAR_DAYS: faCalendarDays,
+	FA_CALENDAR_RANGE: faCalendarRange,
 	FA_CAMERA_SOLID: fasCamera,
 	FA_CARET_SQUARE_RIGHT_SOLID: fasCaretSquareRight,
 	FA_CART_SHOPPING: faCartShopping,
@@ -317,6 +328,7 @@ export const FONTAWESOME_ICONS = {
 	FA_THUMBS_UP_SOLID: fasThumbsUp,
 	FA_THUMBTACK_SOLID: fasThumbtack,
 	FA_TRASH_CAN: faTrashCan,
+	FA_TREE_PALM: faTreePalm,
 	FA_TRIANGLE_EXCLAMATION: faTriangleExclamation,
 	FA_TRIANGLE_EXCLAMATION_SOLID: fasTriangleExclamation,
 	FA_TROPHY_STAR: faTrophyStar,
