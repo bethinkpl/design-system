602150d6f218537ac7fcb4fd6a3bd61bc75349bf
Damian Jóźwiak <damian.jozwiak@bethink.pl>
2023-04-03 12:10:15 +0200

    IT-4603 | Fix exporting svgs by vue-cli

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,8 @@ module.exports = {
 	/**
 	 * we disable parallel for production, because if we use ts-loader the Thread Loader does
 	 * not work properly and communicates it with an error:
-	 * 		Syntax Error: Thread Loader (Worker 0)
-	 * 		Cannot read properties of undefined (reading 'hooks')
+	 *        Syntax Error: Thread Loader (Worker 0)
+	 *        Cannot read properties of undefined (reading 'hooks')
 	 */
 	parallel: process.env.NODE_ENV !== 'production',
 	css: {
@@ -13,6 +13,10 @@ module.exports = {
 		const svgRule = config.module.rule('svg');
 
 		svgRule.uses.clear();
+
+		// remove properties which breaks vue-svg-loader output
+		svgRule.store.delete('generator');
+		svgRule.store.delete('type');
 
 		svgRule
 			.use('vue-loader')
