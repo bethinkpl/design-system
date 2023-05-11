b2e79f95feb85820629d13c5656b1a22f4e23962
tweetgeek <4587534+tweetgeek@users.noreply.github.com>
2023-05-11 07:38:38 +0000

    Deploying to master from @ bethinkpl/design-system@e7c0358f756b3793968bc8a75dbbf3650107c422 🚀

---
+++ dist/lib/js/components/Drawer/DrawerListItem/DrawerListItem.consts.d.ts
@@ -0,0 +1,5 @@
+export declare const DRAWER_LIST_ITEM_STATES: {
+    readonly DEFAULT: "default";
+    readonly LOADING: "loading";
+};
+export declare type DrawerListItemState = typeof DRAWER_LIST_ITEM_STATES[keyof typeof DRAWER_LIST_ITEM_STATES];
---
+++ dist/lib/js/components/Drawer/DrawerListItemGroup/DrawerListItemGroup.stories.d.ts
@@ -0,0 +1,5 @@
+import DrawerListItemGroup from './DrawerListItemGroup.vue';
+import { Meta, StoryFn } from '@storybook/vue3';
+declare const _default: Meta<typeof DrawerListItemGroup>;
+export default _default;
+export declare const Static: StoryFn<typeof DrawerListItemGroup>;
