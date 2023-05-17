717d3decd22fd6ab30f044927b9d0f6bba0113e6
Sebastian Dawidziak <sebastian@tweetgeek.pl>
2023-05-17 11:25:37 +0200

    use decorator

--- lib/js/components/Layouts/ThreeColumnLayout/ThreeColumnLayout.stories.ts
+++ lib/js/components/Layouts/ThreeColumnLayout/ThreeColumnLayout.stories.ts
@@ -6,16 +6,23 @@ import { THREE_COLUMN_LAYOUT_RIGHT_COLUMN_SIZE } from './ThreeColumnLayout.const
 export default {
 	title: 'Components/Layouts/ThreeColumnLayout',
 	component: ThreeColumnLayout,
+	decorators: [
+		(story) => ({
+			components: { story },
+			template:
+				"<div style='display: flex; width: 100vw; height: 100vh; font-size: 12px; color: #fff;'><story /></div>",
+		}),
+	],
 } as Meta<typeof ThreeColumnLayout>;
-
-const StoryTemplate: StoryFn<typeof ThreeColumnLayout> = (args) => ({
+const key = 1231;
+const StoryTemplate: StoryFn<typeof ThreeColumnLayout> = (args, context) => ({
 	components: { ThreeColumnLayout },
 	setup() {
-		return { ...args };
+		return {
+			...args,
+		};
 	},
 	template: `
-		<div
-			style='display: flex; width: 100vw; height: 100vh; font-size: 12px; color: #fff;'>
 		<three-column-layout :rightColumnSize='rightColumnSize'
 							 :rightColumnVisible='rightColumnVisible'
 							 :leftColumnVisible='leftColumnVisible'
@@ -31,11 +38,10 @@ const StoryTemplate: StoryFn<typeof ThreeColumnLayout> = (args) => ({
 			</div>
 		</template>
 
-			<div style='width: 100%; height: 100%; background: #9133c2;'>
-				content
-			</div>
-		</three-column-layout>
-		</div>`,
+		<div style='width: 100%; height: 100%; background: #9133c2;'>
+			content
+		</div>
+		</three-column-layout>`,
 });
 
 export const Interactive = StoryTemplate.bind({});
