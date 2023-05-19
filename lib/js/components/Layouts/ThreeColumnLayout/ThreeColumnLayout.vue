f246c7253bbc038c200e6b173802cf66903f6f44
Sebastian Dawidziak <sebastian@tweetgeek.pl>
2023-05-19 12:45:52 +0200

    format fix

--- lib/js/components/Layouts/ThreeColumnLayout/ThreeColumnLayout.vue
+++ lib/js/components/Layouts/ThreeColumnLayout/ThreeColumnLayout.vue
@@ -74,9 +74,9 @@ $right-column-large-l-max-width: 560px;
 		background: $color-default-overlay;
 		display: none;
 		height: 100%;
+		left: 0;
 		position: fixed;
 		top: 0;
-		left: 0;
 		width: 100%;
 		z-index: $z-index-overlay;
 
