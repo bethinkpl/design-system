359531103d258ee9e9029ee3ab99088bb7db50f4
tweetgeek <4587534+tweetgeek@users.noreply.github.com>
2023-05-23 13:44:46 +0000

    Deploying to master from @ bethinkpl/design-system@f085c90d6723c10384e77b41e678898eebc773aa 🚀

--- dist/lib/js/components/SelectList/SelectListSectionTitle/SelectListSectionTitle.vue.d.ts
+++ dist/lib/js/components/SelectList/SelectListSectionTitle/SelectListSectionTitle.vue.d.ts
@@ -1,14 +1,3 @@
-declare const _default: {
-    name: string;
-    props: {
-        label: {
-            type: StringConstructor;
-            required: boolean;
-        };
-        isUppercase: {
-            type: BooleanConstructor;
-            default: boolean;
-        };
-    };
-};
-export default _default;
+export declare function render(_ctx: any, _cache: any, $props: any, $setup: any, $data: any, $options: any): import("vue").VNode<import("vue").RendererNode, import("vue").RendererElement, {
+    [key: string]: any;
+}>;
