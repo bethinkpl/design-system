44825f46f83471e549ecfac4dc3338d65786e6de
rogatty <7030884+rogatty@users.noreply.github.com>
2023-08-08 12:45:52 +0000

    Deploying to master from @ bethinkpl/design-system@52e014beb2e0526a290615e69a18573a6efcca4b 🚀

--- dist/lib/js/components/SelectList/SelectListItemToggle/SelectListItemToggle.vue.d.ts
+++ dist/lib/js/components/SelectList/SelectListItemToggle/SelectListItemToggle.vue.d.ts
@@ -1,3 +1,57 @@
-export declare function render(_ctx: any, _cache: any, $props: any, $setup: any, $data: any, $options: any): import("vue").VNode<import("vue").RendererNode, import("vue").RendererElement, {
-    [key: string]: any;
-}>;
+import { IconItem } from '../../Icons/Icon';
+import SelectListItem from '../SelectListItem/SelectListItem.vue';
+declare const _default: {
+    name: string;
+    components: {
+        SelectListItem: typeof SelectListItem;
+    };
+    props: {
+        iconOff: {
+            type: ObjectConstructor;
+            default: null;
+            validator(icon: any): boolean;
+        };
+        iconOn: {
+            type: ObjectConstructor;
+            default: null;
+            validator(icon: any): boolean;
+        };
+        isOn: {
+            type: BooleanConstructor;
+            default: boolean;
+        };
+        labelOff: {
+            type: StringConstructor;
+            required: boolean;
+        };
+        labelOn: {
+            type: StringConstructor;
+            required: boolean;
+        };
+        size: {
+            type: StringConstructor;
+            default: string;
+            validator(size: any): boolean;
+        };
+        state: {
+            type: StringConstructor;
+            default: string;
+            validator(size: any): boolean;
+        };
+    };
+    data(): {
+        SELECT_LIST_ITEM_SELECTION_MODE: Readonly<{
+            SELECT_ONLY: string;
+            TOGGLE: string;
+        }>;
+        SELECT_LIST_ITEM_STATES: Readonly<{
+            DEFAULT: string;
+            LOADING: string;
+        }>;
+    };
+    computed: {
+        icon(): IconItem | null;
+        label(): string;
+    };
+};
+export default _default;
