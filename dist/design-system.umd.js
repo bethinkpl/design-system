d96ebba9d2817ab4b4d19538af1a6f40618e2603
kvas-damian <kvas-damian@users.noreply.github.com>
2023-09-01 13:39:58 +0000

    Deploying to master from @ bethinkpl/design-system@10b24602e02c0490fd78a400a8cf1f7219461802 🚀

--- dist/design-system.umd.js
+++ dist/design-system.umd.js
@@ -899,6 +899,43 @@ exports.aliases = aliases;
 
 /***/ }),
 
+/***/ 42561:
+/***/ ((__unused_webpack_module, exports) => {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+var prefix = 'far';
+var iconName = 'box';
+var width = 448;
+var height = 512;
+var aliases = [128230];
+var unicode = 'f466';
+var svgPathData = 'M342.4 80H248v80H388.4L357 89.5c-2.6-5.8-8.3-9.5-14.6-9.5zM400 208H48V416c0 8.8 7.2 16 16 16H384c8.8 0 16-7.2 16-16V208zM59.6 160H200V80H105.6c-6.3 0-12.1 3.7-14.6 9.5L59.6 160zM342.4 32c25.3 0 48.2 14.9 58.5 38l41.6 93.6c3.6 8.2 5.5 17 5.5 26V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V189.6c0-9 1.9-17.8 5.5-26L47.1 70c10.3-23.1 33.2-38 58.5-38H342.4z';
+
+exports.definition = {
+  prefix: prefix,
+  iconName: iconName,
+  icon: [
+    width,
+    height,
+    aliases,
+    unicode,
+    svgPathData
+  ]};
+
+exports.faBox = exports.definition;
+exports.prefix = prefix;
+exports.iconName = iconName;
+exports.width = width;
+exports.height = height;
+exports.ligatures = aliases;
+exports.unicode = unicode;
+exports.svgPathData = svgPathData;
+exports.aliases = aliases;
+
+/***/ }),
+
 /***/ 81197:
 /***/ ((__unused_webpack_module, exports) => {
 
@@ -9389,6 +9426,7 @@ const faBellSlash_1 = __webpack_require__(17537);
 const faBook_1 = __webpack_require__(14104);
 const faBookBlank_1 = __webpack_require__(29311);
 const faBookCopy_1 = __webpack_require__(4274);
+const faBox_1 = __webpack_require__(42561);
 const faBoxArchive_1 = __webpack_require__(81197);
 const faCalendar_1 = __webpack_require__(74740);
 const faCalendarDays_1 = __webpack_require__(72316);
@@ -9573,6 +9611,7 @@ exports.FONTAWESOME_ICONS = {
     FA_BOOK_COPY: faBookCopy_1.faBookCopy,
     FA_BOOK_SPARKLES_SOLID: faBookSparkles_1.faBookSparkles,
     FA_BOOKMARK_SOLID: faBookmark_1.faBookmark,
+    FA_BOX: faBox_1.faBox,
     FA_BOX_ARCHIVE: faBoxArchive_1.faBoxArchive,
     FA_CALENDAR: faCalendar_1.faCalendar,
     FA_CALENDAR_CIRCLE_EXCLAMATION: faCalendarCircleExclamation_1.faCalendarCircleExclamation,
--- dist/lib/js/components/Drawer/DrawerSection/DrawerSection.vue.d.ts
+++ dist/lib/js/components/Drawer/DrawerSection/DrawerSection.vue.d.ts
@@ -70,6 +70,7 @@ declare const _default: {
             readonly FA_BOOK_COPY: import("@fortawesome/fontawesome-common-types").IconDefinition;
             readonly FA_BOOK_SPARKLES_SOLID: import("@fortawesome/fontawesome-common-types").IconDefinition;
             readonly FA_BOOKMARK_SOLID: import("@fortawesome/fontawesome-common-types").IconDefinition;
+            readonly FA_BOX: import("@fortawesome/fontawesome-common-types").IconDefinition;
             readonly FA_BOX_ARCHIVE: import("@fortawesome/fontawesome-common-types").IconDefinition;
             readonly FA_CALENDAR: import("@fortawesome/fontawesome-common-types").IconDefinition;
             readonly FA_CALENDAR_CIRCLE_EXCLAMATION: import("@fortawesome/fontawesome-common-types").IconDefinition;
