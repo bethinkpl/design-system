b051caf1f7b84042bc53dacfac7bfe7a1c2b343f
Damian Jóźwiak <damian.jozwiak@bethink.pl>
2023-10-04 07:20:44 +0200

    IT-5395 | Use shared tile props

--- lib/js/components/SelectList/SelectListItemTile/SelectListItemTile.vue
+++ lib/js/components/SelectList/SelectListItemTile/SelectListItemTile.vue
@@ -25,73 +25,14 @@
 </style>
 
 <script lang="ts">
-import Tile, { TILE_COLORS, TILE_STATES } from '../../Tile';
-import { ICONS } from '../../Icons/Icon';
-import { toRaw } from 'vue';
-import { Value } from 'utils/type.utils';
+import Tile from '../../Tile';
+import { props } from '../../Tile/Tile.shared';
 
 export default {
 	name: 'SelectListItemTile',
 	components: {
 		DsTile: Tile,
 	},
-	props: {
-		interactive: {
-			type: Boolean,
-			default: false,
-		},
-		iconLeft: {
-			type: Object,
-			default: null,
-			validator(iconLeft) {
-				return Object.values(ICONS).includes(toRaw(iconLeft));
-			},
-		},
-		iconRight: {
-			type: Object,
-			default: null,
-			validator(iconRight) {
-				return Object.values(ICONS).includes(toRaw(iconRight));
-			},
-		},
-		text: {
-			type: String,
-			required: true,
-		},
-		eyebrowText: {
-			type: String,
-			required: true,
-		},
-		additionalText: {
-			type: String,
-			default: null,
-		},
-		color: {
-			type: String,
-			default: TILE_COLORS.NEUTRAL,
-			validator(color) {
-				return Object.values(TILE_COLORS).includes(color);
-			},
-		},
-		isEyebrowTextUppercase: {
-			type: Boolean,
-			default: false,
-		},
-		state: {
-			type: String,
-			default: TILE_STATES.DEFAULT,
-			validator(value: Value<typeof TILE_STATES>) {
-				return Object.values(TILE_STATES).includes(value);
-			},
-		},
-		eyebrowEllipsis: {
-			type: Boolean,
-			default: true,
-		},
-		textEllipsis: {
-			type: Boolean,
-			default: true,
-		},
-	},
+	props,
 };
 </script>
