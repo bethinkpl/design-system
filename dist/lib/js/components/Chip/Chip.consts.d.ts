b43e5ac5289464fcb9381ce82a224d688bfa1f00
Piotr Ziółkowski <piotrziolkowski@MacBook-Pro-Piotr.local>
2024-02-14 15:15:22 +0100

    IT-5590 add builded Chip

---
+++ dist/lib/js/components/Chip/Chip.consts.d.ts
@@ -0,0 +1,22 @@
+export declare const PILL_SIZES: {
+    readonly X_SMALL: "x-small";
+    readonly SMALL: "small";
+};
+export declare const PILL_COLORS: {
+    readonly PRIMARY: "primary";
+    readonly PRIMARY_STRONG: "primaryStrong";
+    readonly NEUTRAL: "neutral";
+    readonly FAIL: "fail";
+    readonly SUCCESS: "success";
+    readonly INVERTED: "inverted";
+    readonly WARNING: "warning";
+    readonly INFO: "info";
+};
+export declare const PILL_STATES: {
+    readonly DEFAULT: "default";
+    readonly DISABLED: "disabled";
+};
+export declare const PILL_RADIUSES: {
+    readonly CAPSULE: "capsule";
+    readonly ROUNDED: "rounded";
+};
---
+++ dist/lib/js/components/Chip/Chip.stories.d.ts
@@ -0,0 +1,5 @@
+import Chip from './Chip.vue';
+import { Meta, StoryFn } from '@storybook/vue3';
+declare const _default: Meta<typeof Chip>;
+export default _default;
+export declare const Interactive: StoryFn<typeof Chip>;
