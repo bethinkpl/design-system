71b5a71019eb6f5f13ae74ba9df56b3ed103f229
fraszczakszymon <fraszczakszymon@users.noreply.github.com>
2024-03-06 07:50:16 +0000

    Build dist

--- dist/lib/js/components/Tile/Tile.vue.d.ts
+++ dist/lib/js/components/Tile/Tile.vue.d.ts
@@ -135,6 +135,7 @@ declare const _default: {
             readonly FA_COMMENTS_QUESTION: import("@fortawesome/fontawesome-common-types").IconDefinition;
             readonly FA_COMMENTS_QUESTION_CHECK: import("@fortawesome/fontawesome-common-types").IconDefinition;
             readonly FA_COMPRESS: import("@fortawesome/fontawesome-common-types").IconDefinition;
+            readonly FA_COPY: import("@fortawesome/fontawesome-common-types").IconDefinition;
             readonly FA_DOT_CIRCLE_SOLID: import("@fortawesome/fontawesome-common-types").IconDefinition;
             readonly FA_EARTH_AMERICAS: import("@fortawesome/fontawesome-common-types").IconDefinition;
             readonly FA_ELLIPSIS: import("@fortawesome/fontawesome-common-types").IconDefinition;
