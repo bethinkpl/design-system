c7abd705578bb54e16326f9a17f42e1597f41f53
tweetgeek <4587534+tweetgeek@users.noreply.github.com>
2024-04-04 13:58:04 +0000

    Deploying to master from @ bethinkpl/design-system@938a1bfcbd2648393cc38d72d988055b43945e00 🚀

--- dist/lib/js/components/RichList/RichListItem/RichListItem.consts.d.ts
+++ dist/lib/js/components/RichList/RichListItem/RichListItem.consts.d.ts
@@ -13,21 +13,6 @@ export declare const RICH_LIST_ITEM_SIZE: {
     readonly MEDIUM: "medium";
 };
 export declare type RichListItemSize = typeof RICH_LIST_ITEM_SIZE[keyof typeof RICH_LIST_ITEM_SIZE];
-export declare const RICH_LIST_ITEM_ICON_COLOR: {
-    readonly DEFAULT: "default";
-    readonly INVERTED: "inverted";
-    readonly PRIMARY: "primary";
-    readonly PRIMARY_WEAK: "primary-weak";
-    readonly NEUTRAL: "neutral";
-    readonly NEUTRAL_WEAK: "neutral-weak";
-    readonly DANGER: "danger";
-    readonly FAIL: "fail";
-    readonly WARNING: "warning";
-    readonly SUCCESS: "success";
-    readonly INFO: "info";
-    readonly ACCENT: "accent";
-};
-export declare type RichListItemIconColor = typeof RICH_LIST_ITEM_ICON_COLOR[keyof typeof RICH_LIST_ITEM_ICON_COLOR];
 export declare const RICH_LIST_ITEM_BORDER_COLOR: {
     readonly DEFAULT: "default";
     readonly PRIMARY: "primary";
@@ -51,3 +36,12 @@ export declare const RICH_LIST_ITEM_BORDER_COLOR: {
     readonly ACCENT_WEAK: "accent-weak";
 };
 export declare type RichListItemBorderColor = typeof RICH_LIST_ITEM_BORDER_COLOR[keyof typeof RICH_LIST_ITEM_BORDER_COLOR];
+export declare const RICH_LIST_ITEM_BACKGROUND_COLOR: {
+    readonly NEUTRAL: "neutral";
+    readonly NEUTRAL_WEAK: "neutral-weak";
+};
+export declare type RichListItemBackgroundColor = typeof RICH_LIST_ITEM_BACKGROUND_COLOR[keyof typeof RICH_LIST_ITEM_BACKGROUND_COLOR];
+export declare const RICH_LIST_ITEM_ELEVATION: {
+    readonly SMALL: "small";
+};
+export declare type RichListItemElevation = typeof RICH_LIST_ITEM_ELEVATION[keyof typeof RICH_LIST_ITEM_ELEVATION];
