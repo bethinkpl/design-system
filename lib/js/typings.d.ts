9c9b56304c445aae845f2523cd327a0eab83be25
rogatty <7030884+rogatty@users.noreply.github.com>
2024-05-27 09:04:49 +0200

    Fix vue typings

--- lib/js/typings.d.ts
+++ lib/js/typings.d.ts
@@ -1,15 +1,12 @@
 // TypeScript compiler doesn't read webpack config so we need to tell him how interpret import Vue files
 
 declare module '*.vue' {
-	import { ConcreteComponent } from '@vue/runtime-dom';
+	import { ConcreteComponent } from 'vue';
 	export { ConcreteComponent };
 }
 
 declare module '*.svg' {
-	import { CompatVue } from '@vue/runtime-dom';
-	const Vue: CompatVue;
-	export default Vue;
-	export * from '@vue/runtime-dom';
-	const { configureCompat } = Vue;
-	export { configureCompat };
+	import Vue, { VueConstructor } from 'vue';
+	const content: VueConstructor<Vue>;
+	export default content;
 }
