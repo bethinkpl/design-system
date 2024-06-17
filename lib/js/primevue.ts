6411027e7687349cbdfeffbbd8647c40b531c5ab
Sebastian Dawidziak <sebastian@tweetgeek.pl>
2024-06-17 13:18:57 +0200

    install prime theme

--- lib/js/primevue.ts
+++ lib/js/primevue.ts
@@ -1,56 +1,11 @@
 import PrimeVue from 'primevue/config';
-import Aura from 'primevue/themes/aura';
+import Aura from '@primevue/themes/aura';
 import Tooltip from 'primevue/tooltip';
-import { definePreset } from 'primevue/themes/actions';
 
 export const initializePrimeVue = (app) => {
 	app.use(PrimeVue, {
 		theme: {
-			preset: definePreset(Aura, {
-				semantic: {
-					primary: {
-						50: '{raw.green.50}',
-						100: '{raw.green.100}',
-						200: '{raw.green.200}',
-						300: '{raw.green.300}',
-						400: '{raw.green.400}',
-						500: '{color-primary-icon}',
-						600: '{raw.green.600}',
-						700: '{raw.green.700}',
-						800: '{raw.green.800}',
-						900: '{raw.green.900}',
-						950: '{raw.green.900}',
-					},
-				},
-				components: {
-					tooltip: {
-						colorScheme: {
-							light: {
-								root: {
-									background: '{surface.0}',
-									color: '{surface.700}',
-								},
-								subtitle: {
-									color: '{surface.500}',
-								},
-							},
-							dark: {
-								root: {
-									background: '{surface.900}',
-									color: '{surface.0}',
-								},
-								subtitle: {
-									color: '{surface.400}',
-								},
-							},
-						},
-					},
-				},
-			}),
-			options: {
-				darkModeSelector: '.my-app-dark',
-				prefix: '',
-			},
+			preset: Aura,
 		},
 	});
 
