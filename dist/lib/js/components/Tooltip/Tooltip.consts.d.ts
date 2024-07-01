946879d063d2b25b54438f168ee81293329b9995
tweetgeek <4587534+tweetgeek@users.noreply.github.com>
2024-07-01 10:06:56 +0000

    Deploying to master from @ bethinkpl/design-system@0926a63ee907026a3611be792cdddec33ec66718 🚀

---
+++ dist/lib/js/components/Tooltip/Tooltip.consts.d.ts
@@ -0,0 +1,7 @@
+export declare const TOOLTIP_PLACEMENTS: {
+    readonly TOP: "top";
+    readonly BOTTOM: "bottom";
+    readonly LEFT: "left";
+    readonly RIGHT: "right";
+};
+export declare type TooltipPlacement = typeof TOOLTIP_PLACEMENTS[keyof typeof TOOLTIP_PLACEMENTS];
--- dist/lib/js/index.d.ts
+++ dist/lib/js/index.d.ts
@@ -95,4 +95,7 @@ export { default as BasicRichListItem } from './components/RichList/BasicRichLis
 export { default as GroupRichListItem } from './components/RichList/GroupRichListItem';
 export { default as Switch } from './components/Switch';
 export * from './components/Switch/Switch.consts';
+export { default as DsTooltip } from './components/Tooltip';
+export * from './components/Tooltip/Tooltip.consts';
+export { initializePrimeVue } from './primevue';
 export { initialize as initializeIcons, initializeWithDomWatcher as initializeIconsWithDomWatcher, IconDefinition, } from './icons/fontawesome';
