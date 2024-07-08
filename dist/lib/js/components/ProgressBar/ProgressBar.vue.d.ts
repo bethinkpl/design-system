26ffd8cf3bb057107823363806bcc1bfa5317c97
rogatty <7030884+rogatty@users.noreply.github.com>
2024-07-08 14:35:43 +0000

    Deploying to master from @ bethinkpl/design-system@c725ec2fde17ee3e79622608d0b7db260638c965 🚀

--- dist/lib/js/components/ProgressBar/ProgressBar.vue.d.ts
+++ dist/lib/js/components/ProgressBar/ProgressBar.vue.d.ts
@@ -101,6 +101,7 @@ declare const _default: {
             readonly FA_ARROW_RIGHT_SOLID: import("@fortawesome/fontawesome-common-types").IconDefinition;
             readonly FA_ARROW_ROTATE_LEFT: import("@fortawesome/fontawesome-common-types").IconDefinition;
             readonly FA_ARROW_UP_SOLID: import("@fortawesome/fontawesome-common-types").IconDefinition;
+            readonly FA_ARROW_UP_RIGHT_AND_ARROW_DOWN_LEFT_FROM_CENTER: import("@fortawesome/fontawesome-common-types").IconDefinition;
             readonly FA_ARROW_UP_RIGHT_FROM_SQUARE: import("@fortawesome/fontawesome-common-types").IconDefinition;
             readonly FA_ARROWS_LEFT_RIGHT_SOLID: import("@fortawesome/fontawesome-common-types").IconDefinition;
             readonly FA_ARROWS_MAXIMIZE: import("@fortawesome/fontawesome-common-types").IconDefinition;
