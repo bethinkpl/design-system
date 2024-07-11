fe7bb560bcfc868bb56b8728a88c0b59e7d8edf9
Sebastian Dawidziak <sebastian@tweetgeek.pl>
2024-07-11 13:07:13 +0200

    fix locale

--- lib/js/components/DatePickers/DatePicker/DatePicker.vue
+++ lib/js/components/DatePickers/DatePicker/DatePicker.vue
@@ -244,13 +244,14 @@ export default {
 		if (this.isDatePickerEnabled && !this.datePicker) {
 			if (!flatpickr) {
 				const { default: flatpickrFunction } = await import('flatpickr');
-				const { Polish } = await import('flatpickr/dist/l10n/pl.js');
 
 				flatpickr = flatpickrFunction;
-				flatpickr.localize(Polish);
 			}
 
+			const { Polish } = await import('flatpickr/dist/l10n/pl.js');
+
 			this.datePicker = flatpickr(this.$refs.flatpickrInput, {
+				locale: Polish,
 				positionElement: this.$el,
 				ignoredFocusElements: [this.$el],
 				appendTo: this.$el,
