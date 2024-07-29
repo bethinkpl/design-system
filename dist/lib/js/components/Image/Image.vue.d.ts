7feaec60e7adc4eca0f9e9d3ab3883a3078f4e80
rogatty <7030884+rogatty@users.noreply.github.com>
2024-07-29 12:28:53 +0000

    Deploying to master from @ bethinkpl/design-system@f9d52a89992d5ea44d89a230c0ed2b635290171e 🚀

---
+++ dist/lib/js/components/Image/Image.vue.d.ts
@@ -0,0 +1,43 @@
+import { PropType } from 'vue';
+declare const _default: import("vue").DefineComponent<{
+    accelerate: {
+        type: BooleanConstructor;
+        default: boolean;
+    };
+    fit: {
+        type: PropType<string>;
+        default: string;
+    };
+    src: {
+        type: StringConstructor;
+        required: true;
+    };
+}, unknown, {
+    isLoading: boolean;
+    IMAGE_FITS: Readonly<{
+        CONTAIN: string;
+        COVER: string;
+    }>;
+    SKELETON_RADIUS_SIZES: Readonly<{
+        SMALL: string;
+        MEDIUM: string;
+        LARGE: string;
+    }>;
+}, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {}, string, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<import("vue").ExtractPropTypes<{
+    accelerate: {
+        type: BooleanConstructor;
+        default: boolean;
+    };
+    fit: {
+        type: PropType<string>;
+        default: string;
+    };
+    src: {
+        type: StringConstructor;
+        required: true;
+    };
+}>>, {
+    fit: string;
+    accelerate: boolean;
+}>;
+export default _default;
---
+++ dist/lib/js/components/Skeleton/Skeleton.consts.d.ts
@@ -0,0 +1,6 @@
+export declare const SKELETON_RADIUS_SIZES: {
+    SMALL: string;
+    MEDIUM: string;
+    LARGE: string;
+};
+export declare type SkeletonRadiusSize = typeof SKELETON_RADIUS_SIZES[keyof typeof SKELETON_RADIUS_SIZES];
