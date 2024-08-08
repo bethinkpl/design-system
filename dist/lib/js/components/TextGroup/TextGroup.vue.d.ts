f51518deaa71cc6bf17ea828bd1c5f25f4587b67
fraszczakszymon <fraszczakszymon@users.noreply.github.com>
2024-08-08 06:24:01 +0000

    Build dist

---
+++ dist/lib/js/components/TextGroup/TextGroup.vue.d.ts
@@ -0,0 +1,139 @@
+import { PropType } from 'vue';
+import { TextGroupState } from './TextGroup.consts';
+declare const _default: import("vue").DefineComponent<{
+    size: {
+        type: PropType<string>;
+        default: string;
+    };
+    color: {
+        type: PropType<"neutral">;
+        default: "neutral";
+    };
+    eyebrowText: {
+        type: PropType<string | null>;
+        default: null;
+    };
+    eyebrowTextEllipsis: {
+        type: BooleanConstructor;
+        default: boolean;
+    };
+    isEyebrowTextUppercase: {
+        type: BooleanConstructor;
+        default: boolean;
+    };
+    mainText: {
+        type: PropType<string | null>;
+        default: null;
+    };
+    mainTextEllipsis: {
+        type: BooleanConstructor;
+        default: boolean;
+    };
+    supportingText: {
+        type: PropType<string | null>;
+        default: null;
+    };
+    supportingTextEllipsis: {
+        type: BooleanConstructor;
+        default: boolean;
+    };
+    isInteractive: {
+        type: BooleanConstructor;
+        default: boolean;
+    };
+    skeletonLoadingSize: {
+        type: PropType<string>;
+        default: string;
+    };
+    isSelected: {
+        type: BooleanConstructor;
+        default: boolean;
+    };
+    state: {
+        type: PropType<TextGroupState>;
+        default: "default";
+    };
+}, unknown, {
+    TEXT_GROUP_SIZES: Readonly<{
+        SMALL: string;
+        MEDIUM: string;
+    }>;
+    TEXT_GROUP_STATES: Readonly<{
+        readonly DEFAULT: "default";
+        readonly HOVERED: "hovered";
+        readonly DISABLED: "disabled";
+        readonly LOADING: "loading";
+    }>;
+}, {
+    colorClassName(): string;
+    isLoading(): boolean;
+    loadingSizeClassName(): string;
+}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {}, string, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<import("vue").ExtractPropTypes<{
+    size: {
+        type: PropType<string>;
+        default: string;
+    };
+    color: {
+        type: PropType<"neutral">;
+        default: "neutral";
+    };
+    eyebrowText: {
+        type: PropType<string | null>;
+        default: null;
+    };
+    eyebrowTextEllipsis: {
+        type: BooleanConstructor;
+        default: boolean;
+    };
+    isEyebrowTextUppercase: {
+        type: BooleanConstructor;
+        default: boolean;
+    };
+    mainText: {
+        type: PropType<string | null>;
+        default: null;
+    };
+    mainTextEllipsis: {
+        type: BooleanConstructor;
+        default: boolean;
+    };
+    supportingText: {
+        type: PropType<string | null>;
+        default: null;
+    };
+    supportingTextEllipsis: {
+        type: BooleanConstructor;
+        default: boolean;
+    };
+    isInteractive: {
+        type: BooleanConstructor;
+        default: boolean;
+    };
+    skeletonLoadingSize: {
+        type: PropType<string>;
+        default: string;
+    };
+    isSelected: {
+        type: BooleanConstructor;
+        default: boolean;
+    };
+    state: {
+        type: PropType<TextGroupState>;
+        default: "default";
+    };
+}>>, {
+    color: "neutral";
+    size: string;
+    state: TextGroupState;
+    isInteractive: boolean;
+    eyebrowText: string | null;
+    supportingText: string | null;
+    isSelected: boolean;
+    supportingTextEllipsis: boolean;
+    isEyebrowTextUppercase: boolean;
+    eyebrowTextEllipsis: boolean;
+    mainText: string | null;
+    mainTextEllipsis: boolean;
+    skeletonLoadingSize: string;
+}>;
+export default _default;
