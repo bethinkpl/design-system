bd6a6bf44b005f66b33616fedda8194cd8a63de5
Szymon Frąszczak <fraszczak.szymon@gmail.com>
2024-08-21 11:49:10 +0200

    IT-5903 Set tooltip z-index

--- lib/js/primevue.ts
+++ lib/js/primevue.ts
@@ -10,6 +10,9 @@ export const initializePrimeVue = (app) => {
 				darkModeSelector: '.app-dark', // enabling dark mode in storybook makes all components canvas black
 			},
 		},
+		zIndex: {
+			tooltip: 16777271,
+		}
 	});
 
 	app.directive('pv-tooltip', Tooltip);
