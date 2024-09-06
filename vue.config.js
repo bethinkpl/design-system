5def606ad2ab43a905197bdf231009cf4035c50f
Szymon Frąszczak <fraszczak.szymon@gmail.com>
2024-09-06 11:24:36 +0200

    Lint fix

--- vue.config.js
+++ vue.config.js
@@ -21,10 +21,7 @@ module.exports = {
 	},
 	configureWebpack: (config) => {
 		config.externals = [/^@?primevue\/.*$/];
-		config.plugins = [
-			...config.plugins,
-			new ForkTsCheckerWebpackPlugin(),
-		];
+		config.plugins = [...config.plugins, new ForkTsCheckerWebpackPlugin()];
 	},
 	chainWebpack: (config) => {
 		/**
