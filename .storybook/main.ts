a77c4520b92ffb1f753226356458b5b55c29af72
Szymon Frąszczak <fraszczak.szymon@gmail.com>
2024-10-03 08:12:25 +0200

    IT-5852 Move plugin to separate file and add comments

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,5 @@
 import { StorybookConfig } from '@storybook/vue3-vite';
-import fs from 'fs';
-import path from 'path';
-import sass from 'sass';
-
-const cssFilePath = path.resolve(__dirname, '../public/storybook/preview.css');
-const previewStylesPath = path.resolve(__dirname, '../.storybook/preview.scss');
+import { scssPreviewStylesPlugin } from './scss-preview-styles-plugin';
 
 const isProductionMode = () => {
 	return process.env.NODE_ENV === 'production';
@@ -28,39 +23,9 @@ const config: StorybookConfig = {
 	],
 	framework: '@storybook/vue3-vite',
 	viteFinal: (config) => {
-		config.plugins.push(
-			{
-				name: 'scss-preview-styles',
-				buildStart() {
-					if (isProductionMode()) {
-						const result = sass.compile(previewStylesPath);
-
-						if (!fs.existsSync(path.resolve(__dirname, '../public/storybook'))) {
-							fs.mkdirSync(path.resolve(__dirname, '../public/storybook'), { recursive: true });
-						}
-
-						fs.writeFileSync(cssFilePath, result.css.toString(),);
-					}
-				},
-				configureServer(server) {
-					server.middlewares.use((req, res, next) => {
-						if (req.originalUrl === '/preview.css' && !isProductionMode()) {
-							res.setHeader('Content-Type', 'text/css');
-							res.end(sass.compile(previewStylesPath).css);
-						} else {
-							next();
-						}
-					});
-				},
-				handleHotUpdate({ file, server }) {
-					if (file.endsWith('.scss')) {
-						server.ws.send({
-							type: 'full-reload',
-						});
-					}
-				},
-			}
-		);
+		config.plugins.push(scssPreviewStylesPlugin({
+			isProductionMode: isProductionMode(),
+		}));
 
 		return {
 			...config,
