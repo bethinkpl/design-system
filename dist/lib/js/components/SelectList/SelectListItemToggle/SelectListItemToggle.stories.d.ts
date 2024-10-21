e2854c2a4f7b3f4cb5711a921951fc21b78ee356
fraszczakszymon <fraszczakszymon@users.noreply.github.com>
2024-10-21 07:41:43 +0000

    Build dist

--- dist/lib/js/components/SelectList/SelectListItemToggle/SelectListItemToggle.stories.d.ts
+++
@@ -1,5 +0,0 @@
-import SelectListItemToggle from './SelectListItemToggle.vue';
-import { Meta, StoryFn } from '@storybook/vue3';
-declare const _default: Meta<typeof SelectListItemToggle>;
-export default _default;
-export declare const Interactive: StoryFn<typeof SelectListItemToggle>;
