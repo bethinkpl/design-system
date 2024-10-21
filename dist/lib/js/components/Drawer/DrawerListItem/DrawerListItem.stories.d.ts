b4a6884e06fecada649d74040183b21bc06c0216
fraszczakszymon <fraszczakszymon@users.noreply.github.com>
2024-10-21 12:01:28 +0000

    Build dist

--- dist/lib/js/components/Drawer/DrawerListItem/DrawerListItem.stories.d.ts
+++
@@ -1,5 +0,0 @@
-import DrawerListItem from './DrawerListItem.vue';
-import { Meta, StoryFn } from '@storybook/vue3';
-declare const _default: Meta<typeof DrawerListItem>;
-export default _default;
-export declare const Interactive: StoryFn<typeof DrawerListItem>;
