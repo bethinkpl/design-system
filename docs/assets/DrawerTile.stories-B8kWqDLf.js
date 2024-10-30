aa021677726a8d36f5a0fc169cd9599ffa921843
pioziol123 <pioziol@gmail.com>
2024-10-30 09:02:59 +0000

    Deploying to master from @ bethinkpl/design-system@2f5a3d32d2c3c4560744cd4a29320bc1652923ec 🚀

---
+++ docs/assets/DrawerTile.stories-B8kWqDLf.js
@@ -0,0 +1,10 @@
+import{D as s}from"./DrawerTile-BJkMh2fU.js";import{a as n,b as p,t as m,d as i}from"./Tile.sb.shared-DAh00AIu.js";import"./Tile-DXVTBx8j.js";import"./Icon-6eu_szpS.js";import"./vue.esm-bundler-DiF7B9b7.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";const D={title:"Components/Drawer/DrawerTile",component:s},c=o=>({components:{DrawerTile:s},setup(){return o},template:m("drawer-tile"),data:i}),e=c.bind({});e.argTypes=n;e.args=p;e.parameters={design:{type:"figma",url:"https://www.figma.com/file/izQdYyiBR1GQgFkaOIfIJI/LMS---DS-Components?node-id=6785-111025&t=pzyp7YuJcnGNBfS5-4"}};var r,t,a;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`args => ({
+  components: {
+    DrawerTile
+  },
+  setup() {
+    return args;
+  },
+  template: template('drawer-tile'),
+  data
+})`,...(a=(t=e.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const T=["Interactive"];export{e as Interactive,T as __namedExportsOrder,D as default};
---
+++ docs/assets/Tile.stories-xIzW0_NG.js
@@ -0,0 +1,14 @@
+import{b as a,T as l}from"./Tile-DXVTBx8j.js";import{d,t as T,a as x,b as m}from"./Tile.sb.shared-DAh00AIu.js";import{w as g}from"./decorator-CSnIf-k5.js";import"./Icon-6eu_szpS.js";import"./vue.esm-bundler-DiF7B9b7.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./v4-BZawosSj.js";const E={title:"Components/Tile",component:a,decorators:[g],render:p=>({components:{Tile:a},setup(){return p},data:d,template:T("tile")}),argTypes:x,parameters:{actions:{handles:["click"]},design:{type:"figma",url:"https://www.figma.com/file/izQdYyiBR1GQgFkaOIfIJI/LMS---DS---Components?node-id=1923%3A34378"}}},t={args:m},e={args:{...m,interactive:!1,iconLeft:null,iconRight:null,text:"this is a text text",eyebrowText:"this is an eyebrowText text",additionalText:"this is some additionalText",state:l.DEFAULT}};var s,r,i;t.parameters={...t.parameters,docs:{...(s=t.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args
+}`,...(i=(r=t.parameters)==null?void 0:r.docs)==null?void 0:i.source}}};var o,n,c;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    ...args,
+    interactive: false,
+    iconLeft: null,
+    iconRight: null,
+    text: 'this is a text text',
+    eyebrowText: 'this is an eyebrowText text',
+    additionalText: 'this is some additionalText',
+    state: TILE_STATES.DEFAULT
+  } as Args
+}`,...(c=(n=e.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};const I=["Interactive","Static"];export{t as Interactive,e as Static,I as __namedExportsOrder,E as default};
