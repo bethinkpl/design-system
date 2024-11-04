616b3211d2fe2673d3d146b550ca78152b5555b5
rogatty <rogatty@users.noreply.github.com>
2024-11-04 14:40:44 +0000

    Build dist

--- dist/lib/js/components/TabItem/TabItem.stories.d.ts
+++
@@ -1,5 +0,0 @@
-import TabItem from './TabItem.vue';
-import { Meta, StoryFn } from '@storybook/vue3';
-declare const _default: Meta<typeof TabItem>;
-export default _default;
-export declare const Interactive: StoryFn<typeof TabItem>;
