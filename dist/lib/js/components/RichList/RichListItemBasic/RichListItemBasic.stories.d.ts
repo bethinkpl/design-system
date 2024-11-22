615321f2b93c5c2f960d58e93127465a0c6b00f5
pioziol123 <pioziol123@users.noreply.github.com>
2024-11-22 13:14:02 +0000

    Build dist

--- dist/lib/js/components/RichList/RichListItemBasic/RichListItemBasic.stories.d.ts
+++
@@ -1,5 +0,0 @@
-import RichListItemBasic from './RichListItemBasic.vue';
-import { Meta, StoryFn } from '@storybook/vue3';
-declare const _default: Meta<typeof RichListItemBasic>;
-export default _default;
-export declare const Interactive: StoryFn<typeof RichListItemBasic>;
