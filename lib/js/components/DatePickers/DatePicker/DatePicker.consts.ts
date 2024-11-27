db11830b5c46f301919f68052a99d7025c100c72
Piotr Ziółkowski <piotrziolkowski@MacBook-Pro-Piotr.local>
2024-11-27 16:38:59 +0100

    IT-5863 add fail and neutral tail's colors

--- lib/js/components/DatePickers/DatePicker/DatePicker.consts.ts
+++ lib/js/components/DatePickers/DatePicker/DatePicker.consts.ts
@@ -2,7 +2,9 @@ import { Value } from '../../../utils/type.utils';
 
 export const DATE_PICKER_COLORS = {
 	NEUTRAL_WEAK: 'neutralWeak',
+	SUCCESS: 'neutral',
 	WARNING: 'warning',
+	FAIL: 'fail',
 };
 
 export type DatePickerColors = Value<typeof DATE_PICKER_COLORS>;
