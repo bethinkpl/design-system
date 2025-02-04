b17116e31a80125009920f10f92e3be3304dc55e
tweetgeek <4587534+tweetgeek@users.noreply.github.com>
2025-02-04 11:02:31 +0000

    Deploying to master from @ bethinkpl/design-system@33916dec7580adae095355edbeb215b320a93d74 🚀

---
+++ docs/assets/SelectListItemTile.stories-jXlPEopR.js
@@ -0,0 +1,19 @@
+import{S as a}from"./SelectListItemTile-CziQ20sD.js";import{a as g}from"./Tile-DWASCVea.js";import{a as c,b as l,t as d,d as T}from"./Tile.sb.shared-BZ0VDle3.js";import{w as u}from"./decorator-CSnIf-k5.js";import"./vue.esm-bundler-CWlcb7ht.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./Icon-ChGia3n4.js";import"./Banner-BmKYBJ1B.js";import"./Button-DVtHJ-a3.js";import"./Divider-4HN2u4Et.js";import"./IconButton-rhUVXzuc.js";import"./device-9fgosCm4.js";import"./v4-BZawosSj.js";const R={title:"Components/SelectList/SelectListItemTile",component:a,decorators:[u]},S=s=>({components:{SelectListItemTile:a},setup(){return s},template:d("select-list-item-tile"),data:T}),t=S.bind({});t.argTypes=c;t.args=l;t.parameters={actions:{handles:["click"]},design:{type:"figma",url:"https://www.figma.com/file/izQdYyiBR1GQgFkaOIfIJI/LMS---DS-Components?type=design&node-id=5367-94239&t=GqiOb5BFRTyJrsv8-4"}};const f=s=>({components:{SelectListItemTile:a},setup(){return s},template:d("select-list-item-tile"),data:T}),e=f.bind({});e.argTypes=c;e.args={...l,interactive:!1,iconLeft:null,iconRight:null,text:"this is a text text",eyebrowText:"this is an eyebrowText text",additionalText:"this is some additionalText",state:g.DEFAULT};var i,r,o;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:`args => ({
+  components: {
+    SelectListItemTile
+  },
+  setup() {
+    return args;
+  },
+  template: template('select-list-item-tile'),
+  data
+})`,...(o=(r=t.parameters)==null?void 0:r.docs)==null?void 0:o.source}}};var n,m,p;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`args => ({
+  components: {
+    SelectListItemTile
+  },
+  setup() {
+    return args;
+  },
+  template: template('select-list-item-tile'),
+  data
+})`,...(p=(m=e.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};const k=["Interactive","Static"];export{t as Interactive,e as Static,k as __namedExportsOrder,R as default};
