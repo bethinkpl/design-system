ebb0fd4b7ed5028aacbd9b4b441bd9d2dbd1d31e
Karol Tatała <karol.tatala@bethink.pl>
2025-03-08 16:29:23 +0100

    Combobox Reka UI

--- lib/js/components/Combobox/Combobox.stories.ts
+++ lib/js/components/Combobox/Combobox.stories.ts
@@ -3,6 +3,7 @@ import { Meta, StoryFn, StoryObj } from '@storybook/vue3';
 
 import ComboboxArk from './ComboboxArk.vue';
 import ComboboxPrimeVue from './ComboboxPrimeVue.vue';
+import ComboboxReka from './ComboboxReka.vue';
 
 type ComboboxArkProps = ComponentProps<typeof ComboboxArk>;
 
@@ -21,7 +22,7 @@ const meta: Meta<typeof ComboboxArk> = {
 	title: 'Components/Combobox',
 	component: ComboboxArk,
 	render: (args: ComboboxArkProps) => ({
-		components: { ComboboxArk, ComboboxPrimeVue },
+		components: { ComboboxArk, ComboboxPrimeVue, ComboboxReka },
 		setup() {
 			return {
 				args,
@@ -31,12 +32,15 @@ const meta: Meta<typeof ComboboxArk> = {
 			return {
 				options,
 				valueArk: null,
+				valueReka: null,
 				valuePrimeVue: '',
 			};
 		},
 		template: `<div>
 			<div><h2>Combobox Ark:</h2><ComboboxArk :options="options" v-model="valueArk" /></div>
 			<div>Selected valueArk: {{valueArk}}</div>
+			<div><h2>Combobox Reka:</h2><ComboboxReka :options="options" v-model="valueReka" /></div>
+			<div>Selected valueReka: {{valueReka}}</div>
 			<div><h2>Autocomplete PrimeVue</h2><ComboboxPrimeVue :options="options" v-model="valuePrimeVue" :auto-complete-props="{
 				forceSelection: true,
 			}" /></div>
---
+++ lib/js/components/Combobox/ComboboxReka.vue
@@ -0,0 +1,137 @@
+<template>
+	<div>
+		<combobox-root v-model="value" class="ComboboxRoot">
+			<combobox-anchor class="ComboboxAnchor">
+				<combobox-input class="ComboboxInput" placeholder="Placeholder..." />
+				<combobox-cancel v-if="value" @click="value = null">
+					<icon size="xx-small" :icon="ICONS.FA_XMARK" />
+				</combobox-cancel>
+				<combobox-trigger>
+					<icon size="xx-small" :icon="ICONS.FA_CHEVRON_DOWN" />
+				</combobox-trigger>
+			</combobox-anchor>
+
+			<combobox-content class="ComboboxContent">
+				<combobox-viewport class="ComboboxViewport">
+					<combobox-empty class="ComboboxEmpty">Brak elementów</combobox-empty>
+
+					<combobox-item
+						v-for="option in options"
+						:key="option.name"
+						:value="option.name"
+						class="ComboboxItem"
+					>
+						<combobox-item-indicator class="ComboboxItemIndicator">
+							<icon size="xx-small" :icon="ICONS.FA_CHECK_SOLID" />
+						</combobox-item-indicator>
+						<span>
+							{{ option.name }}
+						</span>
+					</combobox-item>
+				</combobox-viewport>
+			</combobox-content>
+		</combobox-root>
+	</div>
+</template>
+
+<style lang="scss">
+@import '../../../styles/settings/colors/tokens';
+@import '../../../styles/settings/radiuses';
+@import '../../../styles/settings/spacings';
+@import '../../../styles/settings/shadows';
+
+input,
+button {
+	all: unset;
+}
+
+.ComboboxRoot {
+	position: relative;
+	display: inline-flex;
+	flex-direction: column;
+}
+
+.ComboboxAnchor {
+	display: inline-flex;
+	padding: $space-3xs;
+	border-radius: $radius-s;
+	border: 1px solid $color-default-border;
+}
+.ComboboxAnchor:hover {
+	background-color: $color-neutral-background-hovered;
+}
+
+.ComboboxContent {
+	background-color: $color-default-background;
+	border-radius: $radius-s;
+	box-shadow: $shadow-m;
+}
+
+.ComboboxLabel {
+	display: block;
+}
+
+.ComboboxViewport {
+	padding: 5px;
+}
+
+.ComboboxItem {
+	display: flex;
+	gap: $space-4xs;
+	padding: $space-4xs;
+}
+
+.ComboboxItem[data-highlighted] {
+	background-color: $color-neutral-background-hovered;
+}
+</style>
+
+<script setup lang="ts">
+import {
+	ComboboxRoot,
+	ComboboxInput,
+	ComboboxTrigger,
+	ComboboxContent,
+	ComboboxItemIndicator,
+	ComboboxItem,
+	ComboboxViewport,
+	ComboboxAnchor,
+	ComboboxEmpty,
+	ComboboxCancel,
+} from 'reka-ui';
+import { computed, ref } from 'vue';
+import Icon from '../Icons/Icon';
+import { ICONS } from '../Icons/Icon';
+
+interface Item {
+	name: string;
+}
+
+interface Props {
+	options: Array<Item>;
+}
+
+const props = defineProps<Props>();
+
+const value = defineModel<string>();
+
+const valueForCombo = computed(() => {
+	return value.value ? [value.value] : [];
+});
+
+const items = ref(props.options);
+
+const handleInputChange = (details: Combobox.InputValueChangeDetails) => {
+	items.value = props.options.filter((item) =>
+		item.name.toLowerCase().includes(details.inputValue.toLowerCase()),
+	);
+};
+
+const handleFocus = () => {
+	items.value = props.options;
+};
+
+const onValueChange = (value) => {
+	console.log('Value changed', value.items);
+};
+</script>
