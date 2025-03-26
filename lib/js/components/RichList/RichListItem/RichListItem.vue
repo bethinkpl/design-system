c532a8c46541cb52b53f3ff3aa042352040ca74a
Piotr Ziółkowski <pioziol@gmail.com>
2025-03-26 13:26:57 +0100

    IT-6210 fix icons and content spacings in rli

--- lib/js/components/RichList/RichListItem/RichListItem.vue
+++ lib/js/components/RichList/RichListItem/RichListItem.vue
@@ -74,401 +74,6 @@
 	</div>
 </template>
 
-<style scoped lang="scss">
-@import '../../../../styles/settings/animations';
-@import '../../../../styles/settings/colors/tokens';
-@import '../../../../styles/settings/typography/tokens';
-@import '../../../../styles/settings/media-queries';
-@import '../../../../styles/settings/radiuses';
-@import '../../../../styles/settings/spacings';
-@import '../../../../styles/settings/shadows';
-@import '../../../../styles/settings/icons';
-@import './border-colors';
-
-$rich-list-item-background-colors: (
-	neutral: (
-		default: $color-neutral-background,
-		hover: $color-neutral-background-hovered,
-		loading: $color-neutral-background,
-		drag: $color-neutral-background-hovered,
-	),
-	neutral-weak: (
-		default: $color-neutral-background-weak,
-		hover: $color-neutral-background-weak-hovered,
-		loading: $color-neutral-background-weak,
-		drag: $color-neutral-background-weak-hovered,
-	),
-);
-
-// Keep in sync with RICH_LIST_ITEM_MEDIA_HORIZONTAL_WIDTH and _HEIGHT
-$rich-list-item-media-horizontal-width: 100px;
-$rich-list-item-media-horizontal-height: 80px;
-
-.ds-richListItem {
-	$root: &;
-
-	@each $color, $value in $rich-list-item-background-colors {
-		&.-ds-background-#{$color} {
-			background-color: map-get($value, 'default');
-
-			&.-ds-loading {
-				background-color: map-get($value, 'loading');
-			}
-		}
-	}
-
-	cursor: default;
-	display: flex;
-	flex: 1;
-	position: relative;
-	transition: background-color ease-in-out $default-transition-time;
-
-	&.-ds-horizontal {
-		align-items: center;
-		flex-direction: initial;
-		justify-content: initial;
-		padding: 0 $space-4xs;
-
-		#{$root}__container {
-			align-self: initial;
-		}
-
-		#{$root}__dragAndDrop,
-		#{$root}__iconWrapper {
-			align-items: center;
-			padding: $space-3xs $space-3xs $space-3xs $space-s;
-		}
-
-		#{$root}__content {
-			padding: 0 $space-4xs 0 $space-2xs;
-		}
-
-		#{$root}__metadata {
-			justify-content: flex-start;
-			padding-left: $space-4xs;
-		}
-
-		#{$root}__rightContainer {
-			align-items: center;
-			justify-content: initial;
-			padding: $space-xs 0;
-		}
-
-		#{$root}__actionSlot {
-			align-items: center;
-			justify-content: initial;
-			padding: 0 $space-4xs;
-		}
-
-		#{$root}__checkbox {
-			align-items: center;
-			padding: 0 $space-s 0 $space-4xs;
-		}
-
-		&.-ds-small {
-			padding: 0 $space-4xs 0 0;
-
-			#{$root}__dragAndDrop,
-			#{$root}__iconWrapper {
-				padding: $space-3xs 0 $space-3xs $space-xs;
-			}
-
-			#{$root}__checkbox {
-				gap: $space-xs;
-				padding: 0 $space-xs 0 $space-4xs;
-			}
-
-			#{$root}__rightContainer {
-				padding: $space-2xs 0;
-			}
-
-			#{$root}__actionSlot {
-				padding: 0 $space-4xs;
-			}
-
-			#{$root}__metadata {
-				justify-content: flex-start;
-				padding-left: $space-4xs;
-			}
-		}
-	}
-
-	&.-ds-vertical {
-		align-items: flex-start;
-		flex-direction: column;
-		justify-content: center;
-		padding-bottom: $space-4xs;
-
-		#{$root}__container {
-			align-self: stretch;
-			padding: $space-4xs $space-4xs 0;
-		}
-
-		#{$root}__dragAndDrop,
-		#{$root}__iconWrapper {
-			align-items: flex-start;
-			padding: $space-xs 0 0 $space-xs;
-		}
-
-		#{$root}__content {
-			padding: 0 $space-4xs 0 $space-xs;
-		}
-
-		#{$root}__metadata {
-			padding-left: $space-s;
-			padding-right: $space-2xs;
-			width: 100%;
-		}
-
-		#{$root}__rightContainer {
-			align-items: flex-start;
-			justify-content: flex-end;
-		}
-
-		#{$root}__actionSlot {
-			align-items: flex-start;
-			justify-content: flex-end;
-			padding: $space-4xs $space-4xs 0 $space-4xs;
-		}
-
-		#{$root}__checkbox {
-			align-items: flex-start;
-			padding: $space-3xs $space-xs 0 $space-xs;
-		}
-
-		&.-ds-small {
-			#{$root}__dragAndDrop,
-			#{$root}__iconWrapper {
-				padding: $space-2xs $space-4xs 0 $space-2xs;
-			}
-
-			#{$root}__checkbox {
-				gap: $space-s;
-				padding: $space-5xs $space-2xs 0 $space-2xs;
-			}
-
-			#{$root}__actionSlot {
-				padding: 0 $space-4xs 0 $space-5xs;
-			}
-
-			#{$root}__metadata {
-				padding-left: $space-xs;
-			}
-		}
-	}
-
-	&.-ds-verticalWithMedia {
-		padding-right: 0;
-		padding-top: 0;
-	}
-
-	&.-ds-has-media {
-		// Make sure media doesn't cover the border.
-		// If you use a dropdown in one of the slots and it's being cut,
-		// you need to figure out an alternative solution here.
-		overflow: hidden;
-		padding-left: 0;
-	}
-
-	&.-ds-dimmed {
-		.-ds-dimmable {
-			opacity: 0.65;
-		}
-	}
-
-	&.-ds-loading {
-		cursor: initial;
-		pointer-events: none;
-
-		.-ds-dimmable {
-			opacity: 0.5;
-		}
-	}
-
-	&.-ds-interactive {
-		cursor: pointer;
-		pointer-events: initial;
-
-		&.-ds-flat {
-			&:hover {
-				opacity: 1;
-
-				.-ds-dimmable {
-					opacity: 1;
-				}
-			}
-		}
-
-		&:not(.-ds-dimmed):hover {
-			&.-ds-default {
-				@each $color, $value in $rich-list-item-background-colors {
-					&.-ds-background-#{$color} {
-						background-color: map-get($value, 'hover');
-					}
-				}
-			}
-
-			&.-ds-flat {
-				background: $color-neutral-background-ghost-hovered;
-			}
-		}
-
-		&.-ds-draggable-without-handler {
-			&:hover {
-				cursor: grab;
-			}
-
-			&:active {
-				cursor: grabbing;
-			}
-		}
-	}
-
-	&.-ds-elevation-small {
-		box-shadow: $shadow-s;
-	}
-
-	&.-ds-flat {
-		background: $color-neutral-background-ghost;
-
-		&.-ds-loading {
-			background: $color-neutral-background-ghost;
-		}
-
-		#{$root}__wrapper {
-			border: none;
-		}
-
-		&.-ds-interactive.-ds-draggable.-ds-drag {
-			background: $color-neutral-background-ghost-hovered;
-		}
-	}
-
-	&.-ds-default {
-		border: 1px solid $color-neutral-border-weak;
-		border-radius: $radius-s;
-
-		&.-ds-interactive:hover {
-			.-ds-dimmable {
-				opacity: 1;
-			}
-		}
-
-		&.-ds-interactive.-ds-draggable.-ds-drag {
-			@each $color, $value in $rich-list-item-background-colors {
-				&.-ds-background-#{$color} {
-					background-color: map-get($value, 'drag');
-				}
-			}
-		}
-
-		#{$root}__border {
-			@each $color, $value in $rich-list-item-border-colors {
-				&.-ds-border-#{$color} {
-					background-color: $value;
-				}
-			}
-
-			border-radius: $radius-s 0 0 $radius-s;
-			height: calc(100% + 2px);
-			left: -1px;
-			position: absolute;
-			top: -1px;
-			width: $space-4xs;
-		}
-	}
-
-	&__container {
-		align-items: center;
-		display: flex;
-		flex: 1;
-		width: 100%;
-	}
-
-	&__mediaHorizontal {
-		height: $rich-list-item-media-horizontal-height;
-		margin-right: $space-3xs;
-		width: $rich-list-item-media-horizontal-width;
-	}
-
-	&__mediaVertical {
-		aspect-ratio: 4 / 3;
-		overflow: hidden;
-	}
-
-	&__dragAndDrop,
-	&__iconWrapper {
-		align-self: stretch;
-		display: flex;
-	}
-
-	&__content {
-		align-items: center;
-		display: flex;
-		flex: 1 0 0;
-		min-width: 0;
-	}
-
-	&__metadata {
-		align-items: center;
-		gap: $space-xs;
-		padding-right: $space-4xs;
-	}
-
-	&__rightContainer {
-		align-self: stretch;
-		display: flex;
-	}
-
-	&__actionSlot {
-		align-self: stretch;
-		display: flex;
-		gap: $space-2xs;
-	}
-
-	&__checkbox {
-		align-self: stretch;
-		display: flex;
-		gap: $space-s;
-		justify-content: flex-end;
-	}
-
-	&__icon {
-		@include coloredIcon();
-
-		color: $color-neutral-icon-weak;
-	}
-
-	&__dragAndDrop {
-		#{$root}__dragAndDropIcon {
-			color: $color-neutral-icon-weak;
-		}
-
-		&:hover {
-			cursor: grab;
-			#{$root}__dragAndDropIcon {
-				color: $color-neutral-icon-weak-hovered;
-			}
-		}
-
-		&:active {
-			cursor: grabbing;
-		}
-	}
-
-	&.-ds-small {
-		#{$root}__content {
-			align-items: center;
-			padding: 0 $space-4xs 0 $space-2xs;
-		}
-
-		#{$root}__actionSlot {
-			gap: $space-2xs;
-		}
-	}
-}
-</style>
-
 <script lang="ts">
 import DsCheckbox from '../../../components/Form/Checkbox/Checkbox.vue';
 import DsDivider from '../../../components/Divider';
@@ -692,3 +297,402 @@ export default defineComponent({
 	},
 });
 </script>
+
+<style scoped lang="scss">
+@import '../../../../styles/settings/animations';
+@import '../../../../styles/settings/colors/tokens';
+@import '../../../../styles/settings/typography/tokens';
+@import '../../../../styles/settings/media-queries';
+@import '../../../../styles/settings/radiuses';
+@import '../../../../styles/settings/spacings';
+@import '../../../../styles/settings/shadows';
+@import '../../../../styles/settings/icons';
+@import './border-colors';
+
+$rich-list-item-background-colors: (
+	neutral: (
+		default: $color-neutral-background,
+		hover: $color-neutral-background-hovered,
+		loading: $color-neutral-background,
+		drag: $color-neutral-background-hovered,
+	),
+	neutral-weak: (
+		default: $color-neutral-background-weak,
+		hover: $color-neutral-background-weak-hovered,
+		loading: $color-neutral-background-weak,
+		drag: $color-neutral-background-weak-hovered,
+	),
+);
+
+// Keep in sync with RICH_LIST_ITEM_MEDIA_HORIZONTAL_WIDTH and _HEIGHT
+$rich-list-item-media-horizontal-width: 100px;
+$rich-list-item-media-horizontal-height: 80px;
+
+.ds-richListItem {
+	$root: &;
+
+	@each $color, $value in $rich-list-item-background-colors {
+		&.-ds-background-#{$color} {
+			background-color: map-get($value, 'default');
+
+			&.-ds-loading {
+				background-color: map-get($value, 'loading');
+			}
+		}
+	}
+
+	cursor: default;
+	display: flex;
+	flex: 1;
+	position: relative;
+	transition: background-color ease-in-out $default-transition-time;
+
+	&.-ds-horizontal {
+		align-items: center;
+		flex-direction: initial;
+		justify-content: initial;
+		padding: 0 $space-4xs;
+
+		#{$root}__container {
+			align-self: initial;
+		}
+
+		#{$root}__dragAndDrop,
+		#{$root}__iconWrapper {
+			align-items: center;
+			padding: $space-3xs $space-5xs $space-3xs $space-s;
+		}
+
+		#{$root}__content {
+			padding: 0 $space-4xs 0 $space-s;
+		}
+
+		#{$root}__metadata {
+			justify-content: flex-start;
+			padding-left: $space-4xs;
+		}
+
+		#{$root}__rightContainer {
+			align-items: center;
+			justify-content: initial;
+			padding: $space-xs 0;
+		}
+
+		#{$root}__actionSlot {
+			align-items: center;
+			justify-content: initial;
+			padding: 0 $space-4xs;
+		}
+
+		#{$root}__checkbox {
+			align-items: center;
+			padding: 0 $space-s 0 $space-4xs;
+		}
+
+		&.-ds-small {
+			padding: 0 $space-4xs 0 0;
+
+			#{$root}__dragAndDrop,
+			#{$root}__iconWrapper {
+				padding: $space-3xs 0 $space-3xs $space-xs;
+			}
+
+			#{$root}__content {
+				padding: 0 $space-4xs 0 $space-xs;
+			}
+
+			#{$root}__checkbox {
+				gap: $space-xs;
+				padding: 0 $space-xs 0 $space-4xs;
+			}
+
+			#{$root}__rightContainer {
+				padding: $space-2xs 0;
+			}
+
+			#{$root}__actionSlot {
+				padding: 0 $space-4xs;
+			}
+
+			#{$root}__metadata {
+				justify-content: flex-start;
+				padding-left: $space-4xs;
+			}
+		}
+	}
+
+	&.-ds-vertical {
+		align-items: flex-start;
+		flex-direction: column;
+		justify-content: center;
+		padding-bottom: $space-4xs;
+
+		#{$root}__container {
+			align-self: stretch;
+			padding: $space-4xs $space-4xs 0;
+		}
+
+		#{$root}__dragAndDrop,
+		#{$root}__iconWrapper {
+			align-items: flex-start;
+			padding: $space-xs 0 0 $space-xs;
+		}
+
+		#{$root}__content {
+			padding: 0 $space-4xs 0 $space-xs;
+		}
+
+		#{$root}__metadata {
+			padding-left: $space-s;
+			padding-right: $space-2xs;
+			width: 100%;
+		}
+
+		#{$root}__rightContainer {
+			align-items: flex-start;
+			justify-content: flex-end;
+		}
+
+		#{$root}__actionSlot {
+			align-items: flex-start;
+			justify-content: flex-end;
+			padding: $space-4xs $space-4xs 0 $space-4xs;
+		}
+
+		#{$root}__checkbox {
+			align-items: flex-start;
+			padding: $space-3xs $space-xs 0 $space-xs;
+		}
+
+		&.-ds-small {
+			#{$root}__dragAndDrop,
+			#{$root}__iconWrapper {
+				padding: $space-2xs $space-4xs 0 $space-2xs;
+			}
+
+			#{$root}__content {
+				padding: 0 $space-4xs 0 $space-2xs;
+			}
+
+			#{$root}__checkbox {
+				gap: $space-s;
+				padding: $space-5xs $space-2xs 0 $space-2xs;
+			}
+
+			#{$root}__actionSlot {
+				padding: 0 $space-4xs 0 $space-5xs;
+			}
+
+			#{$root}__metadata {
+				padding-left: $space-xs;
+			}
+		}
+	}
+
+	&.-ds-verticalWithMedia {
+		padding-right: 0;
+		padding-top: 0;
+	}
+
+	&.-ds-has-media {
+		// Make sure media doesn't cover the border.
+		// If you use a dropdown in one of the slots and it's being cut,
+		// you need to figure out an alternative solution here.
+		overflow: hidden;
+		padding-left: 0;
+	}
+
+	&.-ds-dimmed {
+		.-ds-dimmable {
+			opacity: 0.65;
+		}
+	}
+
+	&.-ds-loading {
+		cursor: initial;
+		pointer-events: none;
+
+		.-ds-dimmable {
+			opacity: 0.5;
+		}
+	}
+
+	&.-ds-interactive {
+		cursor: pointer;
+		pointer-events: initial;
+
+		&.-ds-flat {
+			&:hover {
+				opacity: 1;
+
+				.-ds-dimmable {
+					opacity: 1;
+				}
+			}
+		}
+
+		&:not(.-ds-dimmed):hover {
+			&.-ds-default {
+				@each $color, $value in $rich-list-item-background-colors {
+					&.-ds-background-#{$color} {
+						background-color: map-get($value, 'hover');
+					}
+				}
+			}
+
+			&.-ds-flat {
+				background: $color-neutral-background-ghost-hovered;
+			}
+		}
+
+		&.-ds-draggable-without-handler {
+			&:hover {
+				cursor: grab;
+			}
+
+			&:active {
+				cursor: grabbing;
+			}
+		}
+	}
+
+	&.-ds-elevation-small {
+		box-shadow: $shadow-s;
+	}
+
+	&.-ds-flat {
+		background: $color-neutral-background-ghost;
+
+		&.-ds-loading {
+			background: $color-neutral-background-ghost;
+		}
+
+		#{$root}__wrapper {
+			border: none;
+		}
+
+		&.-ds-interactive.-ds-draggable.-ds-drag {
+			background: $color-neutral-background-ghost-hovered;
+		}
+	}
+
+	&.-ds-default {
+		border: 1px solid $color-neutral-border-weak;
+		border-radius: $radius-s;
+
+		&.-ds-interactive:hover {
+			.-ds-dimmable {
+				opacity: 1;
+			}
+		}
+
+		&.-ds-interactive.-ds-draggable.-ds-drag {
+			@each $color, $value in $rich-list-item-background-colors {
+				&.-ds-background-#{$color} {
+					background-color: map-get($value, 'drag');
+				}
+			}
+		}
+
+		#{$root}__border {
+			@each $color, $value in $rich-list-item-border-colors {
+				&.-ds-border-#{$color} {
+					background-color: $value;
+				}
+			}
+
+			border-radius: $radius-s 0 0 $radius-s;
+			height: calc(100% + 2px);
+			left: -1px;
+			position: absolute;
+			top: -1px;
+			width: $space-4xs;
+		}
+	}
+
+	&__container {
+		align-items: center;
+		display: flex;
+		flex: 1;
+		width: 100%;
+	}
+
+	&__mediaHorizontal {
+		height: $rich-list-item-media-horizontal-height;
+		margin-right: $space-3xs;
+		width: $rich-list-item-media-horizontal-width;
+	}
+
+	&__mediaVertical {
+		aspect-ratio: 4 / 3;
+		overflow: hidden;
+	}
+
+	&__dragAndDrop,
+	&__iconWrapper {
+		align-self: stretch;
+		display: flex;
+	}
+
+	&__content {
+		align-items: center;
+		display: flex;
+		flex: 1 0 0;
+		min-width: 0;
+	}
+
+	&__metadata {
+		align-items: center;
+		gap: $space-xs;
+		padding-right: $space-4xs;
+	}
+
+	&__rightContainer {
+		align-self: stretch;
+		display: flex;
+	}
+
+	&__actionSlot {
+		align-self: stretch;
+		display: flex;
+		gap: $space-2xs;
+	}
+
+	&__checkbox {
+		align-self: stretch;
+		display: flex;
+		gap: $space-s;
+		justify-content: flex-end;
+	}
+
+	&__icon {
+		@include coloredIcon();
+
+		color: $color-neutral-icon-weak;
+	}
+
+	&__dragAndDrop {
+		#{$root}__dragAndDropIcon {
+			color: $color-neutral-icon-weak;
+		}
+
+		&:hover {
+			cursor: grab;
+
+			#{$root}__dragAndDropIcon {
+				color: $color-neutral-icon-weak-hovered;
+			}
+		}
+
+		&:active {
+			cursor: grabbing;
+		}
+	}
+
+	&.-ds-small {
+		#{$root}__actionSlot {
+			gap: $space-2xs;
+		}
+	}
+}
+</style>
