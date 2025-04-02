3a87f73f402653fb5b40081525298e095018877f
t-tomalak <t-tomalak@users.noreply.github.com>
2025-04-02 06:23:59 +0000

    Build dist

--- dist/lib/js/components/Drawer/DrawerHeader/DrawerHeader.stories.d.ts
+++
@@ -1,5 +0,0 @@
-import DrawerHeader from './DrawerHeader.vue';
-import { Meta, StoryFn } from '@storybook/vue3';
-declare const _default: Meta<typeof DrawerHeader>;
-export default _default;
-export declare const Interactive: StoryFn<typeof DrawerHeader>;
