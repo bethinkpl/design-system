a96ed32affec25670838d504229e0bc4b01e9a5e
latata <1592865+latata@users.noreply.github.com>
2025-05-16 06:54:40 +0000

    Build dist

--- dist/lib/js/components/SelectionTile/SelectionTile.stories.d.ts
+++
@@ -1,5 +0,0 @@
-import SelectionTile from './SelectionTile.vue';
-import { Meta, StoryFn } from '@storybook/vue3';
-declare const _default: Meta<typeof SelectionTile>;
-export default _default;
-export declare const Interactive: StoryFn<typeof SelectionTile>;
