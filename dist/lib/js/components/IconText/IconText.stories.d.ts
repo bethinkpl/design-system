2ff863e13b2a218106fe0b8253105be8fc51135d
latata <1592865+latata@users.noreply.github.com>
2025-05-19 13:21:00 +0000

    Build dist

--- dist/lib/js/components/IconText/IconText.stories.d.ts
+++
@@ -1,5 +0,0 @@
-import { Meta, StoryFn } from '@storybook/vue3';
-import IconText from './IconText.vue';
-declare const _default: Meta<typeof IconText>;
-export default _default;
-export declare const Interactive: StoryFn<typeof IconText>;
