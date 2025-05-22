6d554428bba2b2d7f48c00ec7cb175981e654990
pioziol123 <11304259+pioziol123@users.noreply.github.com>
2025-05-22 11:52:16 +0000

    Build dist

--- dist/lib/js/components/Divider/Divider.stories.d.ts
+++
@@ -1,5 +0,0 @@
-import Divider from './Divider.vue';
-import { Meta, StoryFn } from '@storybook/vue3';
-declare const _default: Meta<typeof Divider>;
-export default _default;
-export declare const Interactive: StoryFn<typeof Divider>;
