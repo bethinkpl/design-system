e8ed6c9f7929dd750438e52b9da346b0bb5d7e42
pioziol123 <11304259+pioziol123@users.noreply.github.com>
2025-05-29 13:31:21 +0000

    Build dist

--- dist/lib/js/components/Drawer/Drawer.stories.d.ts
+++
@@ -1,5 +0,0 @@
-import Drawer from './Drawer.vue';
-import { Meta, StoryFn } from '@storybook/vue3';
-declare const _default: Meta<typeof Drawer>;
-export default _default;
-export declare const Interactive: StoryFn<typeof Drawer>;
