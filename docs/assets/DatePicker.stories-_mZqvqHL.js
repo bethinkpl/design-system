3908ec425ca68307a9c5d83a10507820b3d9dfb9
t-tomalak <12939493+t-tomalak@users.noreply.github.com>
2025-06-06 11:08:49 +0000

    Deploying to master from @ bethinkpl/design-system@c6c6f63a383f56a3df4e366a87051fb1ec7128d6 🚀

---
+++ docs/assets/DatePicker.stories-_mZqvqHL.js
@@ -0,0 +1,187 @@
+import{I as D}from"./Icon-CytrC3Ws.js";import{D as x}from"./Switch-BW1DYjhE.js";import{c as m,d as u,b as g,e as h,a as b}from"./DatePicker-C-1wTByq.js";import"./vue.esm-bundler-C__e7YM5.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./Tile-DXOYbUz7.js";import"./iframe-BorftRc6.js";import"../sb-preview/runtime.js";const{useArgs:I}=__STORYBOOK_MODULE_PREVIEW_API__,N={title:"Components/DatePickers/DatePicker",component:m},f=w=>{const[M,r]=I();return{components:{DatePicker:m,DsSwitch:x},setup(){return w},data(){return{ICONS:Object.freeze(D)}},methods:{updateDate(a){r(a?{date:`${a.getFullYear()}-${a.getMonth()+1}-${a.getDate()}`}:{date:null})}},computed:{formattedDate(){return!this.date||this.date===""?null:new Date(this.date)},formattedMinDate(){return!this.minDate||this.minDate==""?null:new Date(this.minDate)},formattedMaxDate(){return!this.maxDate||this.maxDate==""?null:new Date(this.maxDate)},formattedDisableDates(){return!this.disableDates||!this.disableDates.length?null:this.disableDates.map(a=>new Date(a))}},template:`
+            <date-picker
+                :trigger-type="triggerType"
+                :is-interactive="isInteractive"
+                :placeholder="placeholder"
+                :date="formattedDate"
+                :additional-text="additionalText"
+                :helpMessage="helpMessage"
+                :label="label"
+                :is-label-uppercase="isLabelUppercase"
+                :icon="icon ? ICONS[icon] : null"
+                :is-icon-hidden-on-mobile="isIconHiddenOnMobile"
+                :calendar-position="calendarPosition"
+                :error-message="errorMessage"
+                :state="state"
+                :color="color"
+                :disable-dates="formattedDisableDates"
+                :min-date="formattedMinDate"
+                :max-date="formattedMaxDate"
+                :update-position-based-on-scrollable-selector="updatePositionBasedOnScrollableSelector"
+                @update:date="updateDate"
+            >
+                <ds-switch label-left="lewa" label-right="prawa" />
+            </date-picker>`}},t=f.bind({}),n=Date.now(),s=864e5,O={triggerType:u.TILE,isInteractive:!0,color:g.NEUTRAL_WEAK,label:"Date picker",isLabelUppercase:!1,placeholder:"Wybierz datę",date:"",disableDates:[new Date(n+s*2).toISOString().slice(0,10)],minDate:new Date(n).toISOString().slice(0,10),maxDate:new Date(n+s*30).toISOString().slice(0,10),icon:"FA_CALENDAR_DAYS",isIconHiddenOnMobile:!1,additionalText:"",helpMessage:null,errorMessage:null,calendarPosition:h.BOTTOM,state:b.DEFAULT},S={triggerType:{control:"select",options:Object.values(u)},date:{control:"text"},minDate:{control:"text"},maxDate:{control:"text"},icon:{control:"select",options:[null,...Object.keys(D)]},calendarPosition:{control:"select",options:Object.values(h)},state:{control:"select",options:Object.values(b)},color:{control:"select",options:Object.values(g)},disableDates:{control:"object"}};t.argTypes=S;t.args=O;t.parameters={design:{type:"figma",url:"https://www.figma.com/design/03ABNCSDYWYDmOPJOBGM5l/INI-153-Planowanie-pracy-z-lekcjami?node-id=245-162031&t=g08nj70xhT9BZTpu-4"}};const e=f.bind({});e.decorators=[()=>({template:`
+            <div style="height: 500px; overflow-y: scroll;" class="scrollable-container">
+                <div style="height: 1500px; padding: 6px;">
+                    <story />
+                </div>
+            </div>`})];e.argTypes=S;e.args={...O,updatePositionBasedOnScrollableSelector:".scrollable-container"};e.parameters={design:{type:"figma",url:"https://www.figma.com/design/03ABNCSDYWYDmOPJOBGM5l/INI-153-Planowanie-pracy-z-lekcjami?node-id=245-162031&t=g08nj70xhT9BZTpu-4"}};var i,l,o;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:`args => {
+  const [_, updateArgs] = useArgs();
+  return {
+    components: {
+      DatePicker,
+      DsSwitch
+    },
+    setup() {
+      return args;
+    },
+    data() {
+      return {
+        ICONS: Object.freeze(ICONS)
+      };
+    },
+    methods: {
+      updateDate(date: Date) {
+        if (date) {
+          updateArgs({
+            date: \`\${date.getFullYear()}-\${date.getMonth() + 1}-\${date.getDate()}\`
+          });
+        } else {
+          updateArgs({
+            date: null
+          });
+        }
+      }
+    },
+    computed: {
+      formattedDate() {
+        if (!this.date || this.date === '') {
+          return null;
+        }
+        return new Date(this.date);
+      },
+      formattedMinDate() {
+        if (!this.minDate || this.minDate == '') {
+          return null;
+        }
+        return new Date(this.minDate);
+      },
+      formattedMaxDate() {
+        if (!this.maxDate || this.maxDate == '') {
+          return null;
+        }
+        return new Date(this.maxDate);
+      },
+      formattedDisableDates() {
+        if (!this.disableDates || !this.disableDates.length) {
+          return null;
+        }
+        return this.disableDates.map(date => new Date(date));
+      }
+    },
+    template: \`
+            <date-picker
+                :trigger-type="triggerType"
+                :is-interactive="isInteractive"
+                :placeholder="placeholder"
+                :date="formattedDate"
+                :additional-text="additionalText"
+                :helpMessage="helpMessage"
+                :label="label"
+                :is-label-uppercase="isLabelUppercase"
+                :icon="icon ? ICONS[icon] : null"
+                :is-icon-hidden-on-mobile="isIconHiddenOnMobile"
+                :calendar-position="calendarPosition"
+                :error-message="errorMessage"
+                :state="state"
+                :color="color"
+                :disable-dates="formattedDisableDates"
+                :min-date="formattedMinDate"
+                :max-date="formattedMaxDate"
+                :update-position-based-on-scrollable-selector="updatePositionBasedOnScrollableSelector"
+                @update:date="updateDate"
+            >
+                <ds-switch label-left="lewa" label-right="prawa" />
+            </date-picker>\`
+  };
+}`,...(o=(l=t.parameters)==null?void 0:l.docs)==null?void 0:o.source}}};var d,c,p;e.parameters={...e.parameters,docs:{...(d=e.parameters)==null?void 0:d.docs,source:{originalSource:`args => {
+  const [_, updateArgs] = useArgs();
+  return {
+    components: {
+      DatePicker,
+      DsSwitch
+    },
+    setup() {
+      return args;
+    },
+    data() {
+      return {
+        ICONS: Object.freeze(ICONS)
+      };
+    },
+    methods: {
+      updateDate(date: Date) {
+        if (date) {
+          updateArgs({
+            date: \`\${date.getFullYear()}-\${date.getMonth() + 1}-\${date.getDate()}\`
+          });
+        } else {
+          updateArgs({
+            date: null
+          });
+        }
+      }
+    },
+    computed: {
+      formattedDate() {
+        if (!this.date || this.date === '') {
+          return null;
+        }
+        return new Date(this.date);
+      },
+      formattedMinDate() {
+        if (!this.minDate || this.minDate == '') {
+          return null;
+        }
+        return new Date(this.minDate);
+      },
+      formattedMaxDate() {
+        if (!this.maxDate || this.maxDate == '') {
+          return null;
+        }
+        return new Date(this.maxDate);
+      },
+      formattedDisableDates() {
+        if (!this.disableDates || !this.disableDates.length) {
+          return null;
+        }
+        return this.disableDates.map(date => new Date(date));
+      }
+    },
+    template: \`
+            <date-picker
+                :trigger-type="triggerType"
+                :is-interactive="isInteractive"
+                :placeholder="placeholder"
+                :date="formattedDate"
+                :additional-text="additionalText"
+                :helpMessage="helpMessage"
+                :label="label"
+                :is-label-uppercase="isLabelUppercase"
+                :icon="icon ? ICONS[icon] : null"
+                :is-icon-hidden-on-mobile="isIconHiddenOnMobile"
+                :calendar-position="calendarPosition"
+                :error-message="errorMessage"
+                :state="state"
+                :color="color"
+                :disable-dates="formattedDisableDates"
+                :min-date="formattedMinDate"
+                :max-date="formattedMaxDate"
+                :update-position-based-on-scrollable-selector="updatePositionBasedOnScrollableSelector"
+                @update:date="updateDate"
+            >
+                <ds-switch label-left="lewa" label-right="prawa" />
+            </date-picker>\`
+  };
+}`,...(p=(c=e.parameters)==null?void 0:c.docs)==null?void 0:p.source}}};const k=["Interactive","ScrollableContainer"];export{t as Interactive,e as ScrollableContainer,k as __namedExportsOrder,N as default};
