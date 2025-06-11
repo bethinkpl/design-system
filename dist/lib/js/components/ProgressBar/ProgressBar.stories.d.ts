40e176ce15ce165a0d449796194c2db905bd0479
latata <1592865+latata@users.noreply.github.com>
2025-06-11 14:33:58 +0000

    Build dist

--- dist/lib/js/components/ProgressBar/ProgressBar.stories.d.ts
+++
@@ -1,9 +0,0 @@
-import ProgressBar from './ProgressBar.vue';
-import { Meta, StoryFn } from '@storybook/vue3';
-declare const _default: Meta<typeof ProgressBar>;
-export default _default;
-export declare const Interactive: StoryFn<typeof ProgressBar>;
-export declare const WithoutLabels: StoryFn<typeof ProgressBar>;
-export declare const TwoColors: StoryFn<typeof ProgressBar>;
-export declare const WithGaps: StoryFn<typeof ProgressBar>;
-export declare const Compact: StoryFn<typeof ProgressBar>;
