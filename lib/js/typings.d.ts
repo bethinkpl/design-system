358b5914a652eef51af7ebd9696d290414abc547
Karol Tatała <karol.tatala@bethink.pl>
2025-06-26 09:28:33 +0200

    Change svg type

--- lib/js/typings.d.ts
+++ lib/js/typings.d.ts
@@ -5,7 +5,7 @@ declare module '*.vue' {
 }
 
 declare module '*.svg' {
-	import Vue, { VueConstructor } from 'vue';
-	const content: VueConstructor<Vue>;
+	import { VueConstructor } from 'vue';
+	const content: VueConstructor;
 	export default content;
 }
