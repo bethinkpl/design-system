418aadb232747790936243efe2fe47438618dae0
t-tomalak <12939493+t-tomalak@users.noreply.github.com>
2025-07-31 13:49:54 +0000

    Build dist

--- dist/lib/js/components/Pill/Pill.stories.d.ts
+++
@@ -1,5 +0,0 @@
-import Pill from './Pill.vue';
-import { Meta, StoryFn } from '@storybook/vue3';
-declare const _default: Meta<typeof Pill>;
-export default _default;
-export declare const Interactive: StoryFn<typeof Pill>;
