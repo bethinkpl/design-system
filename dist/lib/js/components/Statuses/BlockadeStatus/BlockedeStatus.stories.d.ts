04fe88c9666ff2e34a7ddd87cb97001c2d565852
bognix <3473327+bognix@users.noreply.github.com>
2025-08-04 08:19:57 +0000

    Build dist

--- dist/lib/js/components/Statuses/BlockadeStatus/BlockedeStatus.stories.d.ts
+++
@@ -1,5 +0,0 @@
-import BlockadeStatus from './BlockadeStatus.vue';
-import { Meta, StoryFn } from '@storybook/vue3';
-declare const _default: Meta<typeof BlockadeStatus>;
-export default _default;
-export declare const Interactive: StoryFn<typeof BlockadeStatus>;
