919aa2a72227026deff4b3e3ab50aa4d8095d533
pioziol123 <pioziol@gmail.com>
2025-08-20 06:36:45 +0000

    Deploying to master from @ bethinkpl/design-system@4424a1c31740f6c480f955e0265700770d2e6fdb 🚀

---
+++ docs/assets/DrawerTile.stories-DM8q2FZN.js
@@ -0,0 +1,10 @@
+import{D as o}from"./DrawerTile-Ct8GXnug.js";import{d as s,t as m,a as n,b as i}from"./Tile.sb.shared-DkFGJTbD.js";import"./Tile-Bqy_YAm-.js";import"./vue.esm-bundler-DcRN7CPI.js";import"./Icon-CQP2P4F0.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./Banner-Chi4ODNz.js";import"./Button-B5ihYM35.js";import"./Divider-NOTDPuJ1.js";import"./IconButton-CfCvN_in.js";import"./device-9fgosCm4.js";import"./FeatureIcon-9bK6JgiT.js";const b={title:"Components/Drawer/DrawerTile",component:o},c=p=>({components:{DrawerTile:o},setup(){return p},template:m("drawer-tile"),data:s}),e=c.bind({});e.argTypes=n;e.args=i;e.parameters={design:{type:"figma",url:"https://www.figma.com/file/izQdYyiBR1GQgFkaOIfIJI/LMS---DS-Components?node-id=6785-111025&t=pzyp7YuJcnGNBfS5-4"}};var r,t,a;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`args => ({
+  components: {
+    DrawerTile
+  },
+  setup() {
+    return args;
+  },
+  template: template('drawer-tile'),
+  data
+})`,...(a=(t=e.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const v=["Interactive"];export{e as Interactive,v as __namedExportsOrder,b as default};
