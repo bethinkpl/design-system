b2f4f879c8e81296a0e1ea752c7b624075b3fb7e
kvas-damian <kvas-damian@users.noreply.github.com>
2025-08-22 05:17:59 +0000

    Deploying to master from @ bethinkpl/design-system@870a4e7b0c98fb70612649474170cdd0e96f66fd 🚀

--- dist/lib/js/components/Pagination/Pagination.vue.d.ts
+++ dist/lib/js/components/Pagination/Pagination.vue.d.ts
@@ -68,6 +68,7 @@ declare const _default: import('vue').DefineComponent<import('vue').ExtractPropT
         readonly SIDEBAR_FLIP_SOLID: VueConstructor<Vue>;
         readonly SLIDERS_SEARCH: VueConstructor<Vue>;
         readonly CLOUD_DISCONNECTED: VueConstructor<Vue>;
+        readonly MOBILE_ROTATE: VueConstructor<Vue>;
         readonly FA_ADDRESS_CARD: import('@fortawesome/fontawesome-common-types').IconDefinition;
         readonly FA_ANGLE_DOWN: import('@fortawesome/fontawesome-common-types').IconDefinition;
         readonly FA_ANGLE_LEFT: import('@fortawesome/fontawesome-common-types').IconDefinition;
@@ -394,6 +395,7 @@ declare const _default: import('vue').DefineComponent<import('vue').ExtractPropT
             readonly SIDEBAR_FLIP_SOLID: VueConstructor<Vue>;
             readonly SLIDERS_SEARCH: VueConstructor<Vue>;
             readonly CLOUD_DISCONNECTED: VueConstructor<Vue>;
+            readonly MOBILE_ROTATE: VueConstructor<Vue>;
             readonly FA_ADDRESS_CARD: import('@fortawesome/fontawesome-common-types').IconDefinition;
             readonly FA_ANGLE_DOWN: import('@fortawesome/fontawesome-common-types').IconDefinition;
             readonly FA_ANGLE_LEFT: import('@fortawesome/fontawesome-common-types').IconDefinition;
@@ -1045,6 +1047,7 @@ declare const _default: import('vue').DefineComponent<import('vue').ExtractPropT
             readonly SIDEBAR_FLIP_SOLID: VueConstructor<Vue>;
             readonly SLIDERS_SEARCH: VueConstructor<Vue>;
             readonly CLOUD_DISCONNECTED: VueConstructor<Vue>;
+            readonly MOBILE_ROTATE: VueConstructor<Vue>;
             readonly FA_ADDRESS_CARD: import('@fortawesome/fontawesome-common-types').IconDefinition;
             readonly FA_ANGLE_DOWN: import('@fortawesome/fontawesome-common-types').IconDefinition;
             readonly FA_ANGLE_LEFT: import('@fortawesome/fontawesome-common-types').IconDefinition;
