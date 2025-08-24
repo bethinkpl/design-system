a47269ae00393c0bd199ad41206868ded14543ef
pioziol123 <pioziol@gmail.com>
2025-08-24 17:08:25 +0000

    Deploying to master from @ bethinkpl/design-system@97ee5ed3e0b5e319c58bc3babe09864fb845f5b7 🚀

---
+++ docs/assets/BasicRichListItem.stories-DfNnQSpr.js
@@ -0,0 +1,202 @@
+import{B as l}from"./BasicRichListItem-MZ4BwQZ_.js";import{w as h}from"./decorator-B_8W69Nd.js";import"./Avatar-CrSnlCQ6.js";import"./Badge-60oiev_y.js";import"./BadgeScore-C5POCxVN.js";import"./Banner-C0Tks7ve.js";import"./Button-jnh2Tlf-.js";import"./ToggleButton-BLQh0Av6.js";import"./Card-BlnGdF9X.js";import"./CardExpandable-Bux5jwuS.js";import"./DatePicker-DzE-Hm05.js";import"./DateRangePicker-Bf5eZ4qn.js";import"./Divider-NOTDPuJ1.js";import"./FeatureIcon-ChWa-R06.js";import"./FormField-C5FuUFzK.js";import"./FormFieldMessage--iIxaxav.js";import"./HelpButton-Bg6h1SIL.js";import{I as a,c as T}from"./Icon-ZJHMCHB3.js";import"./IconButton-M8bwqP9X.js";import"./InputField-Dx68Xoqn.js";import"./string-s9GaJtCv.js";import"./Modal-TccXbC2U.js";import"./ModalDialog-DL3W6g4x.js";import"./NumberInCircle-CE28_Nsz.js";import"./PasswordField-BgFQczsV.js";import"./TabItem--3Ph-uke.js";import"./Tile-BE-T2xX1.js";import"./Toast-BdQfQjP-.js";import"./AccessStatus-A8_KqOx-.js";import"./BlockadeStatus-ciudf9B2.js";import"./SurveyToggle-BIcIWCH7.js";import"./SurveyQuestionScale-DYacuJbB.js";import"./SurveyQuestionOpenEnded-DfwTvHWF.js";import"./SectionHeader-BJ90z-0d.js";import"./PageHeader-Cy-UHuRb.js";import"./SectionTitle-wTuJZZVS.js";import"./Drawer-CrAn35Yo.js";import"./DrawerContent-DAIzyQNz.js";import"./DrawerDivider-CKRXkKRz.js";import"./DrawerHeader-C05dcBld.js";import"./DrawerListItem-BnR_Crxc.js";import"./DrawerListItemGroup-SEWltKLJ.js";import"./DrawerTile-BdD2c2Li.js";import"./DrawerSection-D96q7Udg.js";import"./OutlineItem-Org3ZnRU.js";import"./OutlineDivider-DJ9pnpJN.js";import"./OutlineSectionHeader-CSNr7XVB.js";import"./Chip-DPKBxt4x.js";import"./CounterToggle-Dq9JHZFB.js";import"./SelectList-YMQ8rEMc.js";import"./SelectListItem-BlPwpwLF.js";import"./SelectListItemDivider-CTU7_4OL.js";import"./SelectListItemToggle-D9UVFakO.js";import"./SelectListItemTile-9uJDmv3B.js";import"./SelectListSectionTitle-B9Dj10G9.js";import"./SelectionTile-8hOYCE8b.js";import"./LoadingBar-DGNrwKu4.js";import"./PopOver-BVWJCJpo.js";import"./Dropdown-Bdrd2ZIh.js";import"./ProgressBar-DQMsJoIF.js";import"./ProgressDonutChart-u75GYOXS.js";import"./IconText-Cm57Iuqt.js";import"./Pagination-JlS_MH0D.js";import"./OverlayHeader-C4opzadl.js";import"./Well-URwFKkfO.js";import"./ThreeColumnLayout-LVzDOKfj.js";import{R as C,a as p,b as E,c,d as n,e as d,f as m}from"./GroupRichListItem-JJgjRNp-.js";import"./Switch-Cbq6f924.js";import"./TextGroup-dzR19lzs.js";import"./Tooltip-BvaJ_K9_.js";import{I as O}from"./Image-B7PRENWe.js";import"./Skeleton-D5xhZMQa.js";import"./vue.esm-bundler-DcRN7CPI.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./v4-CwV2VaSl.js";import"./iframe-DZOmauhh.js";import"../sb-preview/runtime.js";import"./device-9fgosCm4.js";import"./user-_JB5aHqq.js";import"./SelectionControl-sOEO29vj.js";import"./vue-popper-m_ujT3wC.js";import"./Checkbox-DOR76zLg.js";const{useArgs:y}=__STORYBOOK_MODULE_PREVIEW_API__,ot={title:"Components/RichList/BasicRichListItem",component:l,decorators:[e=>({components:{story:e},template:"<div style='display: flex;padding: 16px;'><story /></div>"}),h]},v=(e,i={})=>(e.argTypes={type:{options:Object.values(m),control:"select"},size:{options:Object.values(d),control:"select"},layout:{options:Object.values(n),control:{type:"select"}},state:{options:Object.values(c),control:"select"},iconColor:{options:[null,...Object.values(T)],control:"select"},borderColor:{options:[null,...Object.values(E)],control:"select"},icon:{options:[null,...Object.keys(a)],control:"select"},iconColorHex:{control:"text"},borderColorHex:{control:"text"},eyebrow:{control:"text"},eyebrowEllipsis:{control:"boolean"},isEyebrowUppercase:{control:"boolean"},text:{control:"text"},textEllipsis:{control:"boolean"},supportingText:{control:"text"},supportingTextEllipsis:{control:"boolean"},isSupportingTextTooltipEnabled:{control:"boolean"},metadata:{control:"text"},actions:{control:"text"},draggableIconClassName:{control:"text"},backgroundColor:{options:[null,...Object.values(p)],control:"select"},elevation:{options:[null,...Object.values(C)],control:"select"}},e.args={size:d.MEDIUM,type:m.DEFAULT,layout:n.HORIZONTAL,backgroundColor:p.NEUTRAL,elevation:null,isDimmed:!1,isDraggable:!0,hasDraggableHandler:!0,icon:null,iconColor:null,iconColorHex:"",hasActionsSlotDivider:!0,isSelectable:!0,isSelected:!0,isTextGroupSelected:!1,borderColor:null,borderColorHex:"",state:c.DEFAULT,isInteractive:!0,draggableIconClassName:"draggableIconClassName-1",eyebrow:"Eyebrow Uppercase",eyebrowEllipsis:!1,isEyebrowUppercase:!1,text:"Długa nazwa gdy się nie mieści Praesentium dicta sit. Molestiae unde voluptatem eaque labore.",textEllipsis:!1,supportingText:"null",supportingTextEllipsis:!1,isSupportingTextTooltipEnabled:!1,metadata:"Metadata Slot",actions:"ACS",...i},e.parameters={actions:{handles:["icon-click","click","update:is-selected"]},layout:"fullscreen",design:{type:"figma",url:"https://www.figma.com/file/izQdYyiBR1GQgFkaOIfIJI/LMS---DS-Components?type=design&node-id=8673-2345&mode=design&t=QeEz8TmzxDbrGYK3-4"}},e),w=e=>{const[i,r]=y();return{components:{BasicRichListItem:l},setup(){return e},data(){return{ICONS:Object.freeze(a)}},methods:{updateIsSelected(s){r({isSelected:s})}},template:`
+        <basic-rich-list-item
+            :size="size"
+            :type="type"
+            :layout="layout"
+            :is-interactive="isInteractive"
+            :is-draggable="isDraggable"
+            :icon="ICONS[icon]"
+            :icon-color="iconColor"
+            :icon-color-hex="iconColorHex"
+            :is-dimmed="isDimmed"
+            :border-color="borderColor"
+            :border-color-hex="borderColorHex"
+            :state="state"
+            :eyebrow="eyebrow === 'null' ? null : eyebrow"
+            :eyebrow-ellipsis="eyebrowEllipsis"
+            :is-eyebrow-uppercase="isEyebrowUppercase"
+            :text="text"
+            :text-ellipsis="textEllipsis"
+            :supporting-text="supportingText === 'null' ? null : supportingText"
+            :supporting-text-ellipsis="supportingTextEllipsis"
+            :is-supporting-text-tooltip-enabled="isSupportingTextTooltipEnabled"
+            :background-color="backgroundColor"
+            :elevation="elevation"
+            :has-draggable-handler="hasDraggableHandler"
+            :has-actions-slot-divider="hasActionsSlotDivider"
+            :is-selectable="isSelectable"
+            :is-selected="isSelected"
+            :is-text-group-selected="isTextGroupSelected"
+            @update:is-selected="updateIsSelected"
+        >
+            <template v-if="metadata" #metadata>
+                <div v-html="metadata" />
+            </template>
+            <template v-if="actions" #actions>
+                <div v-html="actions" />
+            </template>
+        </basic-rich-list-item>`}},t=w.bind({});v(t);const _=e=>{const[i,r]=y();return{components:{BasicRichListItem:l,DsImage:O},setup(){return e},data(){return{ICONS:Object.freeze(a)}},methods:{updateIsSelected(s){r({isSelected:s})}},template:`
+            <basic-rich-list-item
+                :size="size"
+                :type="type"
+                :layout="layout"
+                :is-interactive="isInteractive"
+                :is-draggable="isDraggable"
+                :icon="ICONS[icon]"
+                :icon-color="iconColor"
+                :icon-color-hex="iconColorHex"
+                :is-dimmed="isDimmed"
+                :border-color="borderColor"
+                :border-color-hex="borderColorHex"
+                :state="state"
+                :eyebrow="eyebrow === 'null' ? null : eyebrow"
+                :eyebrow-ellipsis="eyebrowEllipsis"
+                :is-eyebrow-uppercase="isEyebrowUppercase"
+                :text="text"
+                :text-ellipsis="textEllipsis"
+                :supporting-text="supportingText === 'null' ? null : supportingText"
+                :supporting-text-ellipsis="supportingTextEllipsis"
+                :is-supporting-text-tooltip-enabled="isSupportingTextTooltipEnabled"
+                :background-color="backgroundColor"
+                :elevation="elevation"
+                :has-draggable-handler="hasDraggableHandler"
+                :has-actions-slot-divider="hasActionsSlotDivider"
+                :is-selectable="isSelectable"
+                :is-selected="isSelected"
+                :is-text-group-selected="isTextGroupSelected"
+                @update:is-selected="updateIsSelected"
+            >
+                <template #media>
+                    <ds-image :src="imageSrcUsedInStoryBook"/>
+                </template>
+                <template v-if="metadata" #metadata>
+                    <div v-html="metadata"/>
+                </template>
+                <template v-if="actions" #actions>
+                    <div v-html="actions"/>
+                </template>
+            </basic-rich-list-item>`}},o=_.bind({});v(o,{imageSrcUsedInStoryBook:"https://storage.googleapis.com/media-manager/lek/018f6291-3956-7342-8e6b-0ee901d48643/018f6291-3a56-7213-aef6-b5da7253839f.jpg"});var u,b,g;t.parameters={...t.parameters,docs:{...(u=t.parameters)==null?void 0:u.docs,source:{originalSource:`args => {
+  const [_, updateArgs] = useArgs();
+  return {
+    components: {
+      BasicRichListItem
+    },
+    setup() {
+      return args;
+    },
+    data() {
+      return {
+        ICONS: Object.freeze(ICONS)
+      };
+    },
+    methods: {
+      updateIsSelected(isSelected) {
+        updateArgs({
+          isSelected
+        });
+      }
+    },
+    template: \`
+        <basic-rich-list-item
+            :size="size"
+            :type="type"
+            :layout="layout"
+            :is-interactive="isInteractive"
+            :is-draggable="isDraggable"
+            :icon="ICONS[icon]"
+            :icon-color="iconColor"
+            :icon-color-hex="iconColorHex"
+            :is-dimmed="isDimmed"
+            :border-color="borderColor"
+            :border-color-hex="borderColorHex"
+            :state="state"
+            :eyebrow="eyebrow === 'null' ? null : eyebrow"
+            :eyebrow-ellipsis="eyebrowEllipsis"
+            :is-eyebrow-uppercase="isEyebrowUppercase"
+            :text="text"
+            :text-ellipsis="textEllipsis"
+            :supporting-text="supportingText === 'null' ? null : supportingText"
+            :supporting-text-ellipsis="supportingTextEllipsis"
+            :is-supporting-text-tooltip-enabled="isSupportingTextTooltipEnabled"
+            :background-color="backgroundColor"
+            :elevation="elevation"
+            :has-draggable-handler="hasDraggableHandler"
+            :has-actions-slot-divider="hasActionsSlotDivider"
+            :is-selectable="isSelectable"
+            :is-selected="isSelected"
+            :is-text-group-selected="isTextGroupSelected"
+            @update:is-selected="updateIsSelected"
+        >
+            <template v-if="metadata" #metadata>
+                <div v-html="metadata" />
+            </template>
+            <template v-if="actions" #actions>
+                <div v-html="actions" />
+            </template>
+        </basic-rich-list-item>\`
+  };
+}`,...(g=(b=t.parameters)==null?void 0:b.docs)==null?void 0:g.source}}};var x,S,I;o.parameters={...o.parameters,docs:{...(x=o.parameters)==null?void 0:x.docs,source:{originalSource:`args => {
+  const [_, updateArgs] = useArgs();
+  return {
+    components: {
+      BasicRichListItem,
+      DsImage
+    },
+    setup() {
+      return args;
+    },
+    data() {
+      return {
+        ICONS: Object.freeze(ICONS)
+      };
+    },
+    methods: {
+      updateIsSelected(isSelected) {
+        updateArgs({
+          isSelected
+        });
+      }
+    },
+    template: \`
+            <basic-rich-list-item
+                :size="size"
+                :type="type"
+                :layout="layout"
+                :is-interactive="isInteractive"
+                :is-draggable="isDraggable"
+                :icon="ICONS[icon]"
+                :icon-color="iconColor"
+                :icon-color-hex="iconColorHex"
+                :is-dimmed="isDimmed"
+                :border-color="borderColor"
+                :border-color-hex="borderColorHex"
+                :state="state"
+                :eyebrow="eyebrow === 'null' ? null : eyebrow"
+                :eyebrow-ellipsis="eyebrowEllipsis"
+                :is-eyebrow-uppercase="isEyebrowUppercase"
+                :text="text"
+                :text-ellipsis="textEllipsis"
+                :supporting-text="supportingText === 'null' ? null : supportingText"
+                :supporting-text-ellipsis="supportingTextEllipsis"
+                :is-supporting-text-tooltip-enabled="isSupportingTextTooltipEnabled"
+                :background-color="backgroundColor"
+                :elevation="elevation"
+                :has-draggable-handler="hasDraggableHandler"
+                :has-actions-slot-divider="hasActionsSlotDivider"
+                :is-selectable="isSelectable"
+                :is-selected="isSelected"
+                :is-text-group-selected="isTextGroupSelected"
+                @update:is-selected="updateIsSelected"
+            >
+                <template #media>
+                    <ds-image :src="imageSrcUsedInStoryBook"/>
+                </template>
+                <template v-if="metadata" #metadata>
+                    <div v-html="metadata"/>
+                </template>
+                <template v-if="actions" #actions>
+                    <div v-html="actions"/>
+                </template>
+            </basic-rich-list-item>\`
+  };
+}`,...(I=(S=o.parameters)==null?void 0:S.docs)==null?void 0:I.source}}};const it=["Interactive","WithMedia"];export{t as Interactive,o as WithMedia,it as __namedExportsOrder,ot as default};
