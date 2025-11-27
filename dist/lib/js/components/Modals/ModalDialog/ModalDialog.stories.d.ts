16954f3a4b92ba780b377fa6254b13bf3df5e617
latata <1592865+latata@users.noreply.github.com>
2025-11-27 13:28:12 +0000

    Build dist

--- dist/lib/js/components/Modals/ModalDialog/ModalDialog.stories.d.ts
+++
@@ -1,7 +0,0 @@
-import { Meta, StoryFn } from '@storybook/vue3';
-import ModalDialog from './ModalDialog.vue';
-declare const _default: Meta<typeof ModalDialog>;
-export default _default;
-export declare const Interactive: StoryFn<typeof ModalDialog>;
-export declare const Danger: StoryFn<typeof ModalDialog>;
-export declare const WithImage: StoryFn<typeof ModalDialog>;
