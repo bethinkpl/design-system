28ee3e1b44386bdee7dced4f4769248c2df68948
bognix <3473327+bognix@users.noreply.github.com>
2025-12-02 13:44:14 +0000

    Build dist

--- dist/lib/js/components/Icons/Icon/Icon.stories.d.ts
+++
@@ -1,6 +0,0 @@
-import Icon from './Icon.vue';
-import { Meta, StoryFn } from '@storybook/vue3';
-declare const _default: Meta<typeof Icon>;
-export default _default;
-export declare const Interactive: StoryFn<typeof Icon>;
-export declare const AllIcons: StoryFn<typeof Icon>;
