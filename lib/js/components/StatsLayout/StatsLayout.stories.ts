e0131048e4bd0d6a1babaa4c55404d46a8bbad0d
Sebastian Dawidziak <sebastian@tweetgeek.pl>
2025-12-05 13:41:39 +0100

    IT-7067 | Refactor StatsLayout stories to simplify template structure and add MultipleColumns story

--- lib/js/components/StatsLayout/StatsLayout.stories.ts
+++ lib/js/components/StatsLayout/StatsLayout.stories.ts
@@ -1,60 +1,76 @@
 import { Args, ArgTypes, Meta, StoryObj } from '@storybook/vue3';
-import { useArgs } from '@storybook/preview-api';
 import StatsLayout from './StatsLayout.vue';
 import type { ComponentProps } from 'vue-component-type-helpers';
 import StatsSectionHeader from './StatsSectionHeader/StatsSectionHeader.vue';
 import StatsResetBanner from './StatsResetBanner/StatsResetBanner.vue';
 import SlotPlaceholder from '../../../../.storybook/SlotPlaceholder/SlotPlaceholder.vue';
 
-type StatsLayoutProps = ComponentProps<typeof StatsLayout> & {
-	items?: Array<any>;
+type StatsLayoutProps = ComponentProps<typeof StatsLayout>;
+
+const prepareItemsTemplate = (
+	itemsCount: number = 5,
+	itemTemplate: string = '<slot-placeholder/>',
+) => {
+	return Array.from({ length: itemsCount }, (_, index) => {
+		return `<template v-slot:["statsItem-${index}"]>${itemTemplate}</template>`;
+	}).join('');
 };
 
 const meta: Meta<StatsLayoutProps> = {
 	title: 'Components/StatsLayout/StatsLayout',
 	component: StatsLayout,
 	render: (args) => {
-		const [_, updateArgs] = useArgs();
-
-		const itemsTemplate = (args.items || [])
-			.map((item, index) => {
-				return `<template v-slot:["statsItem-${index}"]><slot-placeholder/></template>`;
-			})
-			.join('');
-
 		return {
-			components: { StatsLayout, StatsSectionHeader, StatsResetBanner, SlotPlaceholder },
+			components: { StatsLayout, SlotPlaceholder },
 			setup() {
-				return {
-					args,
-				};
+				return { args };
 			},
-			methods: {
-				onSelectFilter(filterKey) {
-					updateArgs({
-						selectedFilterKey: filterKey,
-					});
-				},
-			},
-			template:
-				'<stats-layout v-bind="args">' +
-				'<template #sectionHeader>' +
-				'	<stats-section-header v-if="args.filterItems" :filterItems="args.filterItems" :selectedFilterKey="args.selectedFilterKey" @select-filter="onSelectFilter">' +
-				'      <template #infoContent><span>Info content for filter</span></template>' +
-				'	</stats-section-header>' +
-				'   <slot-placeholder v-else :label="\'Slot nagłówka sekcji\'" />' +
-				'</template>' +
-				'<template #overallStatsItem><slot-placeholder /></template>' +
-				'<template #resetBanner>' +
-				'	<stats-reset-banner v-if="args.timeMarker" :timeMarker="args.timeMarker">' +
-				'		<template #infoContent><p>Info content for reset banner</p></template>' +
-				'	</stats-reset-banner>' +
-				'   <slot-placeholder v-else :label="\'Slot banera resetu\'" />' +
-				'</template>' +
-				itemsTemplate +
-				'</stats-layout>',
+			template: `
+		<stats-layout v-bind="args">
+			<template #sectionHeader>
+			   <slot-placeholder label="Slot nagłówka sekcji" />
+			</template>
+			<template #overallStatsItem><slot-placeholder /></template>
+			<template #resetBanner>
+			   <slot-placeholder label="Slot banera resetu" />
+			</template>
+			${prepareItemsTemplate()}
+		</stats-layout>
+		`,
 		};
 	},
+	args: {
+		hasGridHeader: true,
+		hasRightColumn: true,
+		leftColumnLabel: 'Zakres',
+		rightColumnLabel: 'Wyniki',
+		statsItemsHeaderLabel: '{Nazwa użytej taksonomii}',
+		isLoading: false,
+		hasError: false,
+	} as Args,
+	argTypes: {
+		hasGridHeader: {
+			control: 'boolean',
+		},
+		hasRightColumn: {
+			control: 'boolean',
+		},
+		leftColumnLabel: {
+			control: 'text',
+		},
+		rightColumnLabel: {
+			control: 'text',
+		},
+		statsItemsHeaderLabel: {
+			control: 'text',
+		},
+		isLoading: {
+			control: 'boolean',
+		},
+		hasError: {
+			control: 'boolean',
+		},
+	} as ArgTypes,
 	parameters: {
 		design: {
 			type: 'figma',
@@ -66,98 +82,75 @@ export default meta;
 
 type Story = StoryObj<StatsLayoutProps>;
 
-export const Raw: Story = {
-	args: {
-		hasGridHeader: true,
-		hasRightColumn: true,
-		leftColumnLabel: 'Zakres',
-		rightColumnLabel: 'Wyniki',
-		statsItemsHeaderLabel: '{Nazwa użytej taksonomii}',
-		isLoading: false,
-		hasError: false,
-		items: [0, 0, 0],
-	} as Args,
-};
-
-Raw.argTypes = {
-	hasGridHeader: {
-		control: 'boolean',
-	},
-	hasRightColumn: {
-		control: 'boolean',
-	},
-	leftColumnLabel: {
-		control: 'text',
-	},
-	rightColumnLabel: {
-		control: 'text',
-	},
-	statsItemsHeaderLabel: {
-		control: 'text',
-	},
-	isLoading: {
-		control: 'boolean',
-	},
-	hasError: {
-		control: 'boolean',
-	},
-	items: {
-		control: 'object',
-	},
-} as ArgTypes;
+export const Raw: Story = {};
 
 export const WithHeaders: Story = {
-	args: {
-		hasGridHeader: true,
-		hasRightColumn: true,
-		leftColumnLabel: 'Zakres',
-		rightColumnLabel: 'Wyniki',
-		statsItemsHeaderLabel: '{Nazwa użytej taksonomii}',
-		isLoading: false,
-		hasError: false,
-		items: [0, 0, 0],
-		filterItems: [
-			{ key: 'all', label: 'Wszystkie' },
-			{ key: 'week', label: 'Tydzień' },
-			{ key: 'month', label: 'Miesiąc' },
-		],
-		selectedFilterKey: 'all',
-		timeMarker: '6 czerwca 2025',
-	} as Args,
+	render: (args) => {
+		return {
+			components: { StatsLayout, StatsSectionHeader, StatsResetBanner, SlotPlaceholder },
+			setup() {
+				return {
+					args,
+					filterItems: [
+						{ key: 'all', label: 'Wszystkie' },
+						{ key: 'week', label: 'Tydzień' },
+						{ key: 'month', label: 'Miesiąc' },
+					],
+				};
+			},
+			template: `
+			<stats-layout v-bind="args">
+				<template #sectionHeader>
+					<stats-section-header :filter-items="filterItems" selected-filter-key="all">
+						<template #infoContent><span>Info content for filter</span></template>
+					</stats-section-header>
+				</template>
+				<template #overallStatsItem><slot-placeholder /></template>
+				<template #resetBanner>
+					<stats-reset-banner timeMarker="2 czerwca 2025">
+						<template #infoContent><p>Info content for reset banner</p></template>
+					</stats-reset-banner>
+				</template>
+				${prepareItemsTemplate()}
+			</stats-layout>
+`,
+		};
+	},
 };
 
-WithHeaders.argTypes = {
-	filterItems: {
-		control: 'object',
+export const MultipleColumns: Story = {
+	render: (args) => {
+		const itemTemplate = `<div style="display: grid; grid-template-columns: 1fr auto; gap: 8px;"><div><slot-placeholder label="Slot left" /></div><div><slot-placeholder label="Slot right" /></div></div>`;
+
+		return {
+			components: { StatsLayout, StatsSectionHeader, StatsResetBanner, SlotPlaceholder },
+			setup() {
+				return {
+					args,
+					filterItems: [
+						{ key: 'all', label: 'Wszystkie' },
+						{ key: 'week', label: 'Tydzień' },
+						{ key: 'month', label: 'Miesiąc' },
+					],
+				};
+			},
+			template: `
+			<stats-layout v-bind="args">
+				<template #sectionHeader>
+					<stats-section-header :filter-items="filterItems" selected-filter-key="all">
+						<template #infoContent><span>Info content for filter</span></template>
+					</stats-section-header>
+				</template>
+				<template #overallStatsItem>
+				${itemTemplate}
+				</template>
+				<template #resetBanner>
+					<stats-reset-banner timeMarker="2 czerwca 2025">
+						<template #infoContent><p>Info content for reset banner</p></template>
+					</stats-reset-banner>
+				</template>
+				${prepareItemsTemplate(5, itemTemplate)}
+			</stats-layout>`,
+		};
 	},
-	selectedFilterKey: {
-		control: 'text',
-	},
-	timeMarker: {
-		control: 'text',
-	},
-	hasGridHeader: {
-		control: 'boolean',
-	},
-	hasRightColumn: {
-		control: 'boolean',
-	},
-	leftColumnLabel: {
-		control: 'text',
-	},
-	rightColumnLabel: {
-		control: 'text',
-	},
-	statsItemsHeaderLabel: {
-		control: 'text',
-	},
-	isLoading: {
-		control: 'boolean',
-	},
-	hasError: {
-		control: 'boolean',
-	},
-	items: {
-		control: 'object',
-	},
-} as ArgTypes;
+};
