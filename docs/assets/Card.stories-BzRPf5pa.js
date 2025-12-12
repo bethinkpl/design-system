c9d6bffe649df2fb0dc5b1a611a9a118b94810ed
tweetgeek <4587534+tweetgeek@users.noreply.github.com>
2025-12-12 10:33:01 +0000

    Deploying to master from @ bethinkpl/design-system@97db6e24580bc8c38901e125a6cf7f189b32594f 🚀

--- docs/assets/Card.stories-BzRPf5pa.js
+++ docs/assets/Card.stories-BzRPf5pa.js
@@ -1,4 +1,4 @@
-import{C as a,a as n,b as s,c as l,d}from"./Card-7BNJhV7j.js";import{L as i}from"./LoadingBar-D6RhehAD.js";import"./vue.esm-bundler-C98R7Ujk.js";import"./Divider-B4IxPXqZ.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./ContainerRibbon-CT6T0S2o.js";const C={title:"Components/Cards/Card",component:a},p=c=>({components:{Card:a},setup(){return{args:c}},template:`
+import{C as a,a as n,b as s,c as l,d}from"./Card-C8veUiz3.js";import{L as i}from"./LoadingBar-D6RhehAD.js";import"./vue.esm-bundler-C98R7Ujk.js";import"./Divider-B4IxPXqZ.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./ContainerRibbon-CT6T0S2o.js";const C={title:"Components/Cards/Card",component:a},p=c=>({components:{Card:a},setup(){return{args:c}},template:`
         <card v-bind="args">
             <template v-if="args.header" #header>
                 <div v-html="args.header" />
---
+++ docs/assets/SurveyQuestionOpenEnded.stories-dr_mS8xw.js
@@ -0,0 +1,20 @@
+import{S as n}from"./SurveyQuestionOpenEnded-CDOBTIAo.js";import{S as i}from"./string-BELyp4KA.js";import"./Card-C8veUiz3.js";import"./vue.esm-bundler-C98R7Ujk.js";import"./Divider-B4IxPXqZ.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./LoadingBar-D6RhehAD.js";import"./ContainerRibbon-CT6T0S2o.js";import"./Button-qdZYJ3AM.js";import"./Icon-CnR653sL.js";import"./IconButton-C30Nb-iK.js";import"./device-9fgosCm4.js";import"./useLegacyI18n-Cgi06QOG.js";const{useArgs:l}=__STORYBOOK_MODULE_PREVIEW_API__,T={title:"Components/SurveyQuestions/SurveyQuestionOpenEnded",component:n},d=r=>{const[c,s]=l();return{components:{SurveyQuestionOpenEnded:n},setup(){return r},template:'<survey-question-open-ended  :title="title" :value="value" :state="state" :placeholder="placeholder" @input="explanationUpdate"><template v-if="explanation" #explanation><div v-html="explanation" /></template></survey-question-open-ended>',methods:{explanationUpdate(p){s({value:p})}}}},e=d.bind({}),u={title:"Main question write here if it's long it will collapse.",value:"value",explanation:'<h3 class="modalHeader" style="text-align: center; margin-bottom: 16px;">Jak ocenić, czy slajdy i diagramy były zrozumiałe?</h3><div>Wyczerpujące materiały dają poczucie pełnego zrozumieina, przy jednoczesnym usystematyzowaniu informacji. Wpływa na to nie tylko ich jakość, ale też ilość.</div>',placeholder:"Wpisz swoją odpowiedź",state:i.DEFAULT},m={explanation:{control:"text"},state:{control:"select",options:Object.values(i)}};e.argTypes=m;e.args=u;e.parameters={design:{type:"figma",url:"https://www.figma.com/file/izQdYyiBR1GQgFkaOIfIJI/LMS---DS---Components?node-id=1555%3A41739"}};var t,a,o;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`args => {
+  const [_, updateArgs] = useArgs();
+  return {
+    components: {
+      SurveyQuestionOpenEnded
+    },
+    setup() {
+      return args;
+    },
+    // TODO typing in textarea looses focus
+    template: '<survey-question-open-ended  :title="title" :value="value" :state="state" :placeholder="placeholder" @input="explanationUpdate">' + '<template v-if="explanation" #explanation><div v-html="explanation" /></template>' + '</survey-question-open-ended>',
+    methods: {
+      explanationUpdate(value) {
+        updateArgs({
+          value
+        });
+      }
+    }
+  };
+}`,...(o=(a=e.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};const j=["Interactive"];export{e as Interactive,j as __namedExportsOrder,T as default};
