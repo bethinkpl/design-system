5af0eed084c4433dbefb6e357501ee08be169361
latata <karol.tatala@gmail.com>
2026-01-21 10:46:09 +0000

    Deploying to master from @ bethinkpl/design-system@226242876f6883c95f22c264ee61dad8609e8708 🚀

--- dist/lib/js/components/RichList/BasicRichListItem/BasicRichListItem.vue.d.ts
+++ dist/lib/js/components/RichList/BasicRichListItem/BasicRichListItem.vue.d.ts
@@ -286,306 +286,188 @@ declare const _default: import('vue').DefineComponent<import('vue').ExtractPropT
     isEyebrowUppercase: boolean;
     isTextGroupSelected: boolean;
 }, {}, {
-    DsTextGroup: import('vue').DefineComponent<import('vue').ExtractPropTypes<{
+    DsTextGroup: {
+        new (...args: any[]): import('vue').CreateComponentPublicInstanceWithMixins<Readonly<import('vue').ExtractPropTypes<{
+            size: {
+                type: PropType<string>;
+            };
+            prominence: {
+                type: PropType<import('../../TextGroup').TextGroupProminence>;
+            };
+            eyebrowText: {
+                type: PropType<string | null>;
+            };
+            eyebrowTextEllipsis: {
+                type: PropType<boolean>;
+            };
+            isEyebrowTextUppercase: {
+                type: PropType<boolean>;
+            };
+            hasEyebrowMask: {
+                type: PropType<boolean>;
+            };
+            mainText: {
+                type: PropType<string | null>;
+            };
+            mainTextEllipsis: {
+                type: PropType<boolean>;
+            };
+            supportingText: {
+                type: PropType<string | null>;
+            };
+            supportingTextEllipsis: {
+                type: PropType<boolean>;
+            };
+            isInteractive: {
+                type: PropType<boolean>;
+            };
+            skeletonLoadingSize: {
+                type: PropType<string>;
+            };
+            isSelected: {
+                type: PropType<boolean>;
+            };
+            state: {
+                type: PropType<TextGroupState>;
+            };
+            isSupportingTextTooltipEnabled: {
+                type: PropType<boolean>;
+            };
+            isSupportingTextTooltipEnabledOnMobile: {
+                type: PropType<boolean>;
+            };
+            isSupportingTextTooltipAutoFilledWithContent: {
+                type: PropType<boolean>;
+            };
+            supportingTextTooltipContent: {
+                type: PropType<string>;
+            };
+        }>> & Readonly<{}>, {}, {}, {}, {}, import('vue').ComponentOptionsMixin, import('vue').ComponentOptionsMixin, {}, import('vue').PublicProps, {}, true, {}, {}, import('vue').GlobalComponents, import('vue').GlobalDirectives, string, {}, any, import('vue').ComponentProvideOptions, {
+            P: {};
+            B: {};
+            D: {};
+            C: {};
+            M: {};
+            Defaults: {};
+        }, Readonly<import('vue').ExtractPropTypes<{
+            size: {
+                type: PropType<string>;
+            };
+            prominence: {
+                type: PropType<import('../../TextGroup').TextGroupProminence>;
+            };
+            eyebrowText: {
+                type: PropType<string | null>;
+            };
+            eyebrowTextEllipsis: {
+                type: PropType<boolean>;
+            };
+            isEyebrowTextUppercase: {
+                type: PropType<boolean>;
+            };
+            hasEyebrowMask: {
+                type: PropType<boolean>;
+            };
+            mainText: {
+                type: PropType<string | null>;
+            };
+            mainTextEllipsis: {
+                type: PropType<boolean>;
+            };
+            supportingText: {
+                type: PropType<string | null>;
+            };
+            supportingTextEllipsis: {
+                type: PropType<boolean>;
+            };
+            isInteractive: {
+                type: PropType<boolean>;
+            };
+            skeletonLoadingSize: {
+                type: PropType<string>;
+            };
+            isSelected: {
+                type: PropType<boolean>;
+            };
+            state: {
+                type: PropType<TextGroupState>;
+            };
+            isSupportingTextTooltipEnabled: {
+                type: PropType<boolean>;
+            };
+            isSupportingTextTooltipEnabledOnMobile: {
+                type: PropType<boolean>;
+            };
+            isSupportingTextTooltipAutoFilledWithContent: {
+                type: PropType<boolean>;
+            };
+            supportingTextTooltipContent: {
+                type: PropType<string>;
+            };
+        }>> & Readonly<{}>, {}, {}, {}, {}, {}>;
+        __isFragment?: never;
+        __isTeleport?: never;
+        __isSuspense?: never;
+    } & import('vue').ComponentOptionsBase<Readonly<import('vue').ExtractPropTypes<{
         size: {
-            type: PropType<TextGroupSize>;
-            default: string;
+            type: PropType<string>;
         };
-        color: {
-            type: PropType<import('../../TextGroup').TextGroupColor>;
-            default: "neutral";
+        prominence: {
+            type: PropType<import('../../TextGroup').TextGroupProminence>;
         };
         eyebrowText: {
             type: PropType<string | null>;
-            default: null;
         };
         eyebrowTextEllipsis: {
-            type: BooleanConstructor;
-            default: boolean;
+            type: PropType<boolean>;
         };
         isEyebrowTextUppercase: {
-            type: BooleanConstructor;
-            default: boolean;
+            type: PropType<boolean>;
+        };
+        hasEyebrowMask: {
+            type: PropType<boolean>;
         };
         mainText: {
             type: PropType<string | null>;
-            default: null;
         };
         mainTextEllipsis: {
-            type: BooleanConstructor;
-            default: boolean;
+            type: PropType<boolean>;
         };
         supportingText: {
             type: PropType<string | null>;
-            default: null;
         };
         supportingTextEllipsis: {
-            type: BooleanConstructor;
-            default: boolean;
+            type: PropType<boolean>;
         };
         isInteractive: {
-            type: BooleanConstructor;
-            default: boolean;
+            type: PropType<boolean>;
         };
         skeletonLoadingSize: {
-            type: PropType<import('../../TextGroup').TextGroupLoadingSize>;
-            default: string;
+            type: PropType<string>;
         };
         isSelected: {
-            type: BooleanConstructor;
-            default: boolean;
+            type: PropType<boolean>;
         };
         state: {
             type: PropType<TextGroupState>;
-            default: "default";
         };
         isSupportingTextTooltipEnabled: {
-            type: BooleanConstructor;
-            default: boolean;
+            type: PropType<boolean>;
         };
         isSupportingTextTooltipEnabledOnMobile: {
-            type: BooleanConstructor;
-            default: boolean;
+            type: PropType<boolean>;
         };
         isSupportingTextTooltipAutoFilledWithContent: {
-            type: BooleanConstructor;
-            default: boolean;
+            type: PropType<boolean>;
         };
         supportingTextTooltipContent: {
-            type: StringConstructor;
-            default: null;
+            type: PropType<string>;
         };
-    }>, {}, {
-        TEXT_GROUP_SIZES: Readonly<{
-            SMALL: string;
-            MEDIUM: string;
-        }>;
-        TEXT_GROUP_STATES: Readonly<{
-            readonly DEFAULT: "default";
-            readonly HOVERED: "hovered";
-            readonly DISABLED: "disabled";
-            readonly LOADING: "loading";
-        }>;
-    }, {
-        colorClassName(): string;
-        isLoading(): boolean;
-        loadingSizeClassName(): string;
-    }, {}, import('vue').ComponentOptionsMixin, import('vue').ComponentOptionsMixin, {}, string, import('vue').PublicProps, Readonly<import('vue').ExtractPropTypes<{
-        size: {
-            type: PropType<TextGroupSize>;
-            default: string;
+    }>> & Readonly<{}>, {}, {}, {}, {}, import('vue').ComponentOptionsMixin, import('vue').ComponentOptionsMixin, {}, string, {}, {}, string, {}, import('vue').GlobalComponents, import('vue').GlobalDirectives, string, import('vue').ComponentProvideOptions> & import('vue').VNodeProps & import('vue').AllowedComponentProps & import('vue').ComponentCustomProps & (new () => {
+        $slots: {
+            mainText?(_: {}): any;
         };
-        color: {
-            type: PropType<import('../../TextGroup').TextGroupColor>;
-            default: "neutral";
-        };
-        eyebrowText: {
-            type: PropType<string | null>;
-            default: null;
-        };
-        eyebrowTextEllipsis: {
-            type: BooleanConstructor;
-            default: boolean;
-        };
-        isEyebrowTextUppercase: {
-            type: BooleanConstructor;
-            default: boolean;
-        };
-        mainText: {
-            type: PropType<string | null>;
-            default: null;
-        };
-        mainTextEllipsis: {
-            type: BooleanConstructor;
-            default: boolean;
-        };
-        supportingText: {
-            type: PropType<string | null>;
-            default: null;
-        };
-        supportingTextEllipsis: {
-            type: BooleanConstructor;
-            default: boolean;
-        };
-        isInteractive: {
-            type: BooleanConstructor;
-            default: boolean;
-        };
-        skeletonLoadingSize: {
-            type: PropType<import('../../TextGroup').TextGroupLoadingSize>;
-            default: string;
-        };
-        isSelected: {
-            type: BooleanConstructor;
-            default: boolean;
-        };
-        state: {
-            type: PropType<TextGroupState>;
-            default: "default";
-        };
-        isSupportingTextTooltipEnabled: {
-            type: BooleanConstructor;
-            default: boolean;
-        };
-        isSupportingTextTooltipEnabledOnMobile: {
-            type: BooleanConstructor;
-            default: boolean;
-        };
-        isSupportingTextTooltipAutoFilledWithContent: {
-            type: BooleanConstructor;
-            default: boolean;
-        };
-        supportingTextTooltipContent: {
-            type: StringConstructor;
-            default: null;
-        };
-    }>> & Readonly<{}>, {
-        size: string;
-        color: "neutral";
-        state: TextGroupState;
-        isInteractive: boolean;
-        eyebrowText: string | null;
-        isEyebrowTextUppercase: boolean;
-        supportingText: string | null;
-        isSelected: boolean;
-        eyebrowTextEllipsis: boolean;
-        mainText: string | null;
-        mainTextEllipsis: boolean;
-        supportingTextEllipsis: boolean;
-        skeletonLoadingSize: string;
-        isSupportingTextTooltipEnabled: boolean;
-        isSupportingTextTooltipEnabledOnMobile: boolean;
-        isSupportingTextTooltipAutoFilledWithContent: boolean;
-        supportingTextTooltipContent: string;
-    }, {}, {
-        DsSkeleton: import('vue').DefineComponent<import('vue').ExtractPropTypes<{
-            height: {
-                type: StringConstructor;
-                default: string;
-            };
-            radius: {
-                type: PropType<import('../../..').SkeletonRadiusSize>;
-                default: string;
-            };
-            width: {
-                type: StringConstructor;
-                default: string;
-            };
-        }>, {}, {
-            designTokens: {
-                animation: {
-                    background: string;
-                };
-                background: string;
-            };
-        }, {
-            radiusTranslated(): string;
-        }, {}, import('vue').ComponentOptionsMixin, import('vue').ComponentOptionsMixin, {}, string, import('vue').PublicProps, Readonly<import('vue').ExtractPropTypes<{
-            height: {
-                type: StringConstructor;
-                default: string;
-            };
-            radius: {
-                type: PropType<import('../../..').SkeletonRadiusSize>;
-                default: string;
-            };
-            width: {
-                type: StringConstructor;
-                default: string;
-            };
-        }>> & Readonly<{}>, {
-            radius: string;
-            width: string;
-            height: string;
-        }, {}, {
-            PrimeSkeleton: import('@primevue/core').DefineComponent<import('primevue/skeleton').SkeletonProps, import('primevue/skeleton').SkeletonSlots, (e: string, ...args: any[]) => void>;
-        }, {}, string, import('vue').ComponentProvideOptions, true, {}, any>;
-        DsTooltip: import('vue').DefineComponent<import('vue').ExtractPropTypes<{
-            placement: {
-                type: PropType<import('../../Tooltip').TooltipPlacement>;
-                default: "bottom";
-                validator(placement: import('../../Tooltip').TooltipPlacement): boolean;
-            };
-            text: {
-                type: StringConstructor;
-                default: string;
-            };
-            isDisabled: {
-                type: BooleanConstructor;
-                default: boolean;
-            };
-            isPointerVisible: {
-                type: BooleanConstructor;
-                default: boolean;
-            };
-            inline: {
-                type: BooleanConstructor;
-                default: boolean;
-            };
-            isHiddenOnMobile: {
-                type: BooleanConstructor;
-                default: boolean;
-            };
-        }>, {}, {}, {
-            tooltipParams(): {
-                position: any;
-                event: string;
-            };
-            options(): {
-                value: any;
-                disabled: any;
-                dt: {
-                    shadow: string;
-                    padding: string;
-                    background: string;
-                    color: string;
-                    borderRadius: string;
-                    maxWidth: string;
-                };
-                class: string | null;
-                ptOptions: {
-                    mergeProps: boolean;
-                };
-                pt: {
-                    text: string;
-                    arrow: string;
-                };
-            };
-        }, {}, import('vue').ComponentOptionsMixin, import('vue').ComponentOptionsMixin, {}, string, import('vue').PublicProps, Readonly<import('vue').ExtractPropTypes<{
-            placement: {
-                type: PropType<import('../../Tooltip').TooltipPlacement>;
-                default: "bottom";
-                validator(placement: import('../../Tooltip').TooltipPlacement): boolean;
-            };
-            text: {
-                type: StringConstructor;
-                default: string;
-            };
-            isDisabled: {
-                type: BooleanConstructor;
-                default: boolean;
-            };
-            isPointerVisible: {
-                type: BooleanConstructor;
-                default: boolean;
-            };
-            inline: {
-                type: BooleanConstructor;
-                default: boolean;
-            };
-            isHiddenOnMobile: {
-                type: BooleanConstructor;
-                default: boolean;
-            };
-        }>> & Readonly<{}>, {
-            text: string;
-            placement: import('../../Tooltip').TooltipPlacement;
-            isDisabled: boolean;
-            isPointerVisible: boolean;
-            inline: boolean;
-            isHiddenOnMobile: boolean;
-        }, {}, {}, {}, string, import('vue').ComponentProvideOptions, true, {}, any>;
-    }, {}, string, import('vue').ComponentProvideOptions, true, {}, any>;
+    });
     RichListItem: import('vue').DefineComponent<import('vue').ExtractPropTypes<{
         type: {
             type: PropType<RichListItemType>;
