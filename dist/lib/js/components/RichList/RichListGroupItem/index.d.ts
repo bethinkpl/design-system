2342b2c2e02590351f47d011410eb03be964e4fb
t-tomalak <12939493+t-tomalak@users.noreply.github.com>
2026-01-26 11:24:47 +0000

    Build dist

--- dist/lib/js/components/RichList/RichListGroupItem/index.d.ts
+++
@@ -1,3 +0,0 @@
-import RichListGroupItem from './RichListGroupItem.vue';
-export default RichListGroupItem;
-export * from './RichListGroupItem.consts';
--- dist/lib/js/components/SurveyQuestions/SurveyQuestionOpenEnded/SurveyQuestionOpenEnded.stories.d.ts
+++
@@ -1,5 +0,0 @@
-import SurveyQuestionOpenEnded from './SurveyQuestionOpenEnded.vue';
-import { Meta, StoryFn } from '@storybook/vue3';
-declare const _default: Meta<typeof SurveyQuestionOpenEnded>;
-export default _default;
-export declare const Interactive: StoryFn<typeof SurveyQuestionOpenEnded>;
