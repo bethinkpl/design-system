61e27dd8298293e62500dc5118034aa999c3554e
tomaszperkowski <91724331+tomaszperkowski@users.noreply.github.com>
2026-02-13 08:02:50 +0000

    Deploying to master from @ bethinkpl/design-system@1391872a2d2510af0000d0df10e769680c180fce 🚀

--- dist/lib/js/components/Drawer/DrawerSection/DrawerSection.vue.d.ts
+++ dist/lib/js/components/Drawer/DrawerSection/DrawerSection.vue.d.ts
@@ -204,6 +204,7 @@ declare const _default: import('vue').DefineComponent<import('vue').ExtractPropT
         readonly FA_FACE_MEH: import('@fortawesome/fontawesome-common-types').IconDefinition;
         readonly FA_FACE_SMILE: import('@fortawesome/fontawesome-common-types').IconDefinition;
         readonly FA_FACEBOOK: import('@fortawesome/fontawesome-common-types').IconDefinition;
+        readonly FA_FILE_ARROW_DOWN: import('@fortawesome/fontawesome-common-types').IconDefinition;
         readonly FA_FILE_CHECK: import('@fortawesome/fontawesome-common-types').IconDefinition;
         readonly FA_FILE_INVOICE_DOLLAR: import('@fortawesome/fontawesome-common-types').IconDefinition;
         readonly FA_FILE_LINES_SOLID: import('@fortawesome/fontawesome-common-types').IconDefinition;
@@ -617,6 +618,7 @@ declare const _default: import('vue').DefineComponent<import('vue').ExtractPropT
             readonly FA_FACE_MEH: import('@fortawesome/fontawesome-common-types').IconDefinition;
             readonly FA_FACE_SMILE: import('@fortawesome/fontawesome-common-types').IconDefinition;
             readonly FA_FACEBOOK: import('@fortawesome/fontawesome-common-types').IconDefinition;
+            readonly FA_FILE_ARROW_DOWN: import('@fortawesome/fontawesome-common-types').IconDefinition;
             readonly FA_FILE_CHECK: import('@fortawesome/fontawesome-common-types').IconDefinition;
             readonly FA_FILE_INVOICE_DOLLAR: import('@fortawesome/fontawesome-common-types').IconDefinition;
             readonly FA_FILE_LINES_SOLID: import('@fortawesome/fontawesome-common-types').IconDefinition;
@@ -1131,6 +1133,7 @@ declare const _default: import('vue').DefineComponent<import('vue').ExtractPropT
                 readonly FA_FACE_MEH: import('@fortawesome/fontawesome-common-types').IconDefinition;
                 readonly FA_FACE_SMILE: import('@fortawesome/fontawesome-common-types').IconDefinition;
                 readonly FA_FACEBOOK: import('@fortawesome/fontawesome-common-types').IconDefinition;
+                readonly FA_FILE_ARROW_DOWN: import('@fortawesome/fontawesome-common-types').IconDefinition;
                 readonly FA_FILE_CHECK: import('@fortawesome/fontawesome-common-types').IconDefinition;
                 readonly FA_FILE_INVOICE_DOLLAR: import('@fortawesome/fontawesome-common-types').IconDefinition;
                 readonly FA_FILE_LINES_SOLID: import('@fortawesome/fontawesome-common-types').IconDefinition;
