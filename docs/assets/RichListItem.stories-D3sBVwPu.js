e4b10f625478361091a11222c8e0d1d138b9808e
latata <karol.tatala@gmail.com>
2026-03-31 04:34:04 +0000

    Deploying to master from @ bethinkpl/design-system@a117a035769e766e7abf6769e96b18db69d0a73e 🚀

---
+++ docs/assets/RichListItem.stories-D3sBVwPu.js
@@ -0,0 +1,182 @@
+import{h as n,R as _,a as l,b as f,c,d as m,e as d,f as p}from"./GroupRichListItem-D-5FoLXy.js";import{I as s,b as g}from"./Icon-D12JEqJp.js";import"./Avatar-DparM4Na.js";import"./Badge-DK5J9aEp.js";import"./BadgeScore-CHmAcuEs.js";import"./Banner-jWK5cndj.js";import"./Button-BxFGU0Y8.js";import"./ToggleButton-D2RtKA8b.js";import"./Card-BUsalfPo.js";import"./ContainerRibbon-DIpKpXqT.js";import"./CardExpandable-BkgTYvAe.js";import"./Checkbox-CTaQ6uVS.js";import"./CheckboxGroupField-Dh_V1z-6.js";import"./DatePicker-BlRyDF_q.js";import"./DateRangePicker-DVs4Z0W8.js";import"./Divider-CsYapLKZ.js";import"./FeatureIcon-BDYqSNGt.js";import"./FormField-R1QF8eWU.js";import"./FormFieldMessage-CwR_lYqE.js";import"./HelpButton-CYtYNx9d.js";import"./IconButton-8rTo90xc.js";import"./InputField-Db9tKvuw.js";import"./string-d6eLSJAQ.js";import"./Modal-juCwE0yW.js";import"./ModalDialog-BngrMQ6A.js";import"./NumberInCircle-BVKXUHa3.js";import"./PasswordField-CBIcoGbI.js";import"./TabItem-CJROwCIg.js";import"./Tile-OuYkluV7.js";import"./Toast-C8Ulaatj.js";import"./AccessStatus-DrmNlugI.js";import"./BlockadeStatus-Da19Mjz4.js";import"./SurveyToggle-CjVT4HUa.js";import"./SurveyQuestionScale-Dq30rApG.js";import"./SurveyQuestionOpenEnded-CNja62wn.js";import"./SectionHeader-C4PSKWFL.js";import"./PageHeader-LlLvRT6S.js";import"./SectionTitle-DElpdw3G.js";import"./Drawer-Dt1KI_7m.js";import"./DrawerContent-CHnQanlg.js";import"./DrawerDivider-BTkU_vqO.js";import"./DrawerHeader-CofbD8QK.js";import"./DrawerListItem-Bw6TkXbw.js";import"./DrawerListItemGroup-BUd--694.js";import"./DrawerTile-BSp0I6cw.js";import"./DrawerSection-Cb_KVSXI.js";import"./MenuItem-yB1dHLaj.js";import"./MenuDivider-D93GaCei.js";import"./Chip-yV1i3zPV.js";import"./CounterToggle-BitGnUcY.js";import"./SelectList-D6U7eKfK.js";import"./SelectListItem-DoBkebAc.js";import"./SelectListItemDivider-BglVKWEB.js";import"./SelectListItemToggle-BUq0OLd9.js";import"./SelectListItemTile-DWuww1id.js";import"./SelectListSectionTitle-CtH0aIHG.js";import"./SelectionTile-BkNseRgM.js";import"./LoadingBar-Fdravevs.js";import"./PopOver-Byym7Z9P.js";import"./Dropdown-DROiglkN.js";import"./ProgressBar-DzPynoL-.js";import"./ProgressDonutChart-Cz2v5dft.js";import"./IconText-DPK25wYG.js";import"./Pagination-BmtcEpU5.js";import"./OverlayHeader-B-dpyPoO.js";import"./Well-6xekwkE1.js";import"./ThreeColumnLayout-BUGPOXQu.js";import"./BasicRichListItem-D3MQySXf.js";import"./Switch-D4bb3yGJ.js";import"./TextGroup-ChPw6rUC.js";import"./Tooltip-Do-QYl8p.js";import{I as y}from"./Image-iA_E3Cco.js";import"./Skeleton-CA38Yv_j.js";import"./StatsLayout-BI7m20wU.js";import"./StatsResetBanner--kmS_FLO.js";import"./StatsSectionHeader-6Tf2l9OO.js";import{w as x}from"./decorator-dmUe8Ksx.js";import"./vue.esm-bundler-BEu-HfQJ.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./index-yKlVThWI.js";import"./useFormFieldWithinForm-j7qLaE29.js";import"./useLegacyI18n-D8xn4HbG.js";import"./iframe-DlQfUE5-.js";import"./device-9fgosCm4.js";import"./user-_JB5aHqq.js";import"./RadioButton-DbSfUkPg.js";import"./vue-popper-B9Lw6d0S.js";import"./ProgressBarLegendItem-I_FHRLjE.js";import"./ProgressBarLegend-DygAezlC.js";import"./_commonjsHelpers-D6Ya60D-.js";import"./StatsErrorBanner-k8lN807S.js";import"./SpinnerLoading-DpD4t-BV.js";import"./StatsFiltersDropdown-ANVUffq9.js";import"./v4-CwV2VaSl.js";const{useArgs:S}=__STORYBOOK_MODULE_PREVIEW_API__,ut={title:"Components/RichList/RichListItem",component:n,decorators:[e=>({components:{story:e},template:"<div style='display: flex;padding: 16px;'><story /></div>"}),x]},O=(e,i={})=>(e.argTypes={type:{options:Object.values(p),control:"select"},size:{options:Object.values(d),control:"select"},layout:{options:Object.values(m),control:{type:"select"}},state:{options:Object.values(c),control:"select"},iconColor:{options:Object.values(g),control:"select"},borderColor:{options:[null,...Object.values(f)],control:"select"},icon:{options:[null,...Object.keys(s)],control:"select"},iconColorHex:{control:"text"},borderColorHex:{control:"text"},content:{control:"text"},metadata:{control:"text"},actions:{control:"text"},draggableIconClassName:{control:"text"},backgroundColor:{options:[null,...Object.values(l)],control:"select"},elevation:{options:[null,...Object.values(_)],control:"select"}},e.args={size:d.MEDIUM,type:p.DEFAULT,layout:m.HORIZONTAL,backgroundColor:l.NEUTRAL,elevation:null,isDimmed:!1,isDraggable:!0,hasDraggableHandler:!0,icon:null,iconColor:g.NEUTRAL_WEAK,iconColorHex:"",hasActionsSlotDivider:!0,isSelectable:!0,isSelected:!0,borderColor:null,borderColorHex:"",state:c.DEFAULT,isInteractive:!0,draggableIconClassName:"draggableIconClassName-1",content:"Content Slot",metadata:"Metadata Slot",actions:"ACS",...i},e.parameters={actions:{handles:["icon-click","click","update:is-selected"]},layout:"fullscreen",design:{type:"figma",url:"https://www.figma.com/file/izQdYyiBR1GQgFkaOIfIJI/LMS---DS-Components?type=design&node-id=8507-129603&mode=design&t=dC5vBzU9RlP1j7G2-4"}},e),D=e=>{const[i,r]=S();return{components:{RichListItem:n},setup(){return e},data(){return{ICONS:Object.freeze(s)}},methods:{updateIsSelected(a){r({isSelected:a})}},template:`
+            <rich-list-item
+                :size="size"
+                :type="type"
+                :layout="layout"
+                :is-interactive="isInteractive"
+                :is-draggable="isDraggable"
+                :icon="ICONS[icon]"
+                :icon-color="iconColor"
+                :icon-color-hex="iconColorHex"
+                :is-dimmed="isDimmed"
+                :border-color="borderColor"
+                :border-color-hex="borderColorHex"
+                :draggable-icon-class-name="draggableIconClassName"
+                :state="state"
+                :background-color="backgroundColor"
+                :elevation="elevation"
+                :has-draggable-handler="hasDraggableHandler"
+                :has-actions-slot-divider="hasActionsSlotDivider"
+                :is-selectable="isSelectable"
+                :is-selected="isSelected"
+                @update:is-selected="updateIsSelected"
+            >
+                <template v-if="content" #content>
+                    <div v-html="content" />
+                </template>
+                <template v-if="metadata" #metadata>
+                    <div v-html="metadata" />
+                </template>
+                <template v-if="actions" #actions>
+                    <div v-html="actions" />
+                </template>
+            </rich-list-item>`}},t=D.bind({});O(t);const T=e=>{const[i,r]=S();return{components:{RichListItem:n,DsImage:y},setup(){return e},data(){return{ICONS:Object.freeze(s)}},methods:{updateIsSelected(a){r({isSelected:a})}},template:`
+            <rich-list-item
+                :size="size"
+                :type="type"
+                :layout="layout"
+                :is-interactive="isInteractive"
+                :is-draggable="isDraggable"
+                :icon="ICONS[icon]"
+                :icon-color="iconColor"
+                :icon-color-hex="iconColorHex"
+                :is-dimmed="isDimmed"
+                :border-color="borderColor"
+                :border-color-hex="borderColorHex"
+                :draggable-icon-class-name="draggableIconClassName"
+                :state="state"
+                :background-color="backgroundColor"
+                :elevation="elevation"
+                :has-draggable-handler="hasDraggableHandler"
+                :has-actions-slot-divider="hasActionsSlotDivider"
+                :is-selectable="isSelectable"
+                :is-selected="isSelected"
+                @update:is-selected="updateIsSelected"
+            >
+                <template #media>
+                    <ds-image :src="imageSrcUsedInStoryBook" />
+                </template>
+                <template v-if="content" #content>
+                    <div v-html="content" />
+                </template>
+                <template v-if="metadata" #metadata>
+                    <div v-html="metadata" />
+                </template>
+                <template v-if="actions" #actions>
+                    <div v-html="actions" />
+                </template>
+            </rich-list-item>`}},o=T.bind({});O(o,{imageSrcUsedInStoryBook:"https://storage.googleapis.com/media-manager/lek/018f6291-3956-7342-8e6b-0ee901d48643/018f6291-3a56-7213-aef6-b5da7253839f.jpg"});var u,b,v;t.parameters={...t.parameters,docs:{...(u=t.parameters)==null?void 0:u.docs,source:{originalSource:`args => {
+  const [_, updateArgs] = useArgs();
+  return {
+    components: {
+      RichListItem
+    },
+    setup() {
+      return args;
+    },
+    data() {
+      return {
+        ICONS: Object.freeze(ICONS)
+      };
+    },
+    methods: {
+      updateIsSelected(isSelected) {
+        updateArgs({
+          isSelected
+        });
+      }
+    },
+    template: \`
+            <rich-list-item
+                :size="size"
+                :type="type"
+                :layout="layout"
+                :is-interactive="isInteractive"
+                :is-draggable="isDraggable"
+                :icon="ICONS[icon]"
+                :icon-color="iconColor"
+                :icon-color-hex="iconColorHex"
+                :is-dimmed="isDimmed"
+                :border-color="borderColor"
+                :border-color-hex="borderColorHex"
+                :draggable-icon-class-name="draggableIconClassName"
+                :state="state"
+                :background-color="backgroundColor"
+                :elevation="elevation"
+                :has-draggable-handler="hasDraggableHandler"
+                :has-actions-slot-divider="hasActionsSlotDivider"
+                :is-selectable="isSelectable"
+                :is-selected="isSelected"
+                @update:is-selected="updateIsSelected"
+            >
+                <template v-if="content" #content>
+                    <div v-html="content" />
+                </template>
+                <template v-if="metadata" #metadata>
+                    <div v-html="metadata" />
+                </template>
+                <template v-if="actions" #actions>
+                    <div v-html="actions" />
+                </template>
+            </rich-list-item>\`
+  };
+}`,...(v=(b=t.parameters)==null?void 0:b.docs)==null?void 0:v.source}}};var I,h,C;o.parameters={...o.parameters,docs:{...(I=o.parameters)==null?void 0:I.docs,source:{originalSource:`args => {
+  const [_, updateArgs] = useArgs();
+  return {
+    components: {
+      RichListItem,
+      DsImage
+    },
+    setup() {
+      return args;
+    },
+    data() {
+      return {
+        ICONS: Object.freeze(ICONS)
+      };
+    },
+    methods: {
+      updateIsSelected(isSelected) {
+        updateArgs({
+          isSelected
+        });
+      }
+    },
+    template: \`
+            <rich-list-item
+                :size="size"
+                :type="type"
+                :layout="layout"
+                :is-interactive="isInteractive"
+                :is-draggable="isDraggable"
+                :icon="ICONS[icon]"
+                :icon-color="iconColor"
+                :icon-color-hex="iconColorHex"
+                :is-dimmed="isDimmed"
+                :border-color="borderColor"
+                :border-color-hex="borderColorHex"
+                :draggable-icon-class-name="draggableIconClassName"
+                :state="state"
+                :background-color="backgroundColor"
+                :elevation="elevation"
+                :has-draggable-handler="hasDraggableHandler"
+                :has-actions-slot-divider="hasActionsSlotDivider"
+                :is-selectable="isSelectable"
+                :is-selected="isSelected"
+                @update:is-selected="updateIsSelected"
+            >
+                <template #media>
+                    <ds-image :src="imageSrcUsedInStoryBook" />
+                </template>
+                <template v-if="content" #content>
+                    <div v-html="content" />
+                </template>
+                <template v-if="metadata" #metadata>
+                    <div v-html="metadata" />
+                </template>
+                <template v-if="actions" #actions>
+                    <div v-html="actions" />
+                </template>
+            </rich-list-item>\`
+  };
+}`,...(C=(h=o.parameters)==null?void 0:h.docs)==null?void 0:C.source}}};const bt=["Interactive","WithMedia"];export{t as Interactive,o as WithMedia,bt as __namedExportsOrder,ut as default};
