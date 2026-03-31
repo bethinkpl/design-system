e737ddc137cc522438d70c57f5195bb6e9c229e2
tomaszperkowski <91724331+tomaszperkowski@users.noreply.github.com>
2026-03-31 12:44:54 +0000

    Build dist

--- dist/lib/js/components/Drawer/DrawerSection/DrawerSection.stories.d.ts
+++
@@ -1,5 +0,0 @@
-import DrawerSection from './DrawerSection.vue';
-import { Meta, StoryFn } from '@storybook/vue3';
-declare const _default: Meta<typeof DrawerSection>;
-export default _default;
-export declare const Interactive: StoryFn<typeof DrawerSection>;
--- dist/lib/js/components/Outline/OutlineSectionHeader/OutlineSectionHeader.stories.d.ts
+++
@@ -1,5 +0,0 @@
-import OutlineSectionHeader from './OutlineSectionHeader.vue';
-import { Meta, StoryFn } from '@storybook/vue3';
-declare const _default: Meta<typeof OutlineSectionHeader>;
-export default _default;
-export declare const Interactive: StoryFn<typeof OutlineSectionHeader>;
