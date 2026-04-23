63719a5830adc55ee16d75c67fc99955ececfff1
latata <karol.tatala@gmail.com>
2026-04-23 06:04:33 +0000

    Deploying to master from @ bethinkpl/design-system@275c723f0bbfba224d5b87529b99fb237815ae4e 🚀

---
+++ docs/assets/PasswordField.stories-Ck8ILg0-.js
@@ -0,0 +1,42 @@
+import{_ as a}from"./PasswordField-Dx-wpVs0.js";import{_ as v}from"./HelpButton-Cfseejif.js";import{M as S}from"./Modal-D0NmI3oy.js";import{I}from"./Icon-Bw3k2jKC.js";import{a as E,b as F}from"./FormField.stories.shared-DGNCgTc3.js";import{F as l}from"./FormFieldMessage-C2iAROo7.js";import{t as _,r as h}from"./vue.esm-bundler-BEu-HfQJ.js";import"./InputField-CGtFvL12.js";import"./FormField-BFP3H169.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./useFormFieldWithinForm-8k7y-WM_.js";import"./IconButton-BFBHPd5g.js";import"./Button-BiWGn995.js";import"./device-mzBmLsxe.js";import"./Tooltip-DlcUlrZL.js";import"./useLegacyI18n-D8xn4HbG.js";import"./FeatureIcon-CDFOAFZx.js";const G={title:"Components/Form/PasswordField",component:a,render:n=>({components:{PasswordField:a,HelpButton:v,Modal:S},setup(){const{help:d,labelAside:c,message:f,fieldStatus:g,...u}=_(n);return{props:h({...u}),labelAside:c,fieldStatus:g,message:f,help:d,FORM_FIELD_STATES:l,ICONS:I}},data:()=>({value:""}),template:`<PasswordField v-bind="props" v-model="value">
+            <template v-if="help" #help>
+                <HelpButton :is-disabled="props.state === FORM_FIELD_STATES.DISABLED">
+                    <template #modal="{onClose}">
+                        <Modal @close-modal="onClose">Modal</Modal>
+                    </template>
+                </HelpButton>
+            </template>
+            <template #labelAside v-if="labelAside">
+                <div v-html="labelAside" />
+            </template>
+            <template #fieldStatus v-if="fieldStatus">
+                <div v-html="fieldStatus" />
+            </template>
+            <template #message v-if="message">
+                <div v-if="message" v-html="message" />
+            </template>
+            <template #action v-if="action">
+                <div v-html="action" />
+            </template>
+        </PasswordField>`}),argTypes:{...E,suffixText:{control:"text"}}},e={args:{...F,suffixText:"%",inputProps:{placeholder:"Placeholder"}},parameters:{design:{type:"figma",url:"https://www.figma.com/design/izQdYyiBR1GQgFkaOIfIJI/LMS---DS-Components?node-id=13576-21478&m=dev"}}},t={...e,args:{...e.args,state:l.ERROR,messageText:"Error message text"}};var s,r,o;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    ...args,
+    suffixText: '%',
+    inputProps: {
+      placeholder: 'Placeholder'
+    }
+  },
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/izQdYyiBR1GQgFkaOIfIJI/LMS---DS-Components?node-id=13576-21478&m=dev'
+    }
+  }
+}`,...(o=(r=e.parameters)==null?void 0:r.docs)==null?void 0:o.source}}};var m,i,p;t.parameters={...t.parameters,docs:{...(m=t.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  ...Interactive,
+  args: {
+    ...Interactive.args,
+    state: FORM_FIELD_STATES.ERROR,
+    messageText: 'Error message text'
+  }
+}`,...(p=(i=t.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};const J=["Interactive","Error"];export{t as Error,e as Interactive,J as __namedExportsOrder,G as default};
