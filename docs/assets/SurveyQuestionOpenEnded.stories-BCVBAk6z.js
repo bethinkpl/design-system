a21239fbebed40fbc982d82d5ea4d844c89ee741
latata <karol.tatala@gmail.com>
2026-04-24 10:36:20 +0000

    Deploying to master from @ bethinkpl/design-system@eb0eaccf84f903208dfb6bf8925189a274185439 🚀

---
+++ docs/assets/SurveyQuestionOpenEnded.stories-BCVBAk6z.js
@@ -0,0 +1,20 @@
+import{S as n}from"./SurveyQuestionOpenEnded-TriS7904.js";import{S as r}from"./string-DO42DwN6.js";import"./Card-BW_Pjrwo.js";import"./vue.esm-bundler-BEu-HfQJ.js";import"./Divider-Cj9DnfEY.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./LoadingBar-Fdravevs.js";import"./ContainerRibbon-DIpKpXqT.js";import"./Button-BiWGn995.js";import"./Icon-Bw3k2jKC.js";import"./IconButton-HFcQnm17.js";import"./device-mzBmLsxe.js";import"./useLegacyI18n-D8xn4HbG.js";const{useArgs:l}=__STORYBOOK_MODULE_PREVIEW_API__,I={title:"Components/SurveyQuestions/SurveyQuestionOpenEnded",component:n},u=i=>{const[c,s]=l();return{components:{SurveyQuestionOpenEnded:n},setup(){return i},template:`<survey-question-open-ended  :title="title" :value="value" :state="state" :placeholder="placeholder !== '' ? placeholder: null" @input="explanationUpdate"><template v-if="explanation" #explanation><div v-html="explanation" /></template></survey-question-open-ended>`,methods:{explanationUpdate(p){s({value:p})}}}},e=u.bind({}),d={title:"Main question write here if it's long it will collapse.",value:"value",explanation:'<h3 class="modalHeader" style="text-align: center; margin-bottom: 16px;">Jak ocenić, czy slajdy i diagramy były zrozumiałe?</h3><div>Wyczerpujące materiały dają poczucie pełnego zrozumieina, przy jednoczesnym usystematyzowaniu informacji. Wpływa na to nie tylko ich jakość, ale też ilość.</div>',placeholder:"",state:r.DEFAULT},m={explanation:{control:"text"},state:{control:"select",options:Object.values(r)}};e.argTypes=m;e.args=d;e.parameters={design:{type:"figma",url:"https://www.figma.com/file/izQdYyiBR1GQgFkaOIfIJI/LMS---DS---Components?node-id=1555%3A41739"}};var t,a,o;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`args => {
+  const [_, updateArgs] = useArgs();
+  return {
+    components: {
+      SurveyQuestionOpenEnded
+    },
+    setup() {
+      return args;
+    },
+    // TODO typing in textarea looses focus
+    template: '<survey-question-open-ended  :title="title" :value="value" :state="state" :placeholder="placeholder !== \\'\\' ? placeholder: null" @input="explanationUpdate">' + '<template v-if="explanation" #explanation><div v-html="explanation" /></template>' + '</survey-question-open-ended>',
+    methods: {
+      explanationUpdate(value) {
+        updateArgs({
+          value
+        });
+      }
+    }
+  };
+}`,...(o=(a=e.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};const Q=["Interactive"];export{e as Interactive,Q as __namedExportsOrder,I as default};
---
+++ docs/assets/Well.stories-B4GjEFUr.js
@@ -0,0 +1,67 @@
+import{W as s,a as m}from"./Well-BZSY4xln.js";import{I as d}from"./Icon-Bw3k2jKC.js";import{a as h,b as a,C,c as g}from"./Chip-GghG-akA.js";import{D as I,B as D}from"./Banner-DvWMXY8-.js";import"./vue.esm-bundler-BEu-HfQJ.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./Button-BiWGn995.js";import"./IconButton-HFcQnm17.js";import"./device-mzBmLsxe.js";import"./Divider-Cj9DnfEY.js";import"./FeatureIcon-CDFOAFZx.js";function S(e){return`<div style="display: inline-flex; flex-direction: column; width: 100%; gap: 20px;">${e}
+<ds-banner :color="BANNER_COLORS.WARNING" title="Taka kombinacja jest niezgodna z design systemem!" v-if="invalidUsage" /></div>
+`}const y={title:"Components/Well",component:s,render:e=>({components:{Well:s,DsBanner:I},setup(){return{args:e,ICONS:d,BANNER_COLORS:D}},template:S('<well v-bind="args" :chip-left-icon="ICONS[args.chipLeftIcon]"><div v-html="args.content" /></well>'),computed:{invalidUsage(){return e.hasChip&&!e.padding}}}),argTypes:{padding:{control:"select",options:[null,...Object.values(m)]},content:{control:"text"}},parameters:{design:{type:"figma",url:"https://www.figma.com/design/izQdYyiBR1GQgFkaOIfIJI/LMS---DS-Components?node-id=13194-76861&p=f&m=dev"}}},t={args:{padding:null,content:'<h3 style="margin-top: 0">Content</h3>Voluptatem saepe suscipit optio et delectus esse sed velit. Autem maxime soluta aliquam perspiciatis quidem dolor saepe rerum.',hasChip:!1,chipLabel:"Chip z labelem",chipLabelUppercase:!1,chipLeftIcon:null,chipRadius:a.ROUNDED,color:h,colorHex:""}},u={chipLeftIcon:{control:"select",options:[null,...Object.keys(d)]},chipColor:{control:"select",options:Object.values(g)},chipRadius:{control:"select",options:Object.values(a)}};t.argTypes=u;const i={render:e=>({components:{Chip:C,Well:s},setup(){return{args:e,CHIP_RADIUSES:a,content:'<h3 style="margin-top: 0">Content</h3>Voluptatem saepe suscipit optio et delectus esse sed velit. Autem maxime soluta aliquam perspiciatis quidem dolor saepe rerum.'}},template:`<well v-bind="args">
+                <template #accessory>
+                    <chip
+                        label="First Chip"
+                        :radius="CHIP_RADIUSES.ROUNDED"
+                    />
+                    <chip
+                        label="Second Chip"
+                        :radius="CHIP_RADIUSES.ROUNDED"
+                    />
+                    <chip
+                        label="Third Chip"
+                        :radius="CHIP_RADIUSES.ROUNDED"
+                    />
+                </template>
+                <div v-html="content" />
+            </well>`}),args:{padding:m.SMALL}};i.argTypes=u;var o,p,r;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    padding: null,
+    content: '<h3 style="margin-top: 0">Content</h3>' + 'Voluptatem saepe suscipit optio et delectus esse sed velit. Autem maxime soluta aliquam perspiciatis quidem dolor saepe rerum.',
+    hasChip: false,
+    chipLabel: 'Chip z labelem',
+    chipLabelUppercase: false,
+    chipLeftIcon: null,
+    chipRadius: CHIP_RADIUSES.ROUNDED,
+    color: CHIP_DEFAULT_COLOR,
+    colorHex: ''
+  } as Args
+}`,...(r=(p=t.parameters)==null?void 0:p.docs)==null?void 0:r.source}}};var l,n,c;i.parameters={...i.parameters,docs:{...(l=i.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  render: args => {
+    return {
+      components: {
+        Chip,
+        Well
+      },
+      setup() {
+        return {
+          args,
+          CHIP_RADIUSES,
+          content: '<h3 style="margin-top: 0">Content</h3>' + 'Voluptatem saepe suscipit optio et delectus esse sed velit. Autem maxime soluta aliquam perspiciatis quidem dolor saepe rerum.'
+        };
+      },
+      template: \`<well v-bind="args">
+                <template #accessory>
+                    <chip
+                        label="First Chip"
+                        :radius="CHIP_RADIUSES.ROUNDED"
+                    />
+                    <chip
+                        label="Second Chip"
+                        :radius="CHIP_RADIUSES.ROUNDED"
+                    />
+                    <chip
+                        label="Third Chip"
+                        :radius="CHIP_RADIUSES.ROUNDED"
+                    />
+                </template>
+                <div v-html="content" />
+            </well>\`
+    };
+  },
+  args: {
+    padding: WELL_PADDINGS.SMALL
+  }
+}`,...(c=(n=i.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};const H=["Interactive","WithMultipleChips"];export{t as Interactive,i as WithMultipleChips,H as __namedExportsOrder,y as default};
