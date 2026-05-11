f2c6965bf04851cfc94eb50ab88f281de46ce483
latata <1592865+latata@users.noreply.github.com>
2026-05-11 13:53:07 +0000

    Build dist

--- dist/lib/js/components/Modals/Modal/Modal.stories.d.ts
+++
@@ -1,8 +0,0 @@
-import { Meta, StoryFn } from '@storybook/vue3';
-import Modal from './Modal.vue';
-declare const _default: Meta<typeof Modal>;
-export default _default;
-export declare const Interactive: StoryFn<typeof Modal>;
-export declare const Danger: StoryFn<typeof Modal>;
-export declare const WithImage: StoryFn<typeof Modal>;
-export declare const SizeSAndCentered: StoryFn<typeof Modal>;
--- dist/lib/js/components/SelectList/SelectListItemTile/SelectListItemTile.stories.d.ts
+++
@@ -1,6 +0,0 @@
-import SelectListItemTile from './SelectListItemTile.vue';
-import { Meta, StoryFn } from '@storybook/vue3';
-declare const _default: Meta<typeof SelectListItemTile>;
-export default _default;
-export declare const Interactive: StoryFn<typeof SelectListItemTile>;
-export declare const Static: StoryFn<typeof SelectListItemTile>;
