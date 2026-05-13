dc323742ba551a3e42d6910309dfbcb208add837
kvas-damian <kvas-damian@users.noreply.github.com>
2026-05-13 13:52:30 +0000

    Deploying to master from @ bethinkpl/design-system@197c06dc1e6d012a3c65f15e24da16c9de3d04bb 🚀

---
+++ docs/assets/CardExpandable.stories-CRDl7NFn.js
@@ -0,0 +1,37 @@
+import{C as n}from"./CardExpandable-qfxu5_f_.js";import"./vue.esm-bundler-KKLRPfRG.js";import"./Card-CXIbYkbQ.js";import"./Divider-Cv_Se4zt.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./LoadingBar-BunR9MCe.js";import"./ContainerRibbon-DP0rnZE8.js";import"./Icon-q3A7t-gx.js";import"./useLegacyI18n-D1OoysyW.js";const C={title:"Components/Cards/CardExpandable",component:n},p=r=>({components:{CardExpandable:n},setup(){return r},template:`
+            <card-expandable :is-expander-visible="isExpanderVisible" :expander-text-collapsed="expanderTextCollapsed !== '' ? expanderTextCollapsed : null"
+                                             :expander-text-expanded="expanderTextExpanded !== '' ? expanderTextExpanded : null" :is-expanded="isExpanded"
+                                             :header-has-padding="headerHasPadding" :divider-under-header="dividerUnderHeader">
+            <template v-if="header" #header>
+                <div v-html="header" />
+            </template>
+            <template #content="{ isExpanded: isExpandedInternal }">
+                <div v-html="content" />
+                <div v-if="isExpandedInternal">Dodatkowa treść w slocie <code>content</code> widoczna po rozwinięciu</div>
+            </template>
+            <template #expandedContent>
+                <div v-html="expandedContent"></div>
+            </template>
+            </card-expandable>`}),e=p.bind({}),i={header:"header slot",content:"content slot that supports <b>HTML markup</b>",expandedContent:"Dodatkowa treść w slocie <code>expandedContent</code> widoczna po rozwinięciu",isExpanderVisible:!0,expanderTextCollapsed:"Rozwiń",expanderTextExpanded:"Zwiń",isExpanded:!1,headerHasPadding:!1,dividerUnderHeader:!1},o={header:{control:"text"},content:{control:"text"},expandedContent:{control:"text"}};e.argTypes=o;e.args=i;e.parameters={design:{type:"figma",url:"https://www.figma.com/file/izQdYyiBR1GQgFkaOIfIJI/LMS---DS---Components?node-id=1552%3A34959"}};var d,a,t;e.parameters={...e.parameters,docs:{...(d=e.parameters)==null?void 0:d.docs,source:{originalSource:`args => ({
+  components: {
+    CardExpandable
+  },
+  setup() {
+    return args;
+  },
+  template: \`
+            <card-expandable :is-expander-visible="isExpanderVisible" :expander-text-collapsed="expanderTextCollapsed !== '' ? expanderTextCollapsed : null"
+                                             :expander-text-expanded="expanderTextExpanded !== '' ? expanderTextExpanded : null" :is-expanded="isExpanded"
+                                             :header-has-padding="headerHasPadding" :divider-under-header="dividerUnderHeader">
+            <template v-if="header" #header>
+                <div v-html="header" />
+            </template>
+            <template #content="{ isExpanded: isExpandedInternal }">
+                <div v-html="content" />
+                <div v-if="isExpandedInternal">Dodatkowa treść w slocie <code>content</code> widoczna po rozwinięciu</div>
+            </template>
+            <template #expandedContent>
+                <div v-html="expandedContent"></div>
+            </template>
+            </card-expandable>\`
+})`,...(t=(a=e.parameters)==null?void 0:a.docs)==null?void 0:t.source}}};const w=["Interactive"];export{e as Interactive,w as __namedExportsOrder,C as default};
---
+++ docs/assets/StatsFiltersDropdown.stories-CyuOGQD0.js
@@ -0,0 +1,15 @@
+import{_ as t}from"./StatsFiltersDropdown-CRogHgtP.js";import"./vue.esm-bundler-KKLRPfRG.js";import"./Button-BK3hF9wk.js";import"./Icon-q3A7t-gx.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./Dropdown-DEvYvlpw.js";import"./vue-popper-raBrryaq.js";import"./SelectList-CL7MdnGB.js";import"./SelectListItem-Bl9CgQMg.js";import"./useLegacyI18n-D1OoysyW.js";const g={title:"Components/StatsLayout/StatsFiltersDropdown",component:t,render:o=>({components:{StatsFiltersDropdown:t},setup(){return{args:o}},template:'<stats-filters-dropdown v-bind="args"></stats-filters-dropdown>'}),parameters:{design:{type:"figma",url:"https://www.figma.com/design/izQdYyiBR1GQgFkaOIfIJI/LMS---DS-Components?node-id=14870-10787&m=dev"}}},e={args:{filterItems:[{key:"all",label:"All"},{key:"active",label:"Active"},{key:"inactive",label:"Inactive"}],selectedFilterKey:"all"}};e.argTypes={filterItems:{control:"object"},selectedFilterKey:{control:"text"}};var r,a,n;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    filterItems: [{
+      key: 'all',
+      label: 'All'
+    }, {
+      key: 'active',
+      label: 'Active'
+    }, {
+      key: 'inactive',
+      label: 'Inactive'
+    }],
+    selectedFilterKey: 'all'
+  } as Args
+}`,...(n=(a=e.parameters)==null?void 0:a.docs)==null?void 0:n.source}}};const I=["Interactive"];export{e as Interactive,I as __namedExportsOrder,g as default};
