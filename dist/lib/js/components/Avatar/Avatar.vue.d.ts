86b92ddd809c59151a18722ffd3626b0bff59f32
latata <1592865+latata@users.noreply.github.com>
2026-08-25 12:17:52 +0000

    Build dist

--- dist/lib/js/components/Avatar/Avatar.vue.d.ts
+++ dist/lib/js/components/Avatar/Avatar.vue.d.ts
@@ -1,6 +1,11 @@
 import { AvatarAccessStatus, AvatarActivityStatus, AvatarSize } from './Avatar.consts';
 
-declare const _default: import('vue').DefineComponent<import('vue').ExtractPropTypes<__VLS_TypePropsToRuntimeProps<{
+declare function __VLS_template(): Readonly<{
+    teamMemberImage?: () => any;
+}> & {
+    teamMemberImage?: () => any;
+};
+declare const __VLS_component: import('vue').DefineComponent<import('vue').ExtractPropTypes<__VLS_TypePropsToRuntimeProps<{
     username: string;
     avatarUrl?: string;
     size?: AvatarSize;
@@ -17,6 +22,7 @@ declare const _default: import('vue').DefineComponent<import('vue').ExtractPropT
     teamMemberImageUrl?: string;
     activityStatusTooltip?: string;
 }>>> & Readonly<{}>, {}, {}, {}, {}, string, import('vue').ComponentProvideOptions, true, {}, any>;
+declare const _default: __VLS_WithTemplateSlots<typeof __VLS_component, ReturnType<typeof __VLS_template>>;
 export default _default;
 type __VLS_NonUndefinedable<T> = T extends undefined ? never : T;
 type __VLS_TypePropsToRuntimeProps<T> = {
@@ -25,5 +31,10 @@ type __VLS_TypePropsToRuntimeProps<T> = {
     } : {
         type: import('vue').PropType<T[K]>;
         required: true;
+    };
+};
+type __VLS_WithTemplateSlots<T, S> = T & {
+    new (): {
+        $slots: S;
     };
 };
--- dist/lib/js/components/DatePickers/DatePicker/DatePicker.vue.d.ts
+++ dist/lib/js/components/DatePickers/DatePicker/DatePicker.vue.d.ts
@@ -1022,7 +1022,6 @@ declare const _default: import('vue').DefineComponent<{
             readonly FA_MOBILE_ROTATE: import('@fortawesome/fontawesome-common-types').IconDefinition;
             readonly FA_MONEY_BILL1: import('@fortawesome/fontawesome-common-types').IconDefinition;
             readonly FA_MOON_STAR: import('@fortawesome/fontawesome-common-types').IconDefinition;
-            readonly FA_MOON_STAR_SOLID: import('@fortawesome/fontawesome-common-types').IconDefinition;
             readonly FA_MUSIC: import('@fortawesome/fontawesome-common-types').IconDefinition;
             readonly FA_NOTES: import('@fortawesome/fontawesome-common-types').IconDefinition;
             readonly FA_PAPER_PLANE: import('@fortawesome/fontawesome-common-types').IconDefinition;
@@ -1065,6 +1064,7 @@ declare const _default: import('vue').DefineComponent<{
             readonly FA_SQUARE_SOLID: import('@fortawesome/fontawesome-common-types').IconDefinition;
             readonly FA_STAR_SOLID: import('@fortawesome/fontawesome-common-types').IconDefinition;
             readonly FA_STAR: import('@fortawesome/fontawesome-common-types').IconDefinition;
+            readonly FA_SUN_BRIGHT: import('@fortawesome/fontawesome-common-types').IconDefinition;
             readonly FA_STETHOSCOPE: import('@fortawesome/fontawesome-common-types').IconDefinition;
             readonly FA_STOPWATCH_SOLID: import('@fortawesome/fontawesome-common-types').IconDefinition;
             readonly FA_TAG: import('@fortawesome/fontawesome-common-types').IconDefinition;
