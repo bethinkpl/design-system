1324893580a2e728e716fcba4508822390f832e9
Sebastian Dawidziak <sebastian@tweetgeek.pl>
2026-09-11 21:14:41 +0200

    IT-7750 |  Remove `FA_COMMENT_LOCK` icon

--- lib/js/icons/fontawesome.ts
+++ lib/js/icons/fontawesome.ts
@@ -69,7 +69,6 @@ import { faCode } from '@fortawesome/pro-regular-svg-icons/faCode';
 import { faCodeSimple } from '@fortawesome/pro-regular-svg-icons/faCodeSimple';
 import { faComment } from '@fortawesome/pro-regular-svg-icons/faComment';
 import { faCommentDots } from '@fortawesome/pro-regular-svg-icons/faCommentDots';
-import { faCommentLock } from '@fortawesome/pro-regular-svg-icons/faCommentLock';
 import { faCommentSlash } from '@fortawesome/pro-regular-svg-icons/faCommentSlash';
 import { faCommentPlus } from '@fortawesome/pro-regular-svg-icons/faCommentPlus';
 import { faComments } from '@fortawesome/pro-regular-svg-icons/faComments';
@@ -376,7 +375,6 @@ export const FONTAWESOME_ICONS = {
 	FA_COMMENT_DOTS: faCommentDots,
 	FA_COMMENT_PLUS: faCommentPlus,
 	FA_COMMENT_SOLID: fasComment,
-	FA_COMMENT_LOCK: faCommentLock,
 	FA_COMMENT_SLASH: faCommentSlash,
 	FA_COMMENT: faComment,
 	FA_COMMENTS_QUESTION_CHECK: faCommentsQuestionCheck,
